cf6be25f5a5fd3303cde3c31da397a529129b608
Kevin Stratford <kevin@epcc.ed.ac.uk>
2006-05-30 12:00:39 +0000

    Removed used BC_Map[].
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@22 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/src/d3q19.c
+++ trunk/src/d3q19.c
@@ -93,12 +93,6 @@ const double wv[NVEL] = {w0,
 
 Site    * site;
 
-
-const int BC_Map[NVEL] = {0,
-			  18, 17, 16, 15, 14, 13, 12, 11, 10,
-			  9,  8,  7,  6,  5,  4,  3,  2,  1};
-
-
 static double jchi1[NVEL][3];
 static double jchi2[NVEL][3];
 
