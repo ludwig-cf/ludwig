524734260fe6f017a6bfb619f74589f4dba22a30
Kevin Stratford <kevin@epcc.ed.ac.uk>
2006-08-04 18:06:06 +0000

    Added velocity array.
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@47 40876441-3496-0410-a613-a2fca35b2ae2

--- src/lattice.h
+++ src/lattice.h
@@ -9,9 +9,16 @@
 #ifndef _LATTICE_H_
 #define _LATTICE_H_
 
+#ifdef _D3Q19_
+  #include "d3q19.h"
+#else
+  #include "d3q15.h"
+#endif
+
 void LATT_allocate_force(const int);
 void LATT_allocate_phi(const int);
 void LATT_allocate_sites(const int);
+void latt_allocate_velocity(const int);
 
 struct vector {double c[3];};
 
