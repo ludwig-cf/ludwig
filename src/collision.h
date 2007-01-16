4ef989ba18694d6d5612d519eb4994457f685122
Kevin Stratford <kevin@epcc.ed.ac.uk>
2007-01-16 15:49:53 +0000

    See "Changes".
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@84 40876441-3496-0410-a613-a2fca35b2ae2

--- src/collision.h
+++ src/collision.h
@@ -15,7 +15,7 @@ void    MODEL_get_gradients( void );
 void    MODEL_calc_phi( void );
 void    get_fluctuations_stress(double shat[3][3]);
 void   RAND_init_fluctuations(void);
-void    MODEL_collide_multirelaxation(void);
+void    collide(void);
 void     MISC_curvature(void);
 void     MISC_set_mean_phi(double);
 void     latt_zero_force(void);
