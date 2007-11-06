c6adc926b8bdf861f06fabeee3ab1dde05dbca28
Kevin Stratford <kevin@epcc.ed.ac.uk>
2007-11-06 16:31:46 +0000

    New files for Brownian dynamics stuff
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@135 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ branches/dev_src/src/brownian.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ *
+ *  brownian.h
+ *
+ *  Brownian dynmaics
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) The University of Edinburgh (2007)
+ *
+ *  $Id: brownian.h,v 1.1.2.1 2007-11-06 16:31:46 kevin Exp $
+ *
+ *****************************************************************************/
+
+#ifndef _BROWNIAN_H
+#define _BROWNIAN_H
+
+void brownian_step_no_inertia(void);
+void brownian_set_random(void);
+
+#endif
