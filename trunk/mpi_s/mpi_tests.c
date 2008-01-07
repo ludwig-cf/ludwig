173b0daf7dda72a7b3d7addc560bcbbe52ba9a77
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-01-07 17:51:53 +0000

    MPI serial stubs
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@153 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ trunk/mpi_s/mpi_tests.c
@@ -0,0 +1,89 @@
+/*****************************************************************************
+ *
+ *  mpi_tests
+ *
+ *****************************************************************************/
+
+#include <assert.h>
+#include <stdio.h>
+#include "mpi.h"
+
+static MPI_Comm comm_ = MPI_COMM_WORLD;
+static int test_mpi_comm_rank(void);
+static int test_mpi_comm_size(void);
+static int test_mpi_allreduce(void);
+
+int main (int argc, char ** argv) {
+
+  int ireturn;
+
+  printf("Running mpi_s tests...\n");
+
+  ireturn = MPI_Init(&argc, &argv);
+  assert (ireturn == MPI_SUCCESS);
+
+  ireturn = test_mpi_comm_rank();
+  ireturn = test_mpi_comm_size();
+  ireturn = test_mpi_allreduce();
+
+  ireturn = MPI_Finalize();
+  assert(ireturn == MPI_SUCCESS);
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  test_mpi_comm_rank
+ *
+ *****************************************************************************/
+
+int test_mpi_comm_rank() {
+
+  int rank = MPI_PROC_NULL;
+  int ireturn;
+
+  ireturn = MPI_Comm_rank(comm_, &rank);
+  assert(ireturn == MPI_SUCCESS);
+  assert(rank == 0);
+
+  return MPI_SUCCESS;
+}
+
+/*****************************************************************************
+ *
+ *  test_mpi_comm_size
+ *
+ *****************************************************************************/
+
+int test_mpi_comm_size() {
+
+  int size = 0;
+  int ireturn;
+
+  ireturn = MPI_Comm_size(comm_, &size);
+  assert(ireturn == MPI_SUCCESS);
+  assert(size == 1);
+
+  return MPI_SUCCESS;
+}
+
+/*****************************************************************************
+ *
+ *  test_mpi_allreduce
+ *
+ *****************************************************************************/
+
+int test_mpi_allreduce() {
+
+  int ireturn;
+  double dsend, drecv;
+
+  dsend = 1.0; drecv = 0.0;
+  ireturn = MPI_Allreduce(&dsend, &drecv, 1, MPI_DOUBLE, MPI_SUM, comm_);
+  assert(ireturn == MPI_SUCCESS);
+  assert(dsend == 1.0);
+  assert(drecv == dsend);
+
+  return MPI_SUCCESS;
+}
