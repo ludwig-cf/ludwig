fc205d7f219672b2fc13ebe27cdaa5fac5989048
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-03-03 09:39:12 +0000

    yukawa_potential correction
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@191 40876441-3496-0410-a613-a2fca35b2ae2

--- src/potential.h
+++ src/potential.h
@@ -2,7 +2,7 @@
  *
  *  potential.h
  *
- *  $Id: potential.h,v 1.3 2008-02-12 16:23:38 dmarendu Exp $
+ *  $Id: potential.h,v 1.4 2008-03-03 09:39:12 kevin Exp $
  *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
  *
@@ -21,7 +21,7 @@ double soft_sphere_energy(const double);
 double soft_sphere_force(const double);
 double leonard_jones_energy(const double);
 double hard_sphere_energy(const double);
-double yukawa_energy(const double);
+double yukawa_potential(const double);
 double yukawa_force(const double);
 double get_max_potential_range(void);
 
