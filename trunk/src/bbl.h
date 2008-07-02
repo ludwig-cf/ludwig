2a557f9d1756e1de35be2966d5f6a7afed7abdc8
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-07-02 16:10:22 +0000

    Added surface stress diagnostic
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@297 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/src/bbl.h
+++ trunk/src/bbl.h
@@ -2,7 +2,7 @@
  *
  *  bbl.h
  *
- *  $Id: bbl.h,v 1.1 2006-10-12 13:55:21 kevin Exp $
+ *  $Id: bbl.h,v 1.2 2008-07-02 16:10:22 kevin Exp $
  *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
  *
@@ -12,6 +12,7 @@
 #define _BBL_H
 
 void bounce_back_on_links(void);
+void bbl_surface_stress(void);
 double bbl_order_parameter_deficit(void);
 
 #endif
