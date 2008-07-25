a521b774f9a8b9ac7965de6aec10d6f048f8f5f1
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-07-25 09:46:13 +0000

    added boilerplate
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@314 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/d2008-1/src/collision.h
+++ branches/d2008-1/src/collision.h
@@ -2,7 +2,13 @@
  *
  *  collision.h
  *
+ *  $Id: collision.h,v 1.3.4.3 2008-07-25 09:46:13 kevin Exp $
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) 2007 The University of Edinburgh
  *
  *****************************************************************************/
 
