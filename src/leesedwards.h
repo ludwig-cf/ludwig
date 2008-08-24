c46ae43c7ab475161f0da9e30eee7044157726e6
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-08-24 17:48:13 +0000

    Note changes to remove implementation of phi; removal of site pending
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@373 40876441-3496-0410-a613-a2fca35b2ae2

--- src/leesedwards.h
+++ src/leesedwards.h
@@ -3,8 +3,6 @@
 
 enum { PHI_ONLY, SITE_AND_PHI };
 
-enum { INTERP_SITE_F, INTERP_SITE_G, INTERP_PHI };
-
 
 /* struct for Lees-Edwards */
 typedef struct{
@@ -21,5 +19,33 @@ void LE_init( void );
 void LE_apply_LEBC( void );
 void LE_print_params( void );
 void LE_update_buffers( int );
+
+int le_get_nplane(void);
+int le_get_nxbuffer(void);
+int le_index_real_to_buffer(const int, const int);
+int le_index_buffer_to_real(const int);
+int le_site_index(const int, const int, const int);
+int le_plane_location(const int);
+
+double    le_buffer_displacement(const int);
+double    le_get_block_uy(int);
+MPI_Comm  le_communicator(void);
+void      le_displacement_ranks(const double, int[2], int[2]);
+void      le_init_shear_profile(void);
+
+
+/* Address macro. For performance purposes, -DNDEBUG replaces
+ * calls to ADDR, ie., le_site_index() with a macro, which requires that
+ * the local system size be available as "nlocal". nhalo_ is
+ * const, and available from coords.h, as are {X,Y,Z}  */
+
+#ifdef NDEBUG
+#define ADDR(ic,jc,kc) \
+((nlocal[Y]+2*nhalo_)*(nlocal[Z]+2*nhalo_)*(nhalo_+(ic)-1) + \
+                      (nlocal[Z]+2*nhalo_)*(nhalo_+(jc)-1) + \
+                                           (nhalo_+(kc)-1))
+#else
+#define ADDR le_site_index
+#endif
 
 #endif /* _LEESEDWARDS_H */
