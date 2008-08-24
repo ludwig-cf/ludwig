fba82673709a32a343622489a5e723e3971a9198
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-08-24 17:37:59 +0000

    Added variable halo width via nhalo_
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@369 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/src/coords.h
+++ trunk/src/coords.h
@@ -2,9 +2,13 @@
  *
  *  coords.h
  *
- *  $Id: coords.h,v 1.2 2006-10-12 14:09:18 kevin Exp $
+ *  $Id: coords.h,v 1.3 2008-08-24 17:37:59 kevin Exp $
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
  *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) The University of Edinburgh (2008)
  *
  *****************************************************************************/
 
@@ -13,6 +17,8 @@
 
 enum cartesian_directions {X, Y, Z};
 enum cartesian_neighbours {FORWARD, BACKWARD};
+
+extern const int nhalo_;
 
 void   coords_init(void);
 int    N_total(const int);
@@ -25,9 +31,8 @@ int    cart_coords(const int);
 int    cart_neighb(const int direction, const int dimension);
 void   get_N_local(int []);
 void   get_N_offset(int []);
+int    get_site_index(const int, const int, const int);
 
-#ifdef _MPI_
 MPI_Comm cart_comm(void);
-#endif
 
 #endif
