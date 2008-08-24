0d551ff21b7fe02da8646669beaa055763b6e04a
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-08-24 18:05:06 +0000

    Removed model dependency
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@381 40876441-3496-0410-a613-a2fca35b2ae2

--- src/subgrid.c
+++ src/subgrid.c
@@ -6,7 +6,7 @@
  *
  *  See Nash et al. (2007).
  *
- *  $Id: subgrid.c,v 1.3 2007-03-15 18:36:51 kevin Exp $
+ *  $Id: subgrid.c,v 1.4 2008-08-24 18:05:06 kevin Exp $
  *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
  *  (c) 2007 The University of Edinburgh
@@ -20,7 +20,6 @@
 #include "coords.h"
 #include "timer.h"
 #include "physics.h"
-#include "model.h" /* only index_site() at the moment */
 #include "lattice.h"
 #include "colloids.h"
 #include "ccomms.h"
@@ -89,7 +88,7 @@ void subgrid_force_from_particles() {
             for (j = j_min; j <= j_max; j++) {
 	      for (k = k_min; k <= k_max; k++) {
 
-		index = index_site(i, j, k);
+		index = get_site_index(i, j, k);
 
                 /* Separation between r0 and the coordinate position of
 		 * this site */
@@ -103,7 +102,7 @@ void subgrid_force_from_particles() {
 		force[X] = g[X]*dr;
 		force[Y] = g[Y]*dr;
 		force[Z] = g[Z]*dr;
-		add_force_at_site(index, force);
+		hydrodynamics_add_force_local(index, force);
 	      }
 	    }
 	  }
@@ -256,7 +255,7 @@ static void subgrid_interpolation() {
             for (j = j_min; j <= j_max; j++) {
 	      for (k = k_min; k <= k_max; k++) {
 
-		index = index_site(i, j, k);
+		index = get_site_index(i, j, k);
 
                 /* Separation between r0 and the coordinate position of
 		 * this site */
@@ -266,7 +265,7 @@ static void subgrid_interpolation() {
 		r[Z] = r0[Z] - (double) k;
 
 		dr = d_peskin(r[X])*d_peskin(r[Y])*d_peskin(r[Z]);
-		get_velocity_at_lattice(index, u);
+		hydrodynamics_get_velocity(index, u);
 
 		p_colloid->f0.x += u[X]*dr;
 		p_colloid->f0.y += u[Y]*dr;
