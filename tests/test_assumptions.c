a5057330386a4a2d0c3634e12f16b1a0ff36dbb8
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-08-26 08:39:41 +0000

    Added various system information
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@390 40876441-3496-0410-a613-a2fca35b2ae2

--- tests/test_assumptions.c
+++ tests/test_assumptions.c
@@ -10,9 +10,15 @@
  *****************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
+int is_bigendian(void);
+
 int main(int argc, char ** argv) {
+
+  int n;
+  int * p_int;
 
   printf("Testing assumptions...\n");
 
@@ -23,18 +29,9 @@ int main(int argc, char ** argv) {
   assert(sizeof(int) == 4);
   printf("yes\n");
 
-  printf("Checking sizeof(long int) is 4 bytes... ");
-  assert(sizeof(long int) == 4);
-  printf("yes\n");
-
-#ifdef _TESTS_PLUS_KR_
-  /* Note that long long requires K&R extensions to ANSI,
-   * so this might break under strictly conformant ANSI
-   * compiler. However, there are no long long in the code. */
-  printf("Checking sizeof(long long int) is 8 bytes... ");
-  assert(sizeof(long long int) == 8);
-  printf("yes\n");
-#endif
+  printf("Checking sizeof(long int) is >= 4 bytes... ");
+  assert(sizeof(long int) >= 4);
+  printf("yes (%ld bytes)\n", sizeof(long int));
 
   /* All floating point types in the code should be double,
    * which must be 8 bytes. */
@@ -49,9 +46,52 @@ int main(int argc, char ** argv) {
 
   printf("Checking FILENAME_MAX >= 128 characters ... ");
   assert(FILENAME_MAX >= 128);
-  printf("yes\n");
+  printf("yes (%d characters)\n", FILENAME_MAX);
+  printf("Checking BUFSIZ... (%d bytes)\n", BUFSIZ);
 
+  /* See what happens to zero size allocation */
+
+  n = 0;
+  p_int = (int *) malloc(n*sizeof(int));
+
+  if (p_int == NULL) {
+    printf("malloc(0) returns a NULL pointer\n");
+  }
+  else {
+    printf("malloc(0) returns non NULL pointer\n");
+    free(p_int);
+  }
+
+
+  /* Information */
+  printf("Language\n");
+  printf("__STC__ = %d\n", __STDC__);
+#if (__STDC_VERSION__ >= 199901)
+  printf("__STDC_VERSION__ = %ld\n", __STDC_VERSION__);
+#endif
+  printf("__DATE__ is %s\n", __DATE__);
+  printf("__TIME__ is %s\n", __TIME__);
+  printf("__FILE__ is %s\n", __FILE__);
+  printf("__LINE__ is %d\n", __LINE__);
+
+  printf("Host appears to be %s-endian\n", is_bigendian() ? "big" : "little");
   printf("All assumptions ok!\n");
 
   return 0;
+}
+
+/***************************************************************************
+ *
+ *  is_bigendian
+ *
+ *  Byte order for this 4-byte int is 00 00 00 01 for big endian (most
+ *  significant byte stored first).
+ *
+ ***************************************************************************/
+
+int is_bigendian() {
+
+  const int i = 1;
+
+  return (*(char *) &i == 0);
 }
