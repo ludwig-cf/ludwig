4474190c955a764974b63082c66e95ea59a55830
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-11-10 11:21:14 +0000

    Updates to meta information processing
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@430 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/util/extract.c
+++ trunk/util/extract.c
@@ -1,175 +1,315 @@
 /****************************************************************************
  *
- *  i/o extraction
+ *  extract.c
+ *
+ *  This program deals with the consolidation of parallel I/O
+ *  (in serial).
+ *
+ *  To process/recombined parallel I/O into a single file with the
+ *  correct order (ie., z running fastest, then y, then x) run
+ *
+ *  ./extract meta-file data-file
+ *
+ *  where 'meta-file' is the first meta information file in the series,
+ *  and data file is the first datsa file for a given time step. All
+ *  values are expected to be of data-type double - this will be
+ *  preserved on output.
+ *
+ *  Clearly, the two input files must match, or results will be
+ *  unpredictable.
+ *
+ *  Compile with $(CC) extract.c -lm
+ *
+ *  $Id: extract.c,v 1.2 2008-11-10 11:21:14 kevin Exp $
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Grouand and
+ *  Edinburgh Parallel Computing Centre
+ *  (c) The University of Edinburgh (2008)
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
  *
  ****************************************************************************/
 
-
+#include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int nlocal[3];
 int rank, cx, cy, cz, nlx, nly, nlz, nox, noy, noz;
 
-int ntotal[3]  = {512, 1024, 1};
-int ntargets[3] = {512, 1024, 1}; /* Output target */
-int nplanes_ = 32;
-double le_speed_ = 0.008;
+int ntotal[3];
+int ntargets[3]; /* Output target */
+int pe_[3];
+int nplanes_ = 0;
+int nio_;
+int nrec_ = 1;
+int input_isbigendian_ = -1;
+double le_speed_ = 0.0;
 int le_displace_ = 0;
 int * le_displacements_;
 
-int site_index_local(int,int,int);
-int index_section(int, int, int);
+char stub_[FILENAME_MAX];
+
+void read_meta_data_file(const char *);
+int  read_data_file_name(const char *);
+
+int site_index(int, int, int, const int *);
 void read_data(FILE *, int *, double *);
 void write_data(FILE *, int *, double *);
 int copy_data(double *, double *);
 int le_displacement(int, int);
-void set_displacements(void);
+void le_set_displacements(void);
+void le_unroll(double *);
 
 int main(int argc, char ** argv) {
 
-    int pe[3]      = {2, 4, 1};
-    int nio        = 1;
-    int ntime;
+  int ntime;
+  int i, n, p, pe_per_io;
+  int ifail;
 
-    int i, n, p, pe_per_io;
+  double * datalocal;
+  double * datasection;
+  char io_metadata[FILENAME_MAX];
+  char io_data[FILENAME_MAX];
+  char line[FILENAME_MAX];
+
+  FILE * fp_metadata;
+  FILE * fp_data;
+
+  /* Check the command line, then parse the meta data information,
+   * and sort out the data file name  */
+
+  if (argc != 3) {
+    printf("Usage %s meta-file data-file\n", argv[0]);
+    exit(-1);
+  }
+
+  read_meta_data_file(argv[1]);
+  ntime = read_data_file_name(argv[2]);
+
+  /* Work out parallel decompsoition that was used and the number of
+   * processors per I/O group. */
+
+  for (i = 0; i < 3; i++) {
+    nlocal[i] = ntotal[i]/pe_[i];
+  }
+
+  pe_per_io = pe_[0]*pe_[1]*pe_[2]/nio_;
 
 
-    double * datalocal;
-    double * datasection;
-    char io_metadata[FILENAME_MAX];
-    char io_data[FILENAME_MAX];
-    char line[FILENAME_MAX];
+  /* Allocate storage */
 
-    FILE * fp_metadata;
-    FILE * fp_data;
-
-    /* Get time from the command line */
-
-    if (argc != 2) {
-	printf("Use %s time step\n", argv[0]);
-	exit(-1);
-    }
-
-    ntime = atoi(argv[1]);
-    le_displace_ = ntime*le_speed_;
-    printf("Time: %d displacement: %d\n", ntime, le_displace_);
-
-
-    for (i = 0; i < 3; i++) {
-	nlocal[i] = ntotal[i]/pe[i];
-    }
-    /* No processors each io group */
-    pe_per_io = pe[0]*pe[1]*pe[2]/nio;
-
-    /* No sites per processor */
-    n = nlocal[0]*nlocal[1]*nlocal[2];
-    datalocal = (double *) malloc(n*sizeof(double));
-    if (datalocal == NULL) printf("Not allocated datalocal\n");
+  n = nrec_*nlocal[0]*nlocal[1]*nlocal[2];
+  datalocal = (double *) calloc(n, sizeof(double));
+  if (datalocal == NULL) printf("calloc(datalocal) failed\n");
     
-    /* No. sites in the section */ 
-    n = ntargets[0]*ntargets[1]*ntargets[2];
-    datasection = (double *) calloc(n, sizeof(double));
-    if (datasection == NULL) printf("malloc(datasection) failed\n");
+  /* No. sites in the target section (always original at moment) */
 
-    /* LE displacements */
-    le_displacements_ = (int *) malloc(ntotal[0]*sizeof(int));
-    if (le_displacements_ == NULL) printf("malloc(le_displacements_)\n");
-    set_displacements();
+  for (i = 0; i < 3; i++) {
+    ntargets[i] = ntotal[i];
+  }
+ 
+  n = nrec_*ntargets[0]*ntargets[1]*ntargets[2];
+  datasection = (double *) calloc(n, sizeof(double));
+  if (datasection == NULL) printf("calloc(datasection) failed\n");
 
-    /* Control */
-    for (n = 1; n <= nio; n++) {
-
-	/* open metadata file */
-	sprintf(io_metadata, "phi.%3.3d-%3.3d.meta", nio, n);
-	printf("Reading metadata file ... %s ", io_metadata);
-
-	fp_metadata = fopen(io_metadata, "r");
-	if (fp_metadata == NULL) printf("fopen(%s) failed\n", io_metadata);
-
-	for (p = 0; p < 9; p++) {
-	  fgets(line, FILENAME_MAX, fp_metadata);
-	  printf("%s", line);
-	}
-
-	/* open data file */
-	sprintf(io_data, "phi-%6.6d.%3.3d-%3.3d", ntime, nio, n);
-	printf("-> %s\n", io_data);
-
-	fp_data = fopen(io_data, "r+b");
-	if (fp_data == NULL) printf("fopen(%s) failed\n", io_data);
+  /* LE displacements as function of x */
+  le_displacements_ = (int *) malloc(ntotal[0]*sizeof(int));
+  if (le_displacements_ == NULL) printf("malloc(le_displacements_)\n");
+  le_set_displacements();
 
 
-	for (p = 0; p < pe_per_io; p++) {
+  /* Main loop */
 
-	  /* read metadata for this pe */
-	  fscanf(fp_metadata, "%d %d %d %d %d %d %d %d %d %d",
-		 &rank, &cx, &cy, &cz, &nlx, &nly, &nlz, &nox, &noy, &noz);
+  for (n = 1; n <= nio_; n++) {
 
-	  printf("%d %d %d %d %d %d %d %d %d %d\n",
-		 rank, cx, cy, cz, nlx, nly, nlz, nox, noy, noz);
+    /* Open metadata file and skip 12 lines to get to the
+     * decomposition inofmration */
 
-	  /* read data for this pe */
-	  read_data(fp_data, nlocal, datalocal);
+    sprintf(io_metadata, "%s.%3.3d-%3.3d.meta", stub_, nio_, n);
+    printf("Reading metadata file ... %s ", io_metadata);
 
-	  /* keep or discard */
-	  copy_data(datalocal, datasection);
-	}
+    fp_metadata = fopen(io_metadata, "r");
+    if (fp_metadata == NULL) printf("fopen(%s) failed\n", io_metadata);
 
-	fclose(fp_data);
-	fclose(fp_metadata);
+    for (p = 0; p < 12; p++) {
+      fgets(line, FILENAME_MAX, fp_metadata);
+      printf("%s", line);
     }
 
-    /* Write a single file with the final section */
+    /* Open the current data file */
 
-    sprintf(io_data, "phi-%6.6d", ntime);
-    fp_data = fopen(io_data, "w+b");
+    sprintf(io_data, "%s-%6.6d.%3.3d-%3.3d", stub_, ntime, nio_, n);
+    printf("-> %s\n", io_data);
+
+    fp_data = fopen(io_data, "r+b");
     if (fp_data == NULL) printf("fopen(%s) failed\n", io_data);
 
-    printf("\nWriting result to %s\n", io_data);
+    /* Read data file based on offsets recorded in the metadata,
+     * then copy this section to the global array */
 
-    write_data(fp_data, ntargets, datasection);
+    for (p = 0; p < pe_per_io; p++) {
+
+      ifail = fscanf(fp_metadata, "%d %d %d %d %d %d %d %d %d %d",
+		     &rank, &cx, &cy, &cz, &nlx, &nly, &nlz, &nox, &noy, &noz);
+      assert(ifail == 10);
+
+      read_data(fp_data, nlocal, datalocal);
+      copy_data(datalocal, datasection);
+    }
 
     fclose(fp_data);
-    free(datalocal);
-    free(le_displacements_);
+    fclose(fp_metadata);
+  }
 
-    return 0;
+  /* Write a single file with the final section */
+
+  sprintf(io_data, "%s-%6.6d", stub_, ntime);
+  fp_data = fopen(io_data, "w+b");
+  if (fp_data == NULL) printf("fopen(%s) failed\n", io_data);
+
+  printf("\nWriting result to %s\n", io_data);
+
+  write_data(fp_data, ntargets, datasection);
+
+  fclose(fp_data);
+  free(datalocal);
+  free(le_displacements_);
+  
+  return 0;
+}
+
+/****************************************************************************
+ *
+ *  read_meta_data_file
+ *
+ *  This sets a number of the global variable from the meta data file.
+ *
+ ****************************************************************************/
+
+void read_meta_data_file(const char * filename) {
+
+  int npe, nrbyte;
+  int ifail;
+  char tmp[FILENAME_MAX];
+  FILE * fp_meta;
+  const int ncharoffset = 33;
+
+  fp_meta = fopen(filename, "r");
+  if (fp_meta == NULL) {
+    printf("fopen(%s) failed\n", filename);
+    exit(-1);
+  }
+
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  ifail = sscanf(tmp+ncharoffset, "%s\n", stub_);
+  assert(ifail == 1);
+  printf("Read stub: %s\n", stub_);
+  fgets(tmp, FILENAME_MAX, fp_meta);
+
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  ifail = sscanf(tmp+ncharoffset, "%d\n", &nrbyte);
+  assert(ifail == 1);
+  printf("Record size (bytes): %d\n", nrbyte);
+  assert((nrbyte % 8) == 0);
+  nrec_ = nrbyte/8;
+
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  ifail = sscanf(tmp+ncharoffset, "%d", &input_isbigendian_);
+  assert(ifail == 1);
+  assert(input_isbigendian_ == 0 || input_isbigendian_ == 1);
+
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  ifail = sscanf(tmp+ncharoffset, "%d\n", &npe);
+  assert(ifail == 1);
+  printf("Total number of processors %d\n", npe);
+
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  ifail = sscanf(tmp+ncharoffset, "%d %d %d", pe_, pe_+1, pe_+2);
+  assert(ifail == 3);
+  printf("Decomposition is %d %d %d\n", pe_[0], pe_[1], pe_[2]);
+  assert(npe == pe_[0]*pe_[1]*pe_[2]);
+
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  ifail = sscanf(tmp+ncharoffset, "%d %d %d", ntotal, ntotal+1, ntotal+2);
+  assert(ifail == 3);
+  printf("System size is %d %d %d\n", ntotal[0], ntotal[1], ntotal[2]);
+
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  ifail = sscanf(tmp+ncharoffset, "%d", &nplanes_);
+  assert(ifail == 1);
+  assert(nplanes_ >= 0);
+  printf("Number of Lees Edwards planes %d\n", nplanes_);
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  sscanf(tmp+ncharoffset, "%lf", &le_speed_);
+  printf("Lees Edwards speed: %f\n", le_speed_);
+
+  /* Number of I/O groups */
+  fgets(tmp, FILENAME_MAX, fp_meta);
+  sscanf(tmp+ncharoffset, "%d", &nio_);
+  printf("Number of I/O groups: %d\n", nio_);
+
+  fclose(fp_meta);
+
+  return;
+}
+
+/****************************************************************************
+ *
+ *  read_data_file_name
+ *
+ *  This replies on a filename of the form stub-nnnnnn.001-001
+ *  to identify the time step ('nnnnnnn').
+ *
+ ****************************************************************************/
+
+int read_data_file_name(const char * filename) {
+
+  int ntime = -1;
+  char * tmp;
+  
+  tmp = strchr(filename, '-');
+  if (tmp) {
+    sscanf(tmp+1, "%d.", &ntime);
+  }
+
+  assert (ntime >= 0);
+  return ntime;
 }
 
 /****************************************************************************
  *
  *  copy_data
  *
- *  Fill the required section with data from the current pe, if
- *  they intersect.
+ *  Copy the local data into the global array in the correct place.
  *
  ****************************************************************************/
 
 int copy_data(double * datalocal, double * datasection) {
 
-    int ic, jc, kc, ic_g, jc_g, kc_g;
-    int index_l, index_s;
+  int ic, jc, kc, ic_g, jc_g, kc_g;
+  int index_l, index_s, nr;
 
-    /* Sweep through the local sites for this pe */
+  /* Sweep through the local sites for this pe */
 
-    for (ic = 1; ic <= nlocal[0]; ic++) {
-	ic_g = nox + ic;
-	for (jc = 1; jc <= nlocal[1]; jc++) {
-	    jc_g = noy + jc;
-	    for (kc = 1; kc <= nlocal[2]; kc++) {
-		kc_g = noz + kc;
-		index_s = index_section(ic_g, jc_g, kc_g);
+  for (ic = 1; ic <= nlocal[0]; ic++) {
+    ic_g = nox + ic;
+    for (jc = 1; jc <= nlocal[1]; jc++) {
+      jc_g = noy + jc;
+      for (kc = 1; kc <= nlocal[2]; kc++) {
+	kc_g = noz + kc;
+	index_s = nrec_*site_index(ic_g, jc_g, kc_g, ntargets);
 
-		if (index_s >= 0) {
-		    index_l = site_index_local(ic, jc, kc);
-		    *(datasection + index_s) = *(datalocal + index_l);
-		}
-	    }
+	for (nr = 0; nr < nrec_; nr++) {
+	  index_l = nrec_*site_index(ic, jc, kc, nlocal);
+	  *(datasection + index_s + nr) = *(datalocal + index_l + nr);
 	}
+      }
     }
+  }
 
-    return 0;
+  return 0;
 }
 
 /****************************************************************************
@@ -182,25 +322,23 @@ int copy_data(double * datalocal, double * datasection) {
 
 void read_data(FILE * fp_data, int n[3], double * data) {
 
-    int ic, jc, kc, local, index;
-    double phi;
+  int ic, jc, kc, index, nr;
+  double phi;
 
-    for (ic = 1; ic <= n[0]; ic++) {
-	for (jc = 1; jc <= n[1]; jc++) {
-	    for (kc = 1; kc <= n[2]; kc++) {
-		index = site_index_local(ic, jc, kc);
+  for (ic = 1; ic <= n[0]; ic++) {
+    for (jc = 1; jc <= n[1]; jc++) {
+      for (kc = 1; kc <= n[2]; kc++) {
+	index = site_index(ic, jc, kc, nlocal);
 
-		/* index, double (old format)
-		fread(&local, sizeof(int), 1, fp_data);
-		fread(data + index, sizeof(double), 1, fp_data); */
-
-		fread(&phi, sizeof(double), 1, fp_data);
-		*(data + index) = phi;
-	    }
+	for (nr = 0; nr < nrec_; nr++) {
+	  fread(&phi, sizeof(double), 1, fp_data);
+	  *(data + nrec_*index + nr) = phi;
 	}
+      }
     }
+  }
 
-    return;
+  return;
 }
 
 /****************************************************************************
@@ -213,51 +351,21 @@ void read_data(FILE * fp_data, int n[3], double * data) {
 
 void write_data(FILE * fp_data, int n[3], double * data) {
 
-    int ic, jc, kc, index = 0;
+  int ic, jc, kc, index, nr;
 
-    for (ic = 0; ic < n[0]; ic++) {
-	for (jc = 0; jc < n[1]; jc++) {
-	    for (kc = 0; kc < n[2]; kc++) {
-		fwrite(data + index, sizeof(double), 1, fp_data);
-		index++;
-	    }
+  index = 0;
+  for (ic = 0; ic < n[0]; ic++) {
+    for (jc = 0; jc < n[1]; jc++) {
+      for (kc = 0; kc < n[2]; kc++) {
+	for (nr = 0; nr < nrec_; nr++) {
+	  fwrite(data + index, sizeof(double), 1, fp_data);
+	  index++;
 	}
+      }
     }
+  }
 
-    return;
-}
-
-/****************************************************************************
- *
- *  index_section
- *
- *  For given global lattice index (ic, jc, kc), return the index
- *  in the final section. -1 is returned if it's not in the section.
- *
- *  When LE planes are present, global index jc is adjusted as a
- *  function of ic to take account of the planes. We assume an
- *  integer plane displacement.
- *
- ****************************************************************************/
-
-int index_section(int ic, int jc, int kc) {
-
-    int index = -1;
-    int djc;
-
-    /* assume section origin is (1,1,1) */
-    /* (ic,jc,kc) must be in doamin, so */
-
-    if (nplanes_ > 0) {
-	djc = le_displacement(ic, jc);
-    }
-
-    if (ic <= ntargets[0] && djc <= ntargets[1] && kc <= ntargets[2]) {
-	/* work out index */
-	index = ntargets[1]*ntargets[2]*(ic-1) + ntargets[2]*(djc-1) + kc-1;
-    }
-
-    return index;
+  return;
 }
 
 /****************************************************************************
@@ -281,35 +389,42 @@ int le_displacement(int ic, int jc) {
 
 /****************************************************************************
  *
- *  set_displacements
+ *  le_set_displacements
  *
  ****************************************************************************/
 
-void set_displacements() {
+void le_set_displacements() {
 
-    int ic, dy;
-    int di;
+  int ic, dy;
+  int di;
+
+  for (ic = 0; ic < ntotal[0]; ic++) {
+    le_displacements_[ic] = 0.0;
+  }
+
+  if (nplanes_ > 0) {
 
     di = ntotal[0] / nplanes_;
     dy = -(nplanes_/2)*le_displace_;
 
     /* Fist half block */
     for (ic = 1; ic <= di/2; ic++) {
-	le_displacements_[ic-1] = dy;
+      le_displacements_[ic-1] = dy;
     }
 
     dy += le_displace_;
     for (ic = di/2 + 1; ic <= ntotal[0] - di/2; ic++) {
-	le_displacements_[ic-1] = dy;
-	if ( (ic - di/2) % di == 0 ) dy += le_displace_;
+      le_displacements_[ic-1] = dy;
+      if ( (ic - di/2) % di == 0 ) dy += le_displace_;
     }
 
     /* Last half block */
     for (ic = ntotal[0] - di/2 + 1; ic <= ntotal[0]; ic++) {
-	le_displacements_[ic-1] = dy;
+      le_displacements_[ic-1] = dy;
     }
+  }
 
-    return;
+  return;
 }
 
 /****************************************************************************
@@ -318,11 +433,70 @@ void set_displacements() {
  *
  ****************************************************************************/
 
-int site_index_local(int ic, int jc, int kc) {
+int site_index(int ic, int jc, int kc, const int n[3]) {
 
-    int index;
+  int index;
 
-    index = (nlocal[1]*nlocal[2]*(ic-1) + nlocal[2]*(jc-1) + kc-1);
+  index = (n[1]*n[2]*(ic-1) + n[2]*(jc-1) + kc-1);
 
-    return index;
+  return index;
+}
+
+
+/*****************************************************************************
+ *
+ *  le_unroll
+ *
+ *  Unroll the data in the presence of planes.
+ *  This is always done relative to the middle of the system (as defined
+ *  in le_displacements_[]).
+ *
+ *****************************************************************************/
+
+void le_unroll(double * data) {
+
+  int ic, jc, kc;
+  int j1, j2, jdy;
+  double * buffer;
+  double dy, fr;
+
+  assert(nrec_ <= 1);
+
+  /* Allocate the temporary buffer */
+    
+  buffer = (double *) malloc(ntargets[1]*ntargets[2]*sizeof(double));
+  if (buffer == NULL) {
+    printf("malloc(buffer) failed\n");
+    exit(-1);
+  }
+
+  for (ic = 1; ic <= ntargets[0]; ic++) {
+    dy = le_displacements_[ic-1];
+    jdy = floor(dy);
+    fr = dy - jdy;
+
+    for (jc = 1; jc <= ntargets[1]; jc++) {
+      j1 = 1 + (jc - jdy - 2 + 100*ntotal[1]) % ntotal[1];
+      j2 = 1 + j1 % ntotal[1];
+
+      for (kc = 1; kc <= ntargets[2]; kc++) {
+	buffer[site_index(1,jc,kc,ntargets)] = 
+	  fr*data[site_index(ic,j1,kc,ntargets)] +
+	  (1.0 - fr)*data[site_index(ic,j2,kc,ntargets)];
+      }
+    }
+    /* Put the whole buffer plane back in place */
+
+    for (jc = 1; jc <= ntargets[1]; jc++) {
+      for (kc = 1; kc <= ntargets[2]; kc++) {
+	data[site_index(ic,jc,kc,ntargets)] =
+	  buffer[site_index(1,jc,kc,ntargets)];
+      }
+    }
+
+  }
+
+  free(buffer);
+
+  return;
 }
