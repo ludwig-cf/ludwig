b54ea99852e2ab238d3c4fded7050a577c3e0ccb
Kevin Stratford <kevin@epcc.ed.ac.uk>
2008-11-26 19:34:32 +0000

    Added code to analyse the interface profile.
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@442 40876441-3496-0410-a613-a2fca35b2ae2

--- util/capillary.c
+++ util/capillary.c
@@ -16,7 +16,20 @@
  *  and are described below. The output file is always in BINARY
  *  format.
  *
- *  $Id: capillary.c,v 1.2 2008-10-22 16:16:23 kevin Exp $
+ *  1. Output capaillary structure
+ *  Set the required parameters and invoke with no argument
+ *      ./a.out
+ *   
+ *  2. Profiles
+ *  If the program is invoked with a single phi output file
+ *  argument, e.g.,
+ *      ./a.out phi-001000.001-001
+ *  a scatter plot of the profile of the interface in the
+ *  wetting half of the capillary will be produced. That
+ *  is height vs r, the radial distance from the centre.
+ *  The output file should match the capillary structure!
+ *
+ *  $Id: capillary.c,v 1.3 2008-11-26 19:34:32 kevin Exp $
  *
  *  Edinburgh Soft Matter and Statistcal Physics Group and
  *  Edinburgh Parallel Computing Centre
@@ -74,6 +87,8 @@ const int output_type = STATUS_WITH_H;
 
 const char * filename = "capillary.001-001";
 
+static void profile(const char *);
+
 /*****************************************************************************
  *
  *  main program
@@ -93,6 +108,8 @@ int main(int argc, char ** argv) {
   double y0 = 0.5*ymax + 0.5;
   double x, y, r;
   double h, h1, theta;
+
+  if (argc == 2) profile(argv[1]);
 
   printf("Free energy parameters:\n");
   printf("free energy parameter kappa = %f\n", kappa);
@@ -211,4 +228,104 @@ int main(int argc, char ** argv) {
   free(map_in);
 
   return 0;
+}
+
+/*****************************************************************************
+ *
+ *  profile
+ *
+ *  This attempts to give a profile of the interface as a function
+ *  of the radial distance from the centre of the capillary defined
+ *  above.
+ *
+ *  For each position (i, j) we examine the 1-d profile phi(z) and
+ *  locate the position of zero by linear interpolation. The results
+ *  go to standard output.
+ *
+ *  Note that the test for the interface assumes the phi = -1 block
+ *  is in the middle of the system (see block initialisation in
+ *  src/phi_stats.c).
+ *
+ *****************************************************************************/
+
+static void profile(const char * filename) {
+
+  int ic, jc, kc, index;
+  int inside;
+  double rc = 0.5*(xmax-2);
+  double x0 = 0.5*xmax + 0.5;
+  double y0 = 0.5*ymax + 0.5;
+  double r, x, y;
+  double * phi;
+  FILE * fp;
+
+  phi = (double *) malloc(xmax*ymax*zmax*sizeof(double));
+  if (phi == NULL) {
+    printf("malloc(phi) failed\n");
+    exit(-1);
+  }
+
+  /* Read the data */
+
+  printf("Reading phi data from %s...\n", filename);
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    printf("Failed to open %s\n", filename);
+    exit(-1);
+  }
+
+  for (ic = 0; ic < xmax; ic++) {
+    for (jc = 0; jc < ymax; jc++) {
+      for (kc = 0; kc < zmax; kc++) {
+	index = ic*zmax*ymax + jc*zmax + kc;
+	fread(phi + index, 1, sizeof(double), fp);
+      }
+    }
+  }
+
+  fclose(fp);
+
+  /* Find the interface for each solid location */
+
+  for (ic = 0; ic < xmax; ic++) {
+    x = 1.0 + ic - x0;
+    for (jc = 0; jc < ymax; jc++) {
+      y = 1.0 + jc - y0;
+
+      r = sqrt(x*x + y*y);
+
+      /* Work out whether solid or fluid */
+      inside = 0;
+      if (xsection == SQUARE) {
+	if (ic > 0 && ic < xmax-1 && jc > 0 && jc < ymax-1) inside = 1;
+      }
+      if (xsection == CIRCLE) {
+	if (r <= rc) inside = 1;
+      }
+
+      if (inside) {
+	/* Examine the profile */
+	double h, dh;
+	h = -1.0;
+
+	for (kc = z1; kc <= z2; kc++) {
+	  index = ic*zmax*ymax + jc*zmax + kc;
+	  if (phi[index] > 0.0 && phi[index+1] < 0.0) {
+	    /* Linear interpolation to get surface position */
+	    dh = phi[index] / (phi[index] - phi[index+1]);
+	    h = 1.0 + kc + dh;
+	  }
+	}
+	printf("%f %f\n", r, h);
+      }
+    }
+  }
+
+  free(phi);
+
+  /* Do not return! */
+  exit(0);
+
+  return;
 }
