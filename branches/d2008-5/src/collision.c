51a53416f9be1ed77d63d3f31c183a87d00e011f
cevi_parker <cevi_parker>
2009-05-18 16:34:41 +0000

    checking in correct version
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@556 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/d2008-5/src/collision.c
+++ branches/d2008-5/src/collision.c
@@ -4,7 +4,7 @@
  *
  *  Collision stage routines and associated data.
  *
- *  $Id: collision.c,v 1.16.4.2 2009-05-14 11:44:33 cevi_parker Exp $
+ *  $Id: collision.c,v 1.16.4.3 2009-05-18 16:34:41 cevi_parker Exp $
  *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
@@ -261,7 +261,7 @@ void MODEL_collide_multirelaxation() {
 	for (p = 0; p < NVEL; p++) {
 	  site[index].f[p] = 0.0;
 	  for (m = 0; m < nmodes_; m++) {
-	    site[index].f[p] += 2;//mi_[p][m]*mode[m];
+	    site[index].f[p] += mi_[p][m]*mode[m];
 	  }
 	}
 
