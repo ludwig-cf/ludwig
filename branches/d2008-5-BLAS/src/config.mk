d2f3523f75df8930c8a3dfb9bf6638e95aaf4d8a
cevi_parker <cevi_parker>
2009-07-01 10:33:06 +0000

    goto config for hector
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@604 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/d2008-5-BLAS/src/config.mk
+++ branches/d2008-5-BLAS/src/config.mk
@@ -5,12 +5,15 @@
 #Define Machine.
 #
 #MACHINE = HPCX
-MACHINE = Ness
-#MACHINE = HecToR
+#MACHINE = Ness
+MACHINE = HecToR
 #MACHINE = ECDF
 
-# choose 'single' or 'binary' fluid scheme
-SCHEME= single
+# choose compiler suite for Hector Machine option
+#PRG_ENV=pgi
+
+# binary or single fluid
+#SCHEME= single
 
 # chose whether to use got blas
 GOTO = goto
@@ -27,15 +30,24 @@ else
 		CC=gcc
 		MPICC=mpicc 
 		OPTS = -D_D3Q19_ -fastsse
-		CFLAGS=$(OPTS) -DNDEBUG -Minform=warn -Msafeptr -Mipa=inline,fast
+		CFLAGS=$(OPTS) -DNDEBUG -Minform=warn -Msafeptr -Mipa=inline,fast 
 		LIBS= -DACML -lm -lacml -lpgftnrtl -lrt
 	else
 		ifeq ($(MACHINE),HecToR)
-			CC=gcc
-			MPICC=cc
-			OPTS = -D_D3Q19_ -DACML
-			CFLAGS=$(OPTS) -Minform=warn -O3 -DNDEBUG 
-			LIBS= -lm -lacml -lpgftnrtl -lrt
+			ifeq ($(PRG_ENV), pgi)
+				CC=
+				MPICC=cc
+				OPTS = -D_D3Q19_ -DACML -fastsse
+				CFLAGS=$(OPTS) -DNDEBUG -Minform=warn -Msafeptr -Mipa=inline,fast 
+				LIBS=   -DACML -lacml -lrt  -lpgftnrtl -lm
+			else
+				CC=cc
+                                MPICC=cc
+                                OPTS = -D_D3Q19_ -DNDEBUG # -DACML
+                                CFLAGS=$(OPTS) -O3 -OPT:Ofast -OPT:recip=ON -OPT:malloc_algorithm=1 -inline \
+				 -INLINE:preempt=ON -march=auto -m64 -msse3 -LNO:simd=2
+                                LIBS=   -lacml -lm
+			endif
 		else
 			ifeq ($(MACHINE), ECDF)
 				CC=gcc
@@ -64,6 +76,9 @@ ifeq ($(GOTO),goto)
         endif
         ifeq ($(MACHINE),HPCX)
                 LIBS= libgoto_power5-r1.26.a -bmap:map
+        endif
+        ifeq ($(MACHINE), HecToR)
+                LIBS= -lm -L./ -lgoto_barcelonap-r1.26
         endif
 endif
 
