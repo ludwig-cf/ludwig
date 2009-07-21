df62424458a405ed8c67977e719462f50d1f143f
Oliver Henrich <oliver.henrich@strath.ac.uk>
2009-07-21 17:01:50 +0000

    additional code added at the beginning of the time loop for quenches / parameter changes during the run; needs compilation
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@617 40876441-3496-0410-a613-a2fca35b2ae2

--- current/LCMain_hybrid.cc
+++ current/LCMain_hybrid.cc
@@ -250,34 +250,57 @@ int main(int argc, char** argv)
 
   for (n=1+Nstart; n<=Nmax; n++) {
 
+/*************************/
+/* cut droplets or slabs */
+/*************************/
 
-  /* BP equilibration (assuming 1500 steps, change if inappropriate) */
-
-//      if(n==1500) startDroplet();
+      if(n==300100) startDroplet();
 //      if(n==1500) startSlab();
 
+
 /********************************/
-// change chirality during run
+/* change parameters during run */
+/********************************/
+
 /*
 
-   if(n==250000){Abulk=0.012611161; if(myPE==0){cout << "timestep " << n << ": changing parameter; Abulk = " << Abulk << endl;}}
-   if(n==300000){Abulk=0.028375113; if(myPE==0){cout << "timestep " << n << ": changing parameter; Abulk = " << Abulk << endl;}} 
-   if(n==350000){Abulk=0.11350045; if(myPE==0){cout << "timestep " << n << ": changing parameter; Abulk = " << Abulk << endl;}} 
+// temperature-chirality quench 
+
+   if(n==20000){
+
+      gam=3.0;
+      Abulk=0.0069395656; 
+
+
+      if(myPE==0){
+	 cout << "timestep " << n << ": changing parameter" << endl; 
+	 cout << "Abulk = " << Abulk << endl;
+	 cout << "gamma = " << gam << endl;
+	}
+   }
+
+
+
 
    kappa = sqrt(L1*27.0*q0*q0/(Abulk*gam));
    caz = (1.0+4./3.*kappa*kappa);
    tauc = 1.0/8.0*(1-4.0*kappa*kappa+pow(caz,1.5));
 
 */
-/*******************************/
 
+///*
 
-/********************************/
-// change voltage during run
+// change noise strength
 
+if(n==400000){noise_strength=1e-05;if(myPE==0){cout << "timestep " << n << ": changing parameter; noise_strength = " << noise_strength << endl;}}
+if(n==500000){noise_strength=5e-05;if(myPE==0){cout << "timestep " << n << ": changing parameter; noise_strength = " << noise_strength << endl;}}
 
+//*/
 
 /*
+
+// change external E-field
+
    if(n==410000){delVz= 0.55*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
    if(n==460000){delVz= 0.5*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
    if(n==510000){delVz= 0.55*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
@@ -285,6 +308,7 @@ int main(int argc, char** argv)
    if(n==610000){delVz= 0.65*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
 
 */
+
 /********************************/
 
 	computeStressFreeEnergy(n);
