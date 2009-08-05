63f2381c17cc5c242ccccc3d87838d75ba1c1cb1
Oliver Henrich <oliver.henrich@strath.ac.uk>
2009-08-05 10:24:47 +0000

    code which modifies control parameters during the run now in separate file
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@633 40876441-3496-0410-a613-a2fca35b2ae2

--- current/LCMain_hybrid.cc
+++ current/LCMain_hybrid.cc
@@ -250,66 +250,9 @@ int main(int argc, char** argv)
 
   for (n=1+Nstart; n<=Nmax; n++) {
 
-    /*************************/
-    /* cut droplets or slabs */
-    /*************************/
+// includes code which changes the control parameters during the run 
 
-      if(n==2000) startDroplet();
-//      if(n==1500) startSlab();
-
-
-    /********************************/
-    /* change parameters during run */
-    /********************************/
-
-///*
-
-// temperature-chirality quench 
-
-   if(n==20000){
-
-      gam=3.1764706;
-      Abulk=0.01820565; 
-
-
-      if(myPE==0){
-	 cout << "timestep " << n << ": changing parameter" << endl; 
-	 cout << "Abulk = " << Abulk << endl;
-	 cout << "gamma = " << gam << endl;
-	}
-   }
-
-
-
-
-   kappa = sqrt(L1*27.0*q0*q0/(Abulk*gam));
-   caz = (1.0+4./3.*kappa*kappa);
-   tauc = 1.0/8.0*(1-4.0*kappa*kappa+pow(caz,1.5));
-
-//*/
-
-///*
-
-// change noise strength
-
-//if(n==450000){noise_strength=1e-05;if(myPE==0){cout << "timestep " << n << ": changing parameter; noise_strength = " << noise_strength << endl;}}
-//if(n==500000){noise_strength=5e-05;if(myPE==0){cout << "timestep " << n << ": changing parameter; noise_strength = " << noise_strength << endl;}}
-
-//*/
-
-/*
-
-// change external E-field
-
-   if(n==410000){delVz= 0.55*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
-   if(n==460000){delVz= 0.5*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
-   if(n==510000){delVz= 0.55*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
-   if(n==560000){delVz= 0.6*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
-   if(n==610000){delVz= 0.65*5.495388; if(myPE==0){cout << "timestep " << n << ": changing parameter; delVz = " << delVz << endl;}}
-
-*/
-
-/********************************/
+#include "Controlparameter.cc"
 
 	computeStressFreeEnergy(n);
 
