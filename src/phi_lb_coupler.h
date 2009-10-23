065a4423d57726cc0e9425a195e94439ffb8b9e2
Kevin Stratford <kevin@epcc.ed.ac.uk>
2009-10-23 16:26:27 +0000

    Missing prototype
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@682 40876441-3496-0410-a613-a2fca35b2ae2

--- src/phi_lb_coupler.h
+++ src/phi_lb_coupler.h
@@ -2,7 +2,7 @@
  *
  *  phi_lb_coupler.h
  *
- *  $Id: phi_lb_coupler.h,v 1.2 2009-09-02 08:10:32 kevin Exp $
+ *  $Id: phi_lb_coupler.h,v 1.3 2009-10-23 16:26:27 kevin Exp $
  *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
@@ -18,5 +18,6 @@
 void phi_compute_phi_site(void);
 void phi_set_mean_phi(double phi0);
 void phi_init_drop(double r, double xi0);
+void phi_lb_init_drop(double r, double xi0);
 
 #endif
