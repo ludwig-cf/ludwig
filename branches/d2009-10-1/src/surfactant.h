87bdf67f41264b96b6fa529cf5246848b51bc4a2
Kevin Stratford <kevin@epcc.ed.ac.uk>
2009-11-04 09:55:34 +0000

    Surfactant free energy
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@696 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ branches/d2009-10-1/src/surfactant.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+ *
+ *  surfactant.h
+ *
+ *  $Id: surfactant.h,v 1.1.2.1 2009-11-04 09:55:34 kevin Exp $
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group
+ *  and Edinburgh Parallel Computing Centre
+ *
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) The University of Edinburgh (2009)
+ *
+ ****************************************************************************/
+
+#ifndef SURFACTANT_H
+#define SURFACTANT_H
+
+void   surfactant_fluid_parameters_set(double a, double b, double kappa);
+void   surfactant_parameters_set(double kt, double epsilon, double beta,
+				 double w);
+double surfactant_interfacial_tension(void);
+double surfactant_interfacial_width(void);
+double surfactant_langmuir_isotherm(void);
+double surfactant_free_energy_density(const int index);
+double surfactant_chemical_potential(const int index, const int nop);
+double surfactant_isotropic_pressure(const int index);
+void   surfactant_chemical_stress(const int index, double s[3][3]);
+
+#endif
+
