e69de514d76d1819008df4f125a3aa7a7e23b42f
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-01-15 16:49:58 +0000

    Added new f implemetation; old version retained for tests
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@773 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/d2009-10-1/src/propagation.c
+++ branches/d2009-10-1/src/propagation.c
@@ -4,16 +4,20 @@
  *
  *  Propagation schemes for the different models.
  *
- *  $Id: propagation.c,v 1.4 2008-08-24 17:44:27 kevin Exp $
+ *  $Id: propagation.c,v 1.4.16.1 2010-01-15 16:49:58 kevin Exp $
  *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
  *
  *****************************************************************************/
 
+#include <assert.h>
+
 #include "pe.h"
 #include "timer.h"
 #include "coords.h"
 #include "model.h"
+
+#ifndef DIST_NEW
 
 extern Site * site;
 
@@ -385,3 +389,186 @@ static void d3q19_propagate_binary() {
 }
 
 #endif /* _D3Q19_ */
+
+#else
+
+
+static void propagate_d3q15(void);
+static void propagate_d3q19(void);
+
+/*****************************************************************************
+ *
+ *  propagation
+ *
+ *  Driver routine for the propagation stage.
+ *
+ *****************************************************************************/
+
+void propagation() {
+
+  TIMER_start(TIMER_PROPAGATE);
+
+  if (NVEL == 15) propagate_d3q15();
+  if (NVEL == 19) propagate_d3q19();
+
+  TIMER_stop(TIMER_PROPAGATE);
+
+  return;
+}
+
+/*****************************************************************************
+ *
+ *  propagate_d3q15
+ *
+ *  Follows the definition of the velocities in d3q15.c
+ *
+ *****************************************************************************/
+
+static void propagate_d3q15(void) {
+
+  int ic, jc, kc, index, n, p;
+  int xstr, ystr, zstr;
+  int nhalo;
+  int ndist;
+  int nlocal[3];
+
+  extern double * f_;
+
+  assert(NVEL == 15);
+
+  nhalo = coords_nhalo();
+  ndist = distribution_ndist();
+  get_N_local(nlocal);
+
+  zstr = ndist*NVEL;
+  ystr = zstr*(nlocal[Z] + 2*nhalo);
+  xstr = ystr*(nlocal[Y] + 2*nhalo);
+
+  /* Forward moving distributions */
+  
+  for (ic = nlocal[X]; ic >= 1; ic--) {
+    for (jc = nlocal[Y]; jc >= 1; jc--) {
+      for (kc = nlocal[Z]; kc >= 1; kc--) {
+
+        index = get_site_index(ic, jc, kc);
+
+	for (n = 0; n < ndist; n++) {
+	  p = ndist*NVEL*index + n*NVEL;
+	  f_[p + 7] = f_[p                         + (-1)*zstr + 7];
+	  f_[p + 6] = f_[p             + (-1)*ystr             + 6];
+	  f_[p + 5] = f_[p + (-1)*xstr + (+1)*ystr + (+1)*zstr + 5];
+	  f_[p + 4] = f_[p + (-1)*xstr + (+1)*ystr + (-1)*zstr + 4];
+	  f_[p + 3] = f_[p + (-1)*xstr                         + 3];
+	  f_[p + 2] = f_[p + (-1)*xstr + (-1)*ystr + (+1)*zstr + 2];
+	  f_[p + 1] = f_[p + (-1)*xstr + (-1)*ystr + (-1)*zstr + 1];
+	}
+      }
+    }
+  }
+
+  /* Backward moving distributions */
+  
+  for (ic = 1; ic <= nlocal[X]; ic++) {
+    for (jc = 1; jc <= nlocal[Y]; jc++) {
+      for (kc = 1; kc <= nlocal[Z]; kc++) {
+
+        index = get_site_index(ic, jc, kc);
+
+	for (n = 0; n < ndist; n++) {
+	  p = ndist*NVEL*index + n*NVEL;
+	  f_[p +  8] = f_[p                         + (+1)*zstr +  8];
+	  f_[p +  9] = f_[p             + (+1)*ystr             +  9];
+	  f_[p + 10] = f_[p + (+1)*xstr + (-1)*ystr + (-1)*zstr + 10];
+	  f_[p + 11] = f_[p + (+1)*xstr + (-1)*ystr + (+1)*zstr + 11];
+	  f_[p + 12] = f_[p + (+1)*xstr                         + 12];
+	  f_[p + 13] = f_[p + (+1)*xstr + (+1)*ystr + (-1)*zstr + 13];
+	  f_[p + 14] = f_[p + (+1)*xstr + (+1)*ystr + (+1)*zstr + 14];
+	}
+      }
+    }
+  }
+
+  return;
+}
+
+/*****************************************************************************
+ *
+ *  propagate_d3q19
+ *
+ *  Follows the velocities defined in d3q19.c
+ *
+ *****************************************************************************/
+
+static void propagate_d3q19(void) {
+
+  int ic, jc, kc, index, n, p;
+  int xstr, ystr, zstr;
+  int nhalo;
+  int ndist;
+  int nlocal[3];
+
+  extern double * f_;
+
+  assert(NVEL == 19);
+
+  nhalo = coords_nhalo();
+  ndist = distribution_ndist();
+  get_N_local(nlocal);
+
+  zstr = ndist*NVEL;
+  ystr = zstr*(nlocal[Z] + 2*nhalo);
+  xstr = ystr*(nlocal[Y] + 2*nhalo);
+
+  /* Distributions moving forward in memory. */
+  
+  for (ic = nlocal[X]; ic >= 1; ic--) {
+    for (jc = nlocal[Y]; jc >= 1; jc--) {
+      for (kc = nlocal[Z]; kc >= 1; kc--) {
+
+	index = get_site_index(ic, jc, kc);
+
+	for (n = 0; n < ndist; n++) {
+	  p = ndist*NVEL*index + n*NVEL;
+	  f_[p + 9] = f_[p                         + (-1)*zstr + 9];
+	  f_[p + 8] = f_[p             + (-1)*ystr + (+1)*zstr + 8];
+	  f_[p + 7] = f_[p             + (-1)*ystr             + 7];
+	  f_[p + 6] = f_[p             + (-1)*ystr + (-1)*zstr + 6];
+	  f_[p + 5] = f_[p + (-1)*xstr + (+1)*ystr             + 5];
+	  f_[p + 4] = f_[p + (-1)*xstr             + (+1)*zstr + 4];
+	  f_[p + 3] = f_[p + (-1)*xstr                         + 3];
+	  f_[p + 2] = f_[p + (-1)*xstr             + (-1)*zstr + 2];
+	  f_[p + 1] = f_[p + (-1)*xstr + (-1)*ystr             + 1];
+	}
+      }
+    }
+  }
+
+  /* Distributions mvoing backward in memory. */
+  
+  for (ic = 1; ic <= nlocal[X]; ic++) {
+    for (jc = 1; jc <= nlocal[Y]; jc++) {
+      for (kc = 1; kc <= nlocal[Z]; kc++) {
+
+	index = get_site_index(ic, jc, kc);
+
+	for (n = 0; n < ndist; n++) {
+	  p = ndist*NVEL*index + n*NVEL;
+	  f_[p + 10] = f_[p                         + (+1)*zstr + 10];
+	  f_[p + 11] = f_[p             + (+1)*ystr + (-1)*zstr + 11];
+	  f_[p + 12] = f_[p             + (+1)*ystr             + 12];
+	  f_[p + 13] = f_[p             + (+1)*ystr + (+1)*zstr + 13];
+	  f_[p + 14] = f_[p + (+1)*xstr + (-1)*ystr             + 14];
+	  f_[p + 15] = f_[p + (+1)*xstr             + (-1)*zstr + 15];
+	  f_[p + 16] = f_[p + (+1)*xstr                         + 16];
+	  f_[p + 17] = f_[p + (+1)*xstr             + (+1)*zstr + 17];
+	  f_[p + 18] = f_[p + (+1)*xstr + (+1)*ystr             + 18];
+	}
+      }
+    }
+  }
+
+  return;
+}
+
+
+#endif
