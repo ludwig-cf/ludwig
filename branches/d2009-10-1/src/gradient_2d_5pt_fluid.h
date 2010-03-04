ef942b48fb57f9f663f1456dd605e45d2e886809
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-03-04 09:11:20 +0000

    Code for 2-d gradients (untested).
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@820 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ branches/d2009-10-1/src/gradient_2d_5pt_fluid.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ *
+ *  gradient_2d_5pt_fluid.h
+ *
+ *  $Id: gradient_2d_5pt_fluid.h,v 1.1.2.1 2010-03-04 09:11:20 kevin Exp $
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
+ *  (c) The University of Edinburgh (2010)
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *
+ *****************************************************************************/
+
+#ifndef GRADIENT_2D_5PT_FLUID_H
+#define GRADIENT_2D_5PT_FLUID_H
+
+void gradient_2d_5pt_fluid_d2(void);
+void gradient_2d_5pt_fluid_d4(void);
+
+#endif
