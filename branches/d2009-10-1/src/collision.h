4c2d4c43bdec90b70bfbdc2bedb23119a6b8d1c2
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-03-27 11:22:31 +0000

    Moved MODEL_init
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@889 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/d2009-10-1/src/collision.h
+++ branches/d2009-10-1/src/collision.h
@@ -2,20 +2,19 @@
  *
  *  collision.h
  *
- *  $Id: collision.h,v 1.4.14.2 2010-01-15 16:59:15 kevin Exp $
+ *  $Id: collision.h,v 1.4.14.3 2010-03-27 11:22:31 kevin Exp $
  *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
  *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
- *  (c) 2007 The University of Edinburgh
+ *  (c) 2010 The University of Edinburgh
  *
  *****************************************************************************/
 
-#ifndef _COLLISION_H
-#define _COLLISION_H
+#ifndef COLLISION_H
+#define COLLISION_H
 
-void    MODEL_init( void );
 void    get_fluctuations_stress(double shat[3][3]);
 void   RAND_init_fluctuations(void);
 void    collide(void);
