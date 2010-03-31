4fbd43e316b2613b18ead85e2d024ecc1743e4e7
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-03-31 11:57:49 +0000

    Added dyadic; cosmetic update
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@933 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/d2009-10-1/src/polar_active_rt.c
+++ branches/d2009-10-1/src/polar_active_rt.c
@@ -4,7 +4,7 @@
  *
  *  Run time initialisation for active gel free energy.
  *
- *  $Id: polar_active_rt.c,v 1.1.2.1 2010-03-29 05:31:32 kevin Exp $
+ *  $Id: polar_active_rt.c,v 1.1.2.2 2010-03-31 11:57:49 kevin Exp $
  *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
@@ -17,9 +17,10 @@
 #include <assert.h>
 
 #include "pe.h"
-#include "phi.h"
-#include "coords.h"
 #include "runtime.h"
+#include "coords.h"
+#include "phi.h"
+#include "phi_gradients.h"
 #include "free_energy_vector.h"
 #include "polar_active.h"
 #include "polar_active_rt.h"
@@ -38,46 +39,49 @@ void polar_active_run_time(void) {
   double a;
   double b;
   double k1;
-  double k2;
+  double klc;
   double zeta;
   double lambda;
 
   /* Vector order parameter (nop = 3) and del^2 required. */
 
   phi_nop_set(3);
-  phi_gradient_level_set(2);
+  phi_gradients_level_set(2);
   coords_nhalo_set(2);
-
-  info("Gel X free energy selected.\n");
-  info("Vector order parameter nop = 3\n");
-  info("Requires up to del^2 derivatives so setting nhalo = 2\n");
-  info("\n");
 
   /* PARAMETERS */
 
-  /* Set as required. */
-
-  fe_density_set(polar_active_free_energy_density);
-  fe_chemical_stress_set(polar_active_chemical_stress);
-
   n = RUN_get_double_parameter("polar_active_a", &a);
   n = RUN_get_double_parameter("polar_active_b", &b);
-  n = RUN_get_double_parameter("polar_active_kappa1", &k1);
-  n = RUN_get_double_parameter("polar_active_kappa2", &k2);
+  n = RUN_get_double_parameter("polar_active_k", &k1);
+  n = RUN_get_double_parameter("polar_active_klc", &klc);
   n = RUN_get_double_parameter("polar_active_zeta", &zeta);
   n = RUN_get_double_parameter("polar_active_lambda", &lambda);
 
-  info("Parameters:\n");
-  info("Bulk parameter A      = %12.5e\n", a);
-  info("Bulk parameter B      = %12.5e\n", b);
-  info("Interfacial kappa1    = %12.5e\n", k1);
-  info("Interfacial kappa2    = %12.5e\n", k2);
-  info("Active parameter zeta = %12.5e\n", zeta);
-  info("Lambda                = %12.5e\n", lambda);
+  info("Polar active free energy selected.\n");
+  info("Vector order parameter nop = 3\n");
+  info("Requires up to del^2 derivatives so setting nhalo = 2\n");
 
-  polar_active_parameters_set(a, b, k1, k2);
+  if (klc != 0.0) {
+    phi_gradients_dyadic_set(1);
+    info("Requires dyadic term in gradients\n");
+  }
+
+  info("\n");
+
+  info("Parameters:\n");
+  info("Quadratic term a     = %12.5e\n", a);
+  info("Quartic term b       = %12.5e\n", b);
+  info("Elastic constant k   = %12.5e\n", k1);
+  info("Elastic constant klc = %12.5e\n", klc);
+  info("Activity zeta        = %12.5e\n", zeta);
+  info("Lambda               = %12.5e\n", lambda);
+
+  polar_active_parameters_set(a, b, k1, klc);
   polar_active_zeta_set(zeta);
 
+  fe_density_set(polar_active_free_energy_density);
+  fe_chemical_stress_set(polar_active_chemical_stress);
   fe_v_lambda_set(lambda);
   fe_v_molecular_field_set(polar_active_molecular_field);
 
