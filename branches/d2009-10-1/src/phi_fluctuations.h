967a02b6cccd51790fb6ea48ffd7fa462524a7bb
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-04-05 03:42:18 +0000

    Order parameter fluctuations via random flux
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@952 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ branches/d2009-10-1/src/phi_fluctuations.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ *
+ *  phi_fluctuations.h
+ *
+ *  $Id: phi_fluctuations.h,v 1.1.2.1 2010-04-05 03:42:18 kevin Exp $
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) 2010 The University of Edinburgh
+ *
+ *****************************************************************************/
+
+#ifndef PHI_FLUCTUATIONS_H
+#define PHI_FLUCTUATIONS_H
+
+void phi_fluctuations_random_flux(double * fluxw, double * fluxe,
+				  double * fluxy, double * fluxz);
+
+#endif
