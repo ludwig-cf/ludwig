4e34ec9e5f6777a470f9052b9782d6c1d1a0e2e3
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-08-04 17:48:39 +0000

    Moved run time initialisation here
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1062 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ branches/d2009-10-1/src/distribution_rt.h
@@ -0,0 +1,20 @@
+/*****************************************************************************
+ *
+ *  distribution_rt.h
+ *
+ *  $Id: distribution_rt.h,v 1.1.2.1 2010-08-04 17:48:39 kevin Exp $
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) 2010 The University of Edinburgh
+ *
+ *****************************************************************************/
+
+#ifndef DISTRIBUTION_RT_H
+#define DISTRIBUTION_RT_H
+
+void distribution_run_time(void);
+
+#endif
