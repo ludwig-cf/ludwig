6015f65620ac36de1f19ca185f798e69a982ab2a
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-09-23 17:12:02 +0000

    Y and Z swaps now tested.
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1097 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/d2009-10-1/tests/test_colloid_sums.c
+++ branches/d2009-10-1/tests/test_colloid_sums.c
@@ -4,7 +4,7 @@
  *
  *  Test of the various sum routines.
  *
- *  $Id: test_colloid_sums.c,v 1.1.2.2 2010-09-20 17:13:42 kevin Exp $
+ *  $Id: test_colloid_sums.c,v 1.1.2.3 2010-09-23 17:12:02 kevin Exp $
  *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
@@ -23,10 +23,13 @@
 #include "colloid_sums.h"
 #include "tests.h"
 
+static int dim_; /* Current direction */
+
+static void test_colloid_sums_1d(void);
 static void test_colloid_sums_reference_set(colloid_t * cref, int seed);
 static void test_colloid_sums_copy(colloid_t ref, colloid_t * pc);
 static void test_colloid_sums_assert(colloid_t c1, colloid_t * c2);
-static void test_colloid_sums_edge_x(const int ncell[3]);
+static void test_colloid_sums_edge(const int ncell[3], const double r0[3]);
 
 /*****************************************************************************
  *
@@ -36,28 +39,12 @@ static void test_colloid_sums_edge_x(const int ncell[3]);
 
 int main(int argc, char ** argv) {
 
-  int ntotal[3] = {1024, 512, 256};
-  int ncell[3];
-
   pe_init(argc, argv);
+  info("Checking colloid sum messages\n");
 
-  coords_ntotal_set(ntotal);
-  coords_init();
+  test_colloid_sums_1d();
 
-  /* We use cells > 2 to prevent copies in directions other than x
-   * when a call to colloids_halo_state() is made. */
-
-  ncell[X] = 4;
-  ncell[Y] = 3;
-  ncell[Z] = 3;
-  test_colloid_sums_edge_x(ncell);
-
-  ncell[X] = 2;
-  ncell[Y] = 4;
-  ncell[Z] = 3;
-  test_colloid_sums_edge_x(ncell);
-
-  coords_finish();
+  info("Tests complete\n");
   pe_finalise();
 
   return 0;
@@ -65,20 +52,78 @@ int main(int argc, char ** argv) {
 
 /*****************************************************************************
  *
- *  test_colloid_sums_edge_x
+ *  test_colloid_sums_1d
  *
- *  Place a single particle at {delta, nlocal/2, nlocal/2} to test
- *  the x communication.
+ *  Place one colloid locally at the centre of the domain.
+ *  In each direction in turn, we put it close to the (periodic) boundary.
  *
  *****************************************************************************/
 
-static void test_colloid_sums_edge_x(const int ncell[3]) {
+static void test_colloid_sums_1d(void) {
 
-  int         index;
-  int         ic, jc, kc;
-  int         nlocal[3];
+  int ntotal[3] = {1024, 512, 256};
+  int nlocal[3];
+  int ncell[3];
 
-  double      r0[3];
+  double r0[3];
+
+  coords_ntotal_set(ntotal);
+  coords_init();
+
+  /* We use cells > 2 to prevent copies in directions other than dim_
+   * when a call to colloids_halo_state() is made. */
+
+  coords_nlocal(nlocal);
+
+  dim_ = X;
+  ncell[X] = 4;
+  ncell[Y] = 3;
+  ncell[Z] = 3;
+
+  r0[X] = Lmin(X) + 0.5;
+  r0[Y] = Lmin(Y) + 0.5*nlocal[Y];
+  r0[Z] = Lmin(Z) + 0.5*nlocal[Z];
+
+  test_colloid_sums_edge(ncell, r0);
+
+  ncell[X] = 2;
+  ncell[Y] = 4;
+  ncell[Z] = 3;
+
+  test_colloid_sums_edge(ncell, r0);
+
+  dim_ = Y;
+  r0[X] = Lmin(X) + 0.5*nlocal[X];
+  r0[Y] = Lmin(Y) + 0.5;
+  r0[Z] = Lmin(Z) + 0.5*nlocal[Z];
+
+  test_colloid_sums_edge(ncell, r0);
+
+  dim_ = Z;
+  r0[X] = Lmin(X) + 0.5*nlocal[X];
+  r0[Y] = Lmin(Y) + 0.5*nlocal[Y];
+  r0[Z] = Lmin(Z) + 0.5;
+
+  test_colloid_sums_edge(ncell, r0);
+
+  coords_finish();
+
+  return;
+}
+
+/*****************************************************************************
+ *
+ *  test_colloid_sums_edge
+ *
+ *  Place a single particle at r0 to test the communication.
+ *
+ *****************************************************************************/
+
+static void test_colloid_sums_edge(const int ncell[3], const double r0[3]) {
+
+  int index;
+  int ic, jc, kc;
+
   colloid_t * pc;
   colloid_t   cref1;   /* All ranks get the same reference colloids */
   colloid_t   cref2;
@@ -86,16 +131,11 @@ static void test_colloid_sums_edge_x(const int ncell[3]) {
   test_colloid_sums_reference_set(&cref1, 1);
   test_colloid_sums_reference_set(&cref2, 2);
   colloids_cell_ncell_set(ncell);
-  coords_nlocal(nlocal);
 
   colloids_init();
 
   /* This must work in parallel to initialise only a single particle
    * which only gets swapped in the x-direction. */
-
-  r0[X] = Lmin(X) + 0.5;
-  r0[Y] = Lmin(Y) + 0.5*nlocal[Y];
-  r0[Z] = Lmin(Z) + 0.5*nlocal[Z];
 
   index = 1;
   pc = colloid_add_local(index, r0);
@@ -113,6 +153,18 @@ static void test_colloid_sums_edge_x(const int ncell[3]) {
   colloid_sums_dim(X, COLLOID_SUM_STRUCTURE);
   colloid_sums_dim(X, COLLOID_SUM_DYNAMICS);
   colloid_sums_dim(X, COLLOID_SUM_ACTIVE);
+
+  if (dim_ == Y || dim_ == Z) {
+    colloid_sums_dim(Y, COLLOID_SUM_STRUCTURE);
+    colloid_sums_dim(Y, COLLOID_SUM_DYNAMICS);
+    colloid_sums_dim(Y, COLLOID_SUM_ACTIVE);
+  }
+
+  if (dim_ == Z) {
+    colloid_sums_dim(Z, COLLOID_SUM_STRUCTURE);
+    colloid_sums_dim(Z, COLLOID_SUM_DYNAMICS);
+    colloid_sums_dim(Z, COLLOID_SUM_ACTIVE);
+  }
 
   /* Everywhere check colloid index = 1 has the correct sum */
 
