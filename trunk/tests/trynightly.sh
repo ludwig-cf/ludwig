c8587d59c81143141b08890eb92412f15b3b9795
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-12-09 08:56:24 +0000

    The script now works
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1191 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/tests/trynightly.sh
+++ trunk/tests/trynightly.sh
@@ -20,17 +20,20 @@ thisdir=`pwd`
 testdir=trunk/tests
 testscript=trytests.sh
 
-svn co --username stratford http://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk
+# Log file
+
+record=`date +%F-%T`.log
+
+# Checkout the SVN (and send the report to the record)
+
+svn co --username stratford http://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk \
+    &> $record
 
 # Move to the test directory and run the script, sending stdout and
 # stderr to record file
 
-record=`date +%F-%T`.log
-
 cd $testdir
-#./$testscript &> $record
-# Pseudotest
-echo "This are the test results" > $record
+./$testscript &> $record
 
 # Recover the output to the present directory
 
