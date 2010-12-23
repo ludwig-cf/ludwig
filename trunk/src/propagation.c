f631ba49aa21d22e63b583ac63e4d066bfde0053
Kevin Stratford <kevin@epcc.ed.ac.uk>
2010-12-23 13:16:57 +0000

    Added distribution reverse order stuff
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1206 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/src/propagation.c
+++ trunk/src/propagation.c
@@ -4,7 +4,7 @@
  *
  *  Propagation schemes for the different models.
  *
- *  $Id: propagation.c,v 1.5 2010-10-15 12:40:03 kevin Exp $
+ *  $Id$
  *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
@@ -23,6 +23,7 @@
 static void propagate_d2q9(void);
 static void propagate_d3q15(void);
 static void propagate_d3q19(void);
+static void propagate_d3q19_r(void);
 
 /*****************************************************************************
  *
@@ -34,9 +35,17 @@ static void propagate_d3q19(void);
 
 void propagation() {
 
-  if (NVEL == 9) propagate_d2q9();
-  if (NVEL == 15) propagate_d3q15();
-  if (NVEL == 19) propagate_d3q19();
+  if (distribution_order() == MODEL) {
+    if (NVEL == 9) propagate_d2q9();
+    if (NVEL == 15) propagate_d3q15();
+    if (NVEL == 19) propagate_d3q19();
+  }
+  else {
+    /* Reverse implementation */
+    assert(NVEL != 9);
+    assert(NVEL != 15);
+    if (NVEL == 19) propagate_d3q19_r();
+  }
 
   return;
 }
@@ -254,6 +263,89 @@ static void propagate_d3q19(void) {
 	  f_[p + 16] = f_[p + (+1)*xstr                         + 16];
 	  f_[p + 17] = f_[p + (+1)*xstr             + (+1)*zstr + 17];
 	  f_[p + 18] = f_[p + (+1)*xstr + (+1)*ystr             + 18];
+	}
+      }
+    }
+  }
+
+  return;
+}
+
+/*****************************************************************************
+ *
+ *  propagate_d3q19_r
+ *
+ *  Reverse storage implementation.
+ *
+ *****************************************************************************/
+
+static void propagate_d3q19_r(void) {
+
+  int ic, jc, kc, index, n, p, q;
+  int xstr, ystr, zstr;
+  int nhalo;
+  int ndist;
+  int nsite;
+  int nlocal[3];
+
+  extern double * f_;
+
+  assert(NVEL == 19);
+
+  nhalo = coords_nhalo();
+  nsite = coords_nsites();
+  ndist = distribution_ndist();
+  coords_nlocal(nlocal);
+
+  /* Stride in memory for velocities, and space */
+  p = ndist*nsite;
+
+  zstr = 1;
+  ystr = zstr*(nlocal[Z] + 2*nhalo);
+  xstr = ystr*(nlocal[Y] + 2*nhalo);
+
+  for (n = 0; n < ndist; n++) {
+
+    /* Distributions moving forward in memory. */
+  
+    for (ic = nlocal[X]; ic >= 1; ic--) {
+      for (jc = nlocal[Y]; jc >= 1; jc--) {
+	for (kc = nlocal[Z]; kc >= 1; kc--) {
+
+	  index = coords_index(ic, jc, kc);
+
+	  q = n*nsite + index;
+	  f_[9*p + q] = f_[9*p + q                         + (-1)*zstr];
+	  f_[8*p + q] = f_[8*p + q             + (-1)*ystr + (+1)*zstr];
+	  f_[7*p + q] = f_[7*p + q             + (-1)*ystr            ];
+	  f_[6*p + q] = f_[6*p + q             + (-1)*ystr + (-1)*zstr];
+	  f_[5*p + q] = f_[5*p + q + (-1)*xstr + (+1)*ystr            ];
+	  f_[4*p + q] = f_[4*p + q + (-1)*xstr             + (+1)*zstr];
+	  f_[3*p + q] = f_[3*p + q + (-1)*xstr                        ];
+	  f_[2*p + q] = f_[2*p + q + (-1)*xstr             + (-1)*zstr];
+	  f_[1*p + q] = f_[1*p + q + (-1)*xstr + (-1)*ystr            ];
+	}
+      }
+    }
+
+    /* Distributions mvoing backward in memory. */
+  
+    for (ic = 1; ic <= nlocal[X]; ic++) {
+      for (jc = 1; jc <= nlocal[Y]; jc++) {
+	for (kc = 1; kc <= nlocal[Z]; kc++) {
+
+	  index = coords_index(ic, jc, kc);
+
+	  q = n*nsite + index;
+	  f_[10*p + q] = f_[10*p + q                         + (+1)*zstr];
+	  f_[11*p + q] = f_[11*p + q             + (+1)*ystr + (-1)*zstr];
+	  f_[12*p + q] = f_[12*p + q             + (+1)*ystr            ];
+	  f_[13*p + q] = f_[13*p + q             + (+1)*ystr + (+1)*zstr];
+	  f_[14*p + q] = f_[14*p + q + (+1)*xstr + (-1)*ystr            ];
+	  f_[15*p + q] = f_[15*p + q + (+1)*xstr             + (-1)*zstr];
+	  f_[16*p + q] = f_[16*p + q + (+1)*xstr                        ];
+	  f_[17*p + q] = f_[17*p + q + (+1)*xstr             + (+1)*zstr];
+	  f_[18*p + q] = f_[18*p + q + (+1)*xstr + (+1)*ystr            ];
 	}
       }
     }
