a48ef20b298dfa8ab07a2ecfe4e85c47a0108afb
Kevin Stratford <kevin@epcc.ed.ac.uk>
2011-01-19 15:59:49 +0000

    Cholesteric anchoring input and fixes (see issue 457)
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@1230 40876441-3496-0410-a613-a2fca35b2ae2

--- src/colloids_Q_tensor.h
+++ src/colloids_Q_tensor.h
@@ -1,10 +1,27 @@
-#ifndef _COLLOIDS_Q_TENSOR_H
-#define _COLLOIDS_Q_TENSOR_H
+/*****************************************************************************
+ *
+ *  colloids_q_tensor.h
+ *
+ *  $Id$
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
+ *  Juho Lintuvuori (jlintuvu@ph.ed.ac.uk)
+ *  (c) 2011 The University of Edinburgh
+ *
+ *****************************************************************************/
+
+#ifndef COLLOIDS_Q_TENSOR_H
+#define COLLOIDS_Q_TENSOR_H
+
+enum colloid_anchoring {ANCHORING_PLANAR, ANCHORING_NORMAL};
 
 void COLL_set_Q(void);
 void COLL_set_Q_2(void);
 void COLL_randomize_Q(double delta_r);
 void colloids_fix_swd(void);
+void colloids_q_tensor_anchoring_set(const int type);
 
 void scalar_q_io_init(void);
 extern struct io_info_t * io_info_scalar_q_;
