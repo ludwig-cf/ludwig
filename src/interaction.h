934190bfc286f3563496bd9f98463d2cdc2335ce
Kevin Stratford <kevin@epcc.ed.ac.uk>
2011-02-03 09:17:01 +0000

    Changes to interaction.c for big particles
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@1253 40876441-3496-0410-a613-a2fca35b2ae2

--- src/interaction.h
+++ src/interaction.h
@@ -8,22 +8,15 @@
  *  Edinburgh Parallel Computing Centre
  *
  *  Kevin Stratford (kevin@epc.ed.ac.uk)
- *  (c) 2010 The University of Edinburgh
+ *  (c) 2011 The University of Edinburgh
  *
  *****************************************************************************/
 
 #ifndef INTERACTION_H
 #define INTERACTION_H
 
-void      COLL_zero_forces(void);
 void      COLL_init(void);
-
-double    COLL_interactions(void);
 void      COLL_update(void);
-void      COLL_forces(void);
-
-double    soft_sphere_energy(const double);
-double    soft_sphere_force(const double);
 
 void      colloid_gravity(double f[3]);
 void      colloid_gravity_set(const double f[3]);
