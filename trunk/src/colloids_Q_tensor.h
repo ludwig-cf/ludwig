60eaaea73eff9eddda2d706aaa0fe0f9e42f4778
Kevin Stratford <kevin@epcc.ed.ac.uk>
2011-04-20 11:44:00 +0000

    Added support for run time choice of anchoring method
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1325 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/src/colloids_Q_tensor.h
+++ trunk/src/colloids_Q_tensor.h
@@ -16,6 +16,8 @@
 #define COLLOIDS_Q_TENSOR_H
 
 enum colloid_anchoring {ANCHORING_PLANAR, ANCHORING_NORMAL, ANCHORING_FIXED};
+enum colloid_anchoring_method {ANCHORING_METHOD_NONE, ANCHORING_METHOD_ONE,
+			       ANCHORING_METHOD_TWO};
 
 void COLL_set_Q(void);
 void COLL_set_Q_2(void);
@@ -27,6 +29,8 @@ void colloids_q_boundary(const double n[3], double qs[3][3], double q0[3][3]);
 void colloids_q_boundary_normal(const int index, const int di[3],
 				double dn[3]);
 double colloids_q_tensor_w(void);
+void colloids_q_anchoring_method_set(int method);
+int  colloids_q_anchoring_method(void);
 
 extern struct io_info_t * io_info_scalar_q_;
 void scalar_q_io_info_set(struct io_info_t * info);
