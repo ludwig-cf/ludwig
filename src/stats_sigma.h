f444a3de7f79eb9efb49291a4d386dc10322af89
Kevin Stratford <kevin@epcc.ed.ac.uk>
2011-10-21 16:42:59 +0000

    initial version sigma calibration
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@1467 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ src/stats_sigma.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ *
+ *  stats_sigma.h
+ *
+ *  $Id$
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) 2011 The University of Edinburgh
+ *
+ *****************************************************************************/
+
+#ifndef STATS_SIGMA_H
+#define STATS_SIGMA_H
+
+void stats_sigma_init(int nswitch);
+void stats_sigma_measure(int ntime);
+
+#endif
