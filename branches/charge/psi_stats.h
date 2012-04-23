569ea7feaf6e68b72a57401642f134a86f41c891
Kevin Stratford <kevin@epcc.ed.ac.uk>
2012-04-23 10:49:32 +0000

    New psi stats stuff
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1585 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ branches/charge/psi_stats.h
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ *
+ *  psi_stats.h
+ *
+ *  $Id$
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) 2012 The University of Edinburgh
+ *
+ *****************************************************************************/
+
+#ifndef PSI_STATS_H
+#define PSI_STATS_H
+
+#include "psi.h"
+
+int psi_stats_local(psi_t * psi, double * rho_min, double * rho_max,
+		    double * rho_tot);
+int psi_stats_info(psi_t * psi);
+
+#endif
