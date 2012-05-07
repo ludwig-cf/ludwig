8291d3b19ed6ecbb657c9be825cd99560bfcc49e
Kevin Stratford <kevin@epcc.ed.ac.uk>
2012-05-07 09:39:58 +0000

    Added electric field with magnetic field
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1643 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/testcharge/src/magnetic_field_rt.c
+++ branches/testcharge/src/magnetic_field_rt.c
@@ -26,17 +26,26 @@
 
 void magnetic_field_runtime(void) {
 
-  int n;
+  int nb, ne;
   double b0[3];
+  double e0[3];
 
-  n = RUN_get_double_parameter_vector("magnetic_b0", b0);
+  nb = RUN_get_double_parameter_vector("magnetic_b0", b0);
+  if (nb == 1) magnetic_field_b0_set(b0);
 
-  if (n == 1) magnetic_field_b0_set(b0);
+  ne = RUN_get_double_parameter_vector("electric_e0", e0);
+  if (ne == 1) electric_field_e0_set(e0);
 
-  magnetic_field_b0(b0);
-  info("\n");
-  info("Uniform external magnetic field: %12.5e %12.5e %12.5e\n\n",
-       b0[X], b0[Y], b0[Z]);
+  if (nb || ne) {
+    magnetic_field_b0(b0);
+    electric_field_e0(e0);
+
+    info("\n");
+    info("Uniform external fields\n");
+    info("---------------\n");
+    info("Magnetic field: %14.7e %14.7e %14.7e\n", b0[X], b0[Y], b0[Z]);
+    info("Electric field: %14.7e %14.7e %14.7e\n", e0[X], e0[Y], e0[Z]);
+  }
 
   return;
 }
