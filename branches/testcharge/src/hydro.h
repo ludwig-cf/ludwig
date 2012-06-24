7daf35cc75f4202a5e5fd2fbda5d89c09480d34d
Kevin Stratford <kevin@epcc.ed.ac.uk>
2012-06-24 14:04:10 +0000

    testcharge: comments added
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1690 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/testcharge/src/hydro.h
+++ branches/testcharge/src/hydro.h
@@ -2,6 +2,14 @@
  *
  *  hydro.h
  *
+ *  $Id$
+ *
+ *  Edinburgh Soft Matter and Statistical Physics Group and
+ *  Edinburgh Parallel Computing Centre
+ *
+ *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  (c) 2012 The University of Edinburgh
+ *
  *****************************************************************************/
 
 #ifndef HYDRO_H
