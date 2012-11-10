c2baac9d384ff90a2654dab63654dffdd06ef95b
Kevin Stratford <kevin@epcc.ed.ac.uk>
2012-11-10 12:33:33 +0000

    Added explicit volume flux statistic for porous media
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@1810 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/src/stats_velocity.c
+++ trunk/src/stats_velocity.c
@@ -28,6 +28,13 @@
  *
  *  stats_velocity_minmax
  *
+ *  The volume flux of is of interest for porous media calculations
+ *  of permeability. Note that with the body force density f, the
+ *  volume flux is the same as the total momemtum  plus 0.5*f per
+ *  lattice site. So for complex porous media, the total momentum
+ *  can actually look quite wrong (e.g., have the opposite sign to
+ *  the flow).
+ *
  ****************************************************************************/
 
 void stats_velocity_minmax(void) {
@@ -37,6 +44,7 @@ void stats_velocity_minmax(void) {
   double umin[3];
   double umax[3];
   double utmp[3];
+  double usum_local[3], usum[3];
 
   MPI_Comm comm;
 
@@ -46,6 +54,7 @@ void stats_velocity_minmax(void) {
   for (ia = 0; ia < 3; ia++) {
     umin[ia] = FLT_MAX;
     umax[ia] = FLT_MIN;
+    usum_local[ia] = 0.0;
   }
 
   for (ic = 1; ic <= nlocal[X]; ic++) {
@@ -60,6 +69,7 @@ void stats_velocity_minmax(void) {
 	  for (ia = 0; ia < 3; ia++) {
 	    umin[ia] = dmin(umin[ia], utmp[ia]);
 	    umax[ia] = dmax(umax[ia], utmp[ia]);
+	    usum_local[ia] += utmp[ia];
 	  }
 	}
       }
@@ -78,10 +88,13 @@ void stats_velocity_minmax(void) {
     umax[ia] = utmp[ia];
   }
 
+  MPI_Reduce(usum_local, usum, 3, MPI_DOUBLE, MPI_SUM, 0, comm);
+
   info("\n");
   info("Velocity - x y z\n");
   info("[minimum ] %14.7e %14.7e %14.7e\n", umin[X], umin[Y], umin[Z]);
   info("[maximum ] %14.7e %14.7e %14.7e\n", umax[X], umax[Y], umax[Z]);
+  info("[vol flux] %14.7e %14.7e %14.7e\n", usum[X], usum[Y], usum[Z]);
 
   return;
 }
