8307a914d9a422df149852b0c0591d3f9e75f63c
rshort <rshort>
2013-07-10 14:12:52 +0000

    changed testing of psi_fft to use cos functions. Also introduced some timers
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2050 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/fft-testcharge/tests/test_psi_fft.c
+++ branches/fft-testcharge/tests/test_psi_fft.c
@@ -38,6 +38,9 @@ int main() {
   int nlocal[3] = {0, 0, 0};
   int global_coord[3] = {0, 0, 0};
   int global_coord_save[3] = {0, 0, 0};
+  int iter = 1;
+  double pi = 4.0*atan(1.0); 
+  double max;
 
   MPI_Init(NULL, NULL);
   
@@ -68,7 +71,7 @@ int main() {
   coords_nlocal_offset(global_coord);
   coords_nlocal_offset(global_coord_save);
 
-  /*set up psi_sor and psi_fft to be all 0s except for the center element*/ 
+  /*set up psi_sor and psi_fft to be cos functions*/ 
   for(i=1; i<=nlocal[X]; i++) {
     for(j=1; j<=nlocal[Y]; j++) {
       for(k=1; k<=nlocal[Z]; k++) {
@@ -78,15 +81,15 @@ int main() {
           psi_rho_set(psi_fft, index, 0, 0.0);
           psi_rho_set(psi_sor, index, 0, 0.0);
 
-         if(global_coord[0] == N_total(0)/2 && global_coord[1] == N_total(1)/2 && global_coord[2] == N_total(2)/2) {
+/*         if(global_coord[0] == N_total(0)/2 && global_coord[1] == N_total(1)/2 && global_coord[2] == N_total(2)/2) {
             psi_rho_set(psi_fft, index, 1, -10.0);
             psi_rho_set(psi_sor, index, 1, -10.0);
             printf("success\n");
           }
-        else {
-            psi_rho_set(psi_fft, index, 1, 0.0);
-            psi_rho_set(psi_sor, index, 1, 0.0);
-        }
+        else {*/
+          psi_rho_set(psi_fft, index, 1, cos(2*pi*global_coord[Z]/N_total(Z))*cos(2*pi*global_coord[Y]/N_total(Y))*cos(2*pi*global_coord[X]/N_total(X)) );
+          psi_rho_set(psi_sor, index, 1, cos(2*pi*global_coord[Z]/N_total(Z))*cos(2*pi*global_coord[Y]/N_total(Y))*cos(2*pi*global_coord[X]/N_total(X)) );
+//        }
         global_coord[Z] ++;
       }
       global_coord[Z] = global_coord_save[Z];
@@ -97,7 +100,7 @@ int main() {
   } 
 
 
-for(i=0; i<3; i++) { 
+for(i=0; i<iter; i++) { 
  /*use psi_sor_poisson to solve*/
    TIMER_start(TIMER_PSI_SOR_UPDATE);
   if(pe_rank() == 0) { printf("Solving with SOR\n"); }
@@ -106,7 +109,7 @@ for(i=0; i<3; i++) {
 }
 
 
-for(i=0; i<3; i++) { 
+for(i=0; i<iter; i++) { 
   /*use psi_fft_poisson to solve*/
    TIMER_start(TIMER_PSI_FFT_UPDATE);
   if(pe_rank() == 0) { printf("Solving with FFT\n"); }
@@ -116,18 +119,25 @@ for(i=0; i<3; i++) {
 
   TIMER_statistics();
 
+  max = 0.0;
   if(pe_rank() == 0) { printf("Checking results\n"); }
   /*check results are acceptably similar */
   for(i=1; i<=nlocal[X]; i++) {
     for(j=1; j<=nlocal[Y]; j++) {
       for(k=1; k<=nlocal[Z]; k++) {
-        if(pe_rank() == 0) {
-//          printf("%f %f\n", psi_fft->psi[coords_index(i,j,k)], psi_sor->psi[coords_index(i,j,k)]);
+/*        if(pe_rank() == 0) {
+          printf("%f %f\n", psi_fft->psi[coords_index(i,j,k)], psi_sor->psi[coords_index(i,j,k)]);
+          }
+*/
+        if(fabs(psi_fft->psi[coords_index(i,j,k)] - psi_sor->psi[coords_index(i,j,k)]) > max) {
+          max = fabs(psi_fft->psi[coords_index(i,j,k)] - psi_sor->psi[coords_index(i,j,k)]);
         }
 //        assert(psi_sor->psi[coords_index(i,j,k)] - psi_fft->psi[coords_index(i,j,k)] < 1e-5);
       }
     }
   }
+
+  printf("rank: %d, max: %f\n", pe_rank(), max);
 
 
   psi_free(psi_sor);
