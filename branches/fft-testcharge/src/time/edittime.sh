a5227818053c81229c7e3e0d3ec0ebb29f4d94b0
rshort <rshort>
2013-07-25 09:55:50 +0000

    added shell scripts for extracting times for use on BGQ
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2083 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ branches/fft-testcharge/src/time/edittime.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+for t in 128 256; do
+for i in sor fft; do
+
+#extract lines with 'psi update' and remove the filename and :
+grep 'psi update' time_${i}_$t* | sed 's/time.*._//g' | sed 's/://g' > ${i}time_$t
+
+#grep 'nerst planck' time_${i}_$t* | sed 's/time.*._//g' | sed 's/://g' > ${i}plancktime_$t
+
+#extract lines with [psi] and put into .dat file for printing the electric field of the system
+grep '\[psi\]' time_${i}_${t}_128 > psi_${i}_${t}.dat
+
+
+#this bit finds the time on 2 procs and pastes it back into the itimefile
+x=$(wc -l < ${i}time_${t})
+
+y=$(grep 'psi update' time_${i}_${t}_2 |  awk 'BEGIN {} { print $6 } END {}' | tail -1)
+
+echo x $x
+echo y $y
+
+touch temp
+
+for (( z=1; z<=$x; z++ ))
+do
+echo $y >> temp
+done
+
+paste ${i}time_${t} temp > temp2
+mv temp2 ${i}time_${t}
+
+rm temp
+
+done
+done
+
---
+++ branches/fft-testcharge/src/time/plottemplate.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+function plotdata() {
+for t in 128 256 512; do
+cat <<EOF
+  set style fill solid 1.0 border -1
+  set pointsize 2
+  set log y
+  set log x
+  set xlabel "MPI Tasks"
+  set ylabel "Speedup"
+  set term png
+  set output "speedup${t}.png"
+#  set term epslatex color
+#  set output "codegraph.tex"
+EOF
+  echo "plot \\"
+  echo "\"ffttime_${t}\" u 1:(\$10*2/\$7), \"sortime_${t}\" u 1:(\$10*2/\$7), x"
+
+done 
+
+#  i_start=2
+#  i_end=$(echo "$x - 7" | bc)
+#  for i in $(seq $i_start $(echo $i_end -1 | bc)) ;# do
+#    echo "\"timings.dat\" using $i:xticlabels(1), \\"
+#  done
+#  echo "\"timings.dat\" using $i_end:xticlabels(1)"
+}
+
+plotdata | gnuplot
+
+
+#epstopdf codegraph.eps
+
