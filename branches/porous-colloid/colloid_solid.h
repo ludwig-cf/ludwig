897a60706bd7faddcb91c0db62c51b5694480580
Juho Lintuvuori <juho.lintuvuori@u-bordeaux.fr>
2013-11-26 16:59:54 +0000

    added soft repulsive potential for cylinder
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2266 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/porous-colloid/colloid_solid.h
+++ branches/porous-colloid/colloid_solid.h
@@ -20,5 +20,7 @@ void solid_lubrication(colloid_t * pc, double force[3]);
 double cylinder_lubrication(const int dim, const double r[3], const double ah);
 double cylinder_lubrication_force(const double r[3], const double ah, double rhatij[3]);
 double porous_wall_lubrication_force(const int dim, const double r[3], const double ah);
+void cylinder_wall_soft_force(const double r[3], const double ah, double force[3]);
+void cylinder_bottom_soft_force(const double r[3], const double ah, double force[3]);
 
 #endif
