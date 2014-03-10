7a3a83787c102174624335b1ad8c6769c33a69c7
Alan Gray <alang@epcc.ed.ac.uk>
2014-03-10 10:03:49 +0000

    master copy of targetDP include and lib files
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2329 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ trunk/targetDP/targetDP_X86.c
@@ -0,0 +1,152 @@
+/*
+ * targetDP_X86.c: API Implementation for targetDP: X86 version
+ * Alan Gray, November 2013
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <omp.h>
+#include <math.h>
+
+
+void checkTargetError(const char *msg){
+
+  return;
+
+}
+
+void targetInit(size_t nsites,size_t nfieldsmax){
+  return;
+}
+
+void targetFinalize(){
+  return;
+}
+
+
+
+void targetMalloc(void **address_of_ptr,size_t size){
+
+  void* ptr;
+  ptr = malloc(size);
+
+  if(!ptr){
+    printf("malloc failed\n");
+    exit(1);
+  }
+    
+
+  *address_of_ptr=ptr;
+
+
+
+  return;
+  
+}
+
+void targetCalloc(void **address_of_ptr,size_t size){
+
+  void* ptr;
+  ptr = calloc(1,size);
+
+  if(!ptr){
+    printf("calloc failed\n");
+    exit(1);
+  }
+    
+
+  *address_of_ptr=ptr;
+
+
+
+  return;
+  
+}
+
+
+
+void targetFree(void *ptr){
+  
+  free(ptr);
+  return;
+  
+}
+
+
+void copyToTarget(void *targetData,const void* data,size_t size){
+
+  memcpy(targetData,data,size);
+  return;
+
+}
+
+void copyFromTarget(void *data,const void* targetData,size_t size){
+
+  memcpy(data,targetData,size);
+  return;
+
+}
+
+void copyToTargetMasked(double *targetData,const double* data,size_t nsites,
+			size_t nfields,char* siteMask){
+
+  int i,j;
+  for (i=0;i<nfields;i++){
+    for (j=0;j<nsites;j++){
+      if(siteMask[j]) targetData[i*nsites+j]=data[i*nsites+j];
+    }
+  }
+  return;
+  
+}
+
+void copyFromTargetMasked(double *data,const double* targetData,size_t nsites,
+			size_t nfields,char* siteMask){
+
+  int i, j;
+  for (i=0;i<nfields;i++){
+    for (j=0;j<nsites;j++){
+       if(siteMask[j]) data[i*nsites+j]=targetData[i*nsites+j];
+    }
+  }
+  return;
+
+}
+
+
+void syncTarget(){
+  return;
+}
+
+void copyConstantIntToTarget(int *data_d, int *data, int size){
+  memcpy(data_d, data, size); 
+  return;
+} 
+void copyConstantInt1DArrayToTarget(int *data_d, int *data, int size){
+  memcpy(data_d, data, size); 
+  return;
+} 
+void copyConstantInt2DArrayToTarget(int **data_d, int *data, int size){
+  memcpy(data_d, data, size); 
+  return;
+} 
+void copyConstantDoubleToTarget(double *data_d, double *data, int size){
+  memcpy(data_d, data, size); 
+  return;
+} 
+void copyConstantDouble1DArrayToTarget(double *data_d, double *data, int size){
+  memcpy(data_d, data, size); 
+  return;
+} 
+void copyConstantDouble2DArrayToTarget(double **data_d, double *data, int size){
+  memcpy(data_d, data, size); 
+  return;
+} 
+void copyConstantDouble3DArrayToTarget(double ***data_d, double *data, int size){
+  memcpy(data_d, data, size); 
+  return;
+}
+
+
