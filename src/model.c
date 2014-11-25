677b5bb43ccf9bcb68fb0ae336bcb7b63441aa2e
Alan Gray <alang@epcc.ed.ac.uk>
2014-11-25 09:51:05 +0000

    preparing propagation for tagetDP
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@2545 40876441-3496-0410-a613-a2fca35b2ae2

--- src/model.c
+++ src/model.c
@@ -95,6 +95,9 @@ void lb_free(lb_t * lb) {
   if (lb->io_info) io_info_destroy(lb->io_info);
   if (lb->f) free(lb->f);
   if (lb->t_f) targetFree(lb->t_f);
+  if (lb->t_fprime) targetFree(lb->t_fprime);
+  if (lb->siteMask) free(lb->siteMask);
+  if (lb->t_siteMask) targetFree(lb->t_siteMask);
 
   MPI_Type_free(&lb->plane_xy_full);
   MPI_Type_free(&lb->plane_xz_full);
@@ -154,6 +157,15 @@ int lb_init(lb_t * lb) {
 
     /* allocate target copy */
   targetCalloc((void **) &lb->t_f, ndata*sizeof(double));
+
+  /* allocate another space on target for staging data */
+  targetCalloc((void **) &lb->t_fprime, ndata*sizeof(double));
+
+
+  /* allocate boolean lattice-shaped struture for masking*/
+  
+  lb->siteMask = (char  *) calloc(ndata,sizeof(char));
+  targetCalloc((void **) &lb->t_siteMask, ndata*sizeof(char));
 
 
   /* Set up the MPI Datatypes used for full halo messages:
--- src/propagation.c
+++ src/propagation.c
@@ -18,6 +18,8 @@
 #include "coords.h"
 #include "propagation.h"
 #include "lb_model_s.h"
+#include "targetDP.h"
+#include <string.h>
 
 static int lb_propagate_d2q9(lb_t * lb);
 static int lb_propagate_d3q15(lb_t * lb);
@@ -142,6 +144,7 @@ static int lb_propagate_d3q15(lb_t * lb) {
   ystr = zstr*(nlocal[Z] + 2*nhalo);
   xstr = ystr*(nlocal[Y] + 2*nhalo);
 
+
   /* Forward moving distributions */
   
   for (ic = nlocal[X]; ic >= 1; ic--) {
@@ -189,6 +192,14 @@ static int lb_propagate_d3q15(lb_t * lb) {
   return 0;
 }
 
+
+
+TARGET_CONST int tc_nSites;
+TARGET_CONST int tc_ndist;
+TARGET_CONST int tc_nhalo;
+TARGET_CONST int tc_Nall[3];
+
+
 /*****************************************************************************
  *
  *  lb_propagate_d3q19
@@ -196,6 +207,86 @@ static int lb_propagate_d3q15(lb_t * lb) {
  *  Follows the velocities defined in d3q19.c
  *
  *****************************************************************************/
+
+TARGET  void lb_propagate_d3q19_site(double* t_f, 
+				     double* t_fprime, char* t_siteMask,
+					    const int baseIndex){
+  
+
+
+  int zstr = tc_ndist*NVEL;
+  int ystr = zstr*tc_Nall[Z];
+  int xstr = ystr*tc_Nall[Y];
+
+  int n;
+
+  
+    if(t_siteMask[baseIndex]){ //only non-halo sites
+	for (n = 0; n < tc_ndist; n++) {
+	  int p = tc_ndist*NVEL*baseIndex + n*NVEL;
+
+	  t_fprime[p] = t_f[p];
+	  t_fprime[p + 1] = t_f[p + (-1)*xstr + (-1)*ystr             + 1];
+	  t_fprime[p + 2] = t_f[p + (-1)*xstr             + (-1)*zstr + 2];
+	  t_fprime[p + 3] = t_f[p + (-1)*xstr                         + 3];
+	  t_fprime[p + 4] = t_f[p + (-1)*xstr             + (+1)*zstr + 4];
+	  t_fprime[p + 5] = t_f[p + (-1)*xstr + (+1)*ystr             + 5];
+	  t_fprime[p + 6] = t_f[p             + (-1)*ystr + (-1)*zstr + 6];
+	  t_fprime[p + 7] = t_f[p             + (-1)*ystr             + 7];
+	  t_fprime[p + 8] = t_f[p             + (-1)*ystr + (+1)*zstr + 8];
+	  t_fprime[p + 9] = t_f[p                         + (-1)*zstr + 9];
+	  t_fprime[p + 10] = t_f[p                         + (+1)*zstr + 10];
+	  t_fprime[p + 11] = t_f[p             + (+1)*ystr + (-1)*zstr + 11];
+	  t_fprime[p + 12] = t_f[p             + (+1)*ystr             + 12];
+	  t_fprime[p + 13] = t_f[p             + (+1)*ystr + (+1)*zstr + 13];
+	  t_fprime[p + 14] = t_f[p + (+1)*xstr + (-1)*ystr             + 14];
+	  t_fprime[p + 15] = t_f[p + (+1)*xstr             + (-1)*zstr + 15];
+	  t_fprime[p + 16] = t_f[p + (+1)*xstr                         + 16];
+	  t_fprime[p + 17] = t_f[p + (+1)*xstr             + (+1)*zstr + 17];
+	  t_fprime[p + 18] = t_f[p + (+1)*xstr + (+1)*ystr             + 18];
+	}
+ 
+    }
+
+    else{ //direct copy of t_fprime to t_f for halo sites 
+
+	for (n = 0; n < tc_ndist; n++) {
+
+	  int ip;
+	  for (ip=0;ip<NVEL;ip++){
+	  int p = tc_ndist*NVEL*baseIndex + n*NVEL + ip;
+
+	  t_fprime[p] = t_f[p]; 
+
+	  }
+
+
+
+	}
+      
+
+    }
+  
+
+
+
+}
+
+TARGET_ENTRY  void lb_propagate_d3q19_lattice(double* t_f, 
+					      double* t_fprime, char* t_siteMask){
+
+
+  int baseIndex=0;
+
+  //partition binary collision kernel across the lattice on the target
+  TARGET_TLP(baseIndex,tc_nSites){
+    lb_propagate_d3q19_site (t_f,t_fprime,t_siteMask,baseIndex);
+
+  }
+
+
+}
+
 
 static int lb_propagate_d3q19(lb_t * lb) {
 
@@ -210,57 +301,48 @@ static int lb_propagate_d3q19(lb_t * lb) {
   nhalo = coords_nhalo();
   coords_nlocal(nlocal);
 
-  zstr = lb->ndist*NVEL;
-  ystr = zstr*(nlocal[Z] + 2*nhalo);
-  xstr = ystr*(nlocal[Y] + 2*nhalo);
+  int Nall[3];
+  Nall[X]=nlocal[X]+2*nhalo;  Nall[Y]=nlocal[Y]+2*nhalo;  Nall[Z]=nlocal[Z]+2*nhalo;
 
-  /* Distributions moving forward in memory. */
-  
-  for (ic = nlocal[X]; ic >= 1; ic--) {
-    for (jc = nlocal[Y]; jc >= 1; jc--) {
-      for (kc = nlocal[Z]; kc >= 1; kc--) {
+  int nSites=Nall[X]*Nall[Y]*Nall[Z];
 
-	index = coords_index(ic, jc, kc);
+  int nFields=NVEL*lb->ndist;
 
-	for (n = 0; n < lb->ndist; n++) {
-	  p = lb->ndist*NVEL*index + n*NVEL;
-	  lb->f[p + 9] = lb->f[p                         + (-1)*zstr + 9];
-	  lb->f[p + 8] = lb->f[p             + (-1)*ystr + (+1)*zstr + 8];
-	  lb->f[p + 7] = lb->f[p             + (-1)*ystr             + 7];
-	  lb->f[p + 6] = lb->f[p             + (-1)*ystr + (-1)*zstr + 6];
-	  lb->f[p + 5] = lb->f[p + (-1)*xstr + (+1)*ystr             + 5];
-	  lb->f[p + 4] = lb->f[p + (-1)*xstr             + (+1)*zstr + 4];
-	  lb->f[p + 3] = lb->f[p + (-1)*xstr                         + 3];
-	  lb->f[p + 2] = lb->f[p + (-1)*xstr             + (-1)*zstr + 2];
-	  lb->f[p + 1] = lb->f[p + (-1)*xstr + (-1)*ystr             + 1];
-	}
-      }
-    }
-  }
 
-  /* Distributions mvoing backward in memory. */
-  
+  //zstr = lb->ndist*NVEL;
+  //ystr = zstr*(nlocal[Z] + 2*nhalo);
+  //xstr = ystr*(nlocal[Y] + 2*nhalo);
+
+
+  //start constant setup
+  copyConstantIntToTarget(&tc_nSites,&nSites, sizeof(int)); 
+  copyConstantIntToTarget(&tc_ndist,&lb->ndist, sizeof(int)); 
+  copyConstantIntToTarget(&tc_nhalo,&nhalo, sizeof(int)); 
+  copyConstantInt1DArrayToTarget( (int*) tc_Nall,Nall, 3*sizeof(int)); 
+  //end constant setup
+
+
+  memset(lb->siteMask,0,nSites*sizeof(char));
+  // set all non-halo sites to 1
   for (ic = 1; ic <= nlocal[X]; ic++) {
     for (jc = 1; jc <= nlocal[Y]; jc++) {
       for (kc = 1; kc <= nlocal[Z]; kc++) {
 
-	index = coords_index(ic, jc, kc);
+  	index=coords_index(ic, jc, kc);
+	lb->siteMask[index]=1;
 
-	for (n = 0; n < lb->ndist; n++) {
-	  p = lb->ndist*NVEL*index + n*NVEL;
-	  lb->f[p + 10] = lb->f[p                         + (+1)*zstr + 10];
-	  lb->f[p + 11] = lb->f[p             + (+1)*ystr + (-1)*zstr + 11];
-	  lb->f[p + 12] = lb->f[p             + (+1)*ystr             + 12];
-	  lb->f[p + 13] = lb->f[p             + (+1)*ystr + (+1)*zstr + 13];
-	  lb->f[p + 14] = lb->f[p + (+1)*xstr + (-1)*ystr             + 14];
-	  lb->f[p + 15] = lb->f[p + (+1)*xstr             + (-1)*zstr + 15];
-	  lb->f[p + 16] = lb->f[p + (+1)*xstr                         + 16];
-	  lb->f[p + 17] = lb->f[p + (+1)*xstr             + (+1)*zstr + 17];
-	  lb->f[p + 18] = lb->f[p + (+1)*xstr + (+1)*ystr             + 18];
-	}
       }
     }
   }
+
+
+
+  copyToTarget(lb->t_f,lb->f,nSites*nFields*sizeof(double)); 
+  copyToTarget(lb->t_siteMask,lb->siteMask,nSites*sizeof(char)); 
+
+  lb_propagate_d3q19_lattice TARGET_LAUNCH(nSites) (lb->t_f,lb->t_fprime,lb->t_siteMask);
+
+  copyFromTarget(lb->f,lb->t_fprime,nSites*nFields*sizeof(double)); 
 
   return 0;
 }
