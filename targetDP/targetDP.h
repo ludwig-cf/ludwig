337e5f2978152cb9f150896221ffa7ca3155bf7a
Alan Gray <alang@epcc.ed.ac.uk>
2015-02-20 12:01:22 +0000

    working towards compliance with emerging targetDP specification
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@2603 40876441-3496-0410-a613-a2fca35b2ae2

--- targetDP/targetDP.h
+++ targetDP/targetDP.h
@@ -10,56 +10,58 @@
 #define _DATA_PARALLEL_INCLUDED
 
 
-/* Language "extensions", implemented through preprocessor */
-
-
 #ifdef CUDA /* CUDA */
 
-#define HOST extern "C" __host__
-
-
-/* default threads per block */
-#define DEFAULT_TPB 256
-//#define DEFAULT_TPB 32
-
-/* kernel function specifiers */
-#define TARGET __device__ 
-#define TARGET_ENTRY __global__
-
-/* constant data specifier */
-#define TARGET_CONST __constant__
+/* Settings */
+#define DEFAULT_TPB 256 /* default threads per block */
 
 /* Instruction-level-parallelism vector length */
 //#define NILP 2
 #define NILP 1
 
 
+/* Language Extensions */
+
+#define HOST extern "C" __host__
+#define __targetHost__ extern "C" __host__
+
+
+/* kernel function specifiers */
+#define __target__ __device__ 
+#define __targetEntry__ __global__
+
+/* constant data specifier */
+#define __targetConst__ __constant__
+
 /* special kernel launch syntax */
-#define TARGET_LAUNCH(extent) \
+#define __targetLaunch__(extent) \
   <<<((extent/NILP)+DEFAULT_TPB-1)/DEFAULT_TPB,DEFAULT_TPB>>>
 
-#define TARGET_LAUNCH_NOSTRIDE(extent) \
+#define __targetLaunchNoStride__(extent) \
   <<<((extent)+DEFAULT_TPB-1)/DEFAULT_TPB,DEFAULT_TPB>>>
   
 
 /* Thread-level-parallelism execution macro */
-#define TARGET_TLP(simtIndex,extent) \
+#define __targetTLP__(simtIndex,extent) \
   simtIndex = NILP*(blockIdx.x*blockDim.x+threadIdx.x);	\
   if (simtIndex < extent)
 
-#define TARGET_TLP_NOSTRIDE(simtIndex,extent) \
+#define __targetTLPNoStride__(simtIndex,extent) \
   simtIndex = (blockIdx.x*blockDim.x+threadIdx.x);	\
   if (simtIndex < extent)
 
-#define __getTargetConstantAddress__(addr_of_ptr,const_object) \
+
+/* Functions */
+
+#define targetConstAddress(addr_of_ptr,const_object) \
   cudaGetSymbolAddress(addr_of_ptr, const_object); \
   checkTargetError("__getTargetConstantAddress__"); 
 
-#define __copyConstantToTarget__(data_d, data, size) \
+#define copyConstToTarget(data_d, data, size) \
   cudaMemcpyToSymbol(*data_d, (const void*) data, size, 0,cudaMemcpyHostToDevice); \
-   checkTargetError("__copyConstantToTarget__"); 
+   checkTargetError("copyConstToTarget"); 
 
-#define __copyConstantFromTarget__(data, data_d, size) \
+#define copyConstFromTarget(data, data_d, size) \
   cudaMemcpyFromSymbol((void*) data, *data_d, size, 0,cudaMemcpyDeviceToHost); \
    checkTargetError("__copyConstantFromTarget__"); 
 
@@ -69,41 +71,51 @@
 
 #else /* X86 */
 
-#define HOST
-
-/* kernel function specifiers */
-#define TARGET 
-#define TARGET_ENTRY
-
-/* constant data specifier */
-#define TARGET_CONST 
-
-/* special kernel launch syntax */
-#define TARGET_LAUNCH(extent)
-#define TARGET_LAUNCH_NOSTRIDE(extent)
+/* Settings */
 
 /* Instruction-level-parallelism vector length */
 #define NILP 1
 
+
+/* Language Extensions */
+
+#define HOST
+#define __targetHost__
+
+/* kernel function specifiers */
+#define __target__
+#define __targetEntry__
+
+/* constant data specifier */
+#define __targetConst__ 
+
+/* special kernel launch syntax */
+#define __targetLaunch__(extent)
+#define __targetLaunchNoStride__(extent)
+
+
 /* Thread-level-parallelism execution macro */
-/* #define TARGET_TLP(simtIndex,extent)    _Pragma("omp parallel for")	\
+/* #define __targetTLP__(simtIndex,extent)    _Pragma("omp parallel for")	\
    for(simtIndex=0;simtIndex<extent;simtIndex+=NILP)*/
 
-#define TARGET_TLP(simtIndex,extent)   	\
+#define __targetTLP__(simtIndex,extent)   	\
   for(simtIndex=0;simtIndex<extent;simtIndex+=NILP)
 
-#define TARGET_TLP_NOSTRIDE(simtIndex,extent)   	\
+#define __targetTLPNoStride__(simtIndex,extent)   	\
   for(simtIndex=0;simtIndex<extent;simtIndex++)
 
-#define __getTargetConstantAddress__(addr_of_ptr,const_object) \
+
+/* functions */
+
+#define targetConstAddress(addr_of_ptr,const_object) \
   *addr_of_ptr=&(const_object);
 
 
-#define __copyConstantToTarget__(data_d, data, size) \
+#define copyConstToTarget(data_d, data, size) \
   memcpy(data_d,data,size);
 
 
-#define __copyConstantFromTarget__(data, data_d, size) \
+#define copyConstFromTarget(data, data_d, size) \
   memcpy(data,data_d,size);
 
 
@@ -112,6 +124,8 @@
 
 
 /* Common */
+
+/* depricated? */
 
 /* Initialisation */
 /* #define TARGET_INDEX_INIT(extent)		\
@@ -143,14 +157,16 @@
 #define SITE(array,field) \
   array[targetExtent*p+baseIndex+vecIndex]
 
+/* end depricated? */
 
-#define GET_3DCOORDS_FROM_INDEX(index,coords,extents)	     \
+
+#define targetCoords3D(coords,extents,index)					\
   coords[0]=(index)/(extents[1]*extents[2]);				\
   coords[1] = ((index) - extents[1]*extents[2]*coords[0]) / extents[2];	\
   coords[2] = (index) - extents[1]*extents[2]*coords[0]			\
     - extents[2]*coords[1]; 
 
-#define INDEX_FROM_3DCOORDS(coords0,coords1,coords2,extents)	\
+#define targetIndex3D(coords0,coords1,coords2,extents)	\
   extents[2]*extents[1]*(coords0)				\
   + extents[2]*(coords1)					\
   + (coords2); 
@@ -159,10 +175,9 @@ enum {TARGET_HALO,TARGET_EDGE};
 
 /* API */
 
-void targetInit(size_t nsites, size_t nfieldsmax);
+void targetInit(size_t nsites,size_t nfieldsmax);
 void targetFinalize();
 void checkTargetError(const char *msg);
-void targetMalloc(void **address_of_ptr,size_t size);
 void copyToTarget(void *targetData,const void* data,size_t size);
 void copyFromTarget(void *data,const void* targetData,size_t size);
 void copyToTargetMasked(double *targetData,const double* data,size_t nsites,
@@ -174,9 +189,9 @@ void copyToTargetMaskedAoS(double *targetData,const double* data,size_t nsites,
 void copyFromTargetMaskedAoS(double *data,const double* targetData,size_t nsites,
 			size_t nfields,char* siteMask);
 
-void copyFromTargetHaloEdge(double *data,const double* targetData,int extents[3], size_t nfields, int nhalo,int haloOrEdge);
-void copyToTargetHaloEdge(double *targetData,const double* data,int extents[3], size_t nfields, int nhalo,int haloOrEdge);
-void syncTarget();
+void copyFromTargetBoundary3D(double *data,const double* targetData,int extents[3], size_t nfields, int offset,int depth);
+void copyToTargetBoundary3D(double *targetData,const double* data,int extents[3], size_t nfields, int offset,int depth);
+void targetSynchronize();
 void targetFree(void *ptr);
 void checkTargetError(const char *msg);
 void targetMalloc(void **address_of_ptr,const size_t size);
