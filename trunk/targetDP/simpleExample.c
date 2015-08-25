fc61e1fa89c035db8fbe7c9e8e69a40d301f9faa
Alan Gray <alang@epcc.ed.ac.uk>
2015-08-25 13:37:54 +0000

    adding license
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2719 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/targetDP/simpleExample.c
+++ trunk/targetDP/simpleExample.c
@@ -1,15 +1,30 @@
-//simple targetDP example
-//
-//to compile for CPU:
-//gcc -I. -c simpleExample.c
-//gcc -I. simpleExample.c libtargetDP_C.a 
-//
-//to compile for GPU:
-//nvcc -x cu -arch=sm_35 -I. -DCUDA -dc -c simpleExample.c 
-//nvcc -arch=sm_35 simpleExample.o libtargetDP_CUDA.a 
-
-//TODO: add makefile
-//TODO: comment below code
+/*
+ * simleExample.c: simple targetDP example
+ * Alan Gray
+ * 
+ * Copyright 2015 The University of Edinburgh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ * 
+ * to compile for CPU:
+ * gcc -I. -c simpleExample.c
+ * gcc -I. simpleExample.c libtargetDP_C.a 
+ * 
+ * to compile for GPU:
+ * nvcc -x cu -arch=sm_35 -I. -DCUDA -dc -c simpleExample.c 
+ * nvcc -arch=sm_35 simpleExample.o libtargetDP_CUDA.a 
+ *
+ */
 
 #include <stdio.h>
 #include <targetDP.h>
