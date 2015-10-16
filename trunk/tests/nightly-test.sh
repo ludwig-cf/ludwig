33ad11560a9de8bc872bac325e22b4a97c14a7ff
Kevin Stratford <kevin@epcc.ed.ac.uk>
2015-10-16 11:54:54 +0000

    1399 Added GPU tests to nightly script
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2756 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/tests/nightly-test.sh
+++ trunk/tests/nightly-test.sh
@@ -17,6 +17,7 @@
 # Despite --login we still need to get appropriate paths, etc:
 . /etc/profile
 eval `/usr/bin/modulecmd bash load PMPI`
+export PATH=$PATH:/usr/local/cuda/bin
 
 
 # This is the local directory for the nightly stuff
@@ -41,12 +42,16 @@ svn co --username stratford http://ccpforge.cse.rl.ac.uk/svn/ludwig &> $record
 # start via bsub (indy0.epcc.ed.ac.uk)
 
 cd $testdir
-bsub -o $record -e $record -n 64 -W 600 -q normal -J test-all < test-all.sh
+bsub -o $record -e $record -n 64 -W 1200 -q normal -J test-cpu < test-all.sh
+
+bsub -w "done(test-cpu)" -o $record -e $record -q gpu -J test-gpu < test-gpu-01.sh
 
 # Wait for the tests to finish, and clean up
 # This includes a copy of the summary to a public location
 # (the interactive job will wait for the main "test-all" job to
 # finish before starting and itself will finish before the copy is attempted)
+
+# Using "-q gpu" as "-q interactive" not active
 
 finish=$(cat <<EOF
  rm -rf ludwig
@@ -55,7 +60,7 @@ EOF
 )
 
 cd $thisdir
-bsub -w "done(test-all)" -o $record -e $record -I -q interactive "$finish"
+bsub -w "done(test-gpu)" -o $record -e $record -I -q gpu "$finish"
 
 scp -p $summary kevin@garnet:~/html/.
 
