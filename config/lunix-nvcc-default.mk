aa86ee4859a94e34affc911572a78cb31fc6bfe8
Kevin Stratford <kevin@epcc.ed.ac.uk>
2015-11-12 16:36:18 +0000

    Issue 1384 branch commit
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/branches/kevin-1407@2771 40876441-3496-0410-a613-a2fca35b2ae2

--- config/lunix-nvcc-default.mk
+++ config/lunix-nvcc-default.mk
@@ -7,7 +7,12 @@
 #
 ##############################################################################
 
-ROOT_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+CC=nvcc
+MPICC=dont
+CFLAGS=-O2 -arch=sm_35 -x cu -DCUDA -DCUDAHOST -dc
 
-include $(ROOT_DIR)/config.mk
+OPTS=-DNP_D3Q6
 
+LAUNCH_SERIAL_CMD=
+LAUNCH_MPI_CMD=mpirun
+LAUNCH_MPI_NP_SWITCH=-np
