b3c9c963300996fb5e5de5e889a21c356526d6db
Alan Gray <alang@epcc.ed.ac.uk>
2015-11-27 11:16:50 +0000

    minor fix
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@2787 40876441-3496-0410-a613-a2fca35b2ae2

--- src/bbl.c
+++ src/bbl.c
@@ -163,8 +163,7 @@ int bounce_back_on_links(bbl_t * bbl, lb_t * lb_in, colloids_info_t * cinfo) {
   int Nall[3];
   Nall[X]=nlocal[X]+2*nhalo;  Nall[Y]=nlocal[Y]+2*nhalo;  Nall[Z]=nlocal[Z]+2*nhalo;
   int nSites=Nall[X]*Nall[Y]*Nall[Z];
-  int nDist;
-  copyFromTarget(&nDist,&(lb_in->ndist),sizeof(int));
+  int nDist=lb_in->ndist;
   int nFields=NVEL*nDist;
 
   assert(bbl);
