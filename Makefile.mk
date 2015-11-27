febd3343d9ee8248bf78c4f22bc40c8e432bca60
Kevin Stratford <kevin@epcc.ed.ac.uk>
2015-11-27 09:18:10 +0000

    Issue 1394 branch update
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/branches/kevin-1407@2783 40876441-3496-0410-a613-a2fca35b2ae2

--- Makefile.mk
+++ Makefile.mk
@@ -2,8 +2,19 @@
 #
 #  Makefile.mk
 #
-#  Define here platform-dependent information.
-#  There are no targets.
+#  Please copy one configuration file from the ./config
+#  directory to this directory (top level Ludwig directory)
+#  and make any appropriate changes for your platform.
+#
+#  No changes should be required in this file itself.
+#
+#
+#  Edinburgh Soft Matter and Statistical Physics Group and
+#  Edinburgh Parallel Computing Centre
+#
+#  (c) 2015 The University of Edinburgh
+#  Contributing authors:
+#  Kevin Stratford (kevin@epcc.ed.ac.uk)
 #
 ##############################################################################
 
