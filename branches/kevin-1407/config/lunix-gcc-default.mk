ba0fb50584dcd2a154c480ff8899c368f3fee001
Kevin Stratford <kevin@epcc.ed.ac.uk>
2015-12-23 09:47:03 +0000

    Issue 1384 branch update
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2798 40876441-3496-0410-a613-a2fca35b2ae2

--- branches/kevin-1407/config/lunix-gcc-default.mk
+++ branches/kevin-1407/config/lunix-gcc-default.mk
@@ -1,17 +1,26 @@
 ##############################################################################
 #
-#  Makefile.mk
+#  lunix-gcc-default.mk
 #
-#  Define here platform-dependent information.
-#  There are no targets.
+#  A typical Unix-like system will use:
+#    gcc   Gnu C compiler
+#    mpicc Wrapper to the local MPI C compiler
+#
+#  Running the tests requires
+#     - an MPI launch command (often "mpirun")
+#     - the identity of the switch which controls the number of MPI tasks
+#     - a serial "launch command" (can be useful for platforms requiring
+#       cross-compiled)
+#       e.g., "aprun -n 1" on Cray systems. Leave blank if none is required.
 #
 ##############################################################################
 
 CC=gcc
 MPICC=mpicc
-CFLAGS=-O2
+CFLAGS=-O2 -Wall
 
-OPTS=-DNP_D3Q6
+AR = ar
+ARFLAGS = -cru
 
 LAUNCH_SERIAL_CMD=
 LAUNCH_MPI_CMD=mpirun
