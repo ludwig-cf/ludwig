1f621bd0b5f5b89aea1131ac9723097bee9b7e02
Alan Gray <alang@epcc.ed.ac.uk>
2016-01-05 16:15:25 +0000

    optimised, and added (optional) verbose performance reporting for kernel
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig@2802 40876441-3496-0410-a613-a2fca35b2ae2

--- trunk/src/blue_phase_beris_edwards.c
+++ trunk/src/blue_phase_beris_edwards.c
@@ -63,6 +63,7 @@
 #include "map_s.h"
 #include "timer.h"
 
+
 static int blue_phase_be_update(field_t * fq, field_grad_t * fq_grad, hydro_t * hydro, advflux_t * f,
 				map_t * map, noise_t * noise);
 
@@ -122,6 +123,96 @@ __targetHost__ int blue_phase_beris_edwards(field_t * fq,
   return 0;
 }
 
+/*IMPORTANT NOTE*/
+
+/* the below routine is a COPY of that in blue_phase.h */
+/* required to be in scope here for performance reasons on GPU */
+/* since otherwise the compiler places the emporary q[][][] etc arrays */
+/* in regular off-chip memory rather than registers */
+/* which has a huge impact on performance */
+/* TO DO: place this in a header file to be included both here and blue_phase.c */
+/* or work out how to get the compiler to inline it from the different source file */
+
+__targetHost__ __target__ void blue_phase_compute_h_inline(double q[3][3], 
+						    double dq[3][3][3],
+						    double dsq[3][3], 
+						    double h[3][3],
+						    bluePhaseKernelConstants_t* pbpc) {
+  int ia, ib, ic, id;
+
+  double q2;
+  double e2;
+  double eq;
+  double sum;
+
+  /* From the bulk terms in the free energy... */
+
+  q2 = 0.0;
+
+  for (ia = 0; ia < 3; ia++) {
+    for (ib = 0; ib < 3; ib++) {
+      q2 += q[ia][ib]*q[ia][ib];
+    }
+  }
+
+  for (ia = 0; ia < 3; ia++) {
+    for (ib = 0; ib < 3; ib++) {
+      sum = 0.0;
+      for (ic = 0; ic < 3; ic++) {
+  	sum += q[ia][ic]*q[ib][ic];
+      }
+      h[ia][ib] = -pbpc->a0_*(1.0 - pbpc->r3_*pbpc->gamma_)*q[ia][ib]
+  	+ pbpc->a0_*pbpc->gamma_*(sum - pbpc->r3_*q2*pbpc->d_[ia][ib]) - pbpc->a0_*pbpc->gamma_*q2*q[ia][ib];
+    }
+  }
+
+  /* From the gradient terms ... */
+  /* First, the sum e_abc d_b Q_ca. With two permutations, we
+   * may rewrite this as e_bca d_b Q_ca */
+
+  eq = 0.0;
+  for (ib = 0; ib < 3; ib++) {
+    for (ic = 0; ic < 3; ic++) {
+      for (ia = 0; ia < 3; ia++) {
+  	eq += pbpc->e_[ib][ic][ia]*dq[ib][ic][ia];
+      }
+    }
+  }
+
+  /* d_c Q_db written as d_c Q_bd etc */
+  for (ia = 0; ia < 3; ia++) {
+    for (ib = 0; ib < 3; ib++) {
+       sum = 0.0;
+       for (ic = 0; ic < 3; ic++) {
+	 for (id = 0; id < 3; id++) {
+	   sum += pbpc->e_[ia][ic][id]*dq[ic][ib][id] + pbpc->e_[ib][ic][id]*dq[ic][ia][id];
+	 }
+       }
+       
+       h[ia][ib] += pbpc->kappa0*dsq[ia][ib]
+      - 2.0*pbpc->kappa1*pbpc->q0*sum + 4.0*pbpc->r3_*pbpc->kappa1*pbpc->q0*eq*pbpc->d_[ia][ib]
+      - 4.0*pbpc->kappa1*pbpc->q0*pbpc->q0*q[ia][ib];
+      
+    }
+  }
+
+  /* Electric field term */
+
+  e2 = 0.0;
+  for (ia = 0; ia < 3; ia++) {
+    e2 += pbpc->e0[ia]*pbpc->e0[ia];
+  }
+
+  for (ia = 0; ia < 3; ia++) {
+    for (ib = 0; ib < 3; ib++) {
+      h[ia][ib] +=  pbpc->epsilon_*(pbpc->e0[ia]*pbpc->e0[ib] - pbpc->r3_*pbpc->d_[ia][ib]*e2);
+    }
+  }
+
+  return;
+}
+
+
 /*****************************************************************************
  *
  *  blue_phase_be_update
@@ -134,14 +225,18 @@ __targetHost__ int blue_phase_beris_edwards(field_t * fq,
  *
  *****************************************************************************/
 
-__targetEntry__ void blue_phase_be_update_lattice(field_t * t_q,
-						  field_grad_t * t_q_grad, 
-						  hydro_t * hydro,
-						  advflux_t * flux,
+__targetEntry__ void blue_phase_be_update_lattice(double* __restrict__ qdata,
+						  const double* __restrict__ graddata, 
+						  const double* __restrict__ graddelsq, 
+						  const double* __restrict__ hydrou,
+						  const double* __restrict__ fluxe,
+						  const double* __restrict__ fluxw,
+						  const double* __restrict__ fluxy,
+						  const double* __restrict__ fluxz,
 						  map_t * map,
 						  int noise_on,
 						  noise_t * noise,
-						  void* pcon,
+						  void* pcon, int nf, int hydroOn,
 void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 
   int index;
@@ -151,7 +246,6 @@ void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 
   int ia, ib, id;
   int indexj, indexk;
-  int nf;
   int status;
 
   double q[3][3];
@@ -171,8 +265,6 @@ void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 
   bluePhaseKernelConstants_t* pbpc= (bluePhaseKernelConstants_t*) pcon;
 
-  nf=t_q->nf;
-  
   int coords[3];
   targetCoords3D(coords,tc_Nall,index);
   
@@ -204,35 +296,35 @@ void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 
 	int ia, ib;
 	
-	q[X][X] = t_q->data[FLDADR(tc_nSites,NQAB,index,XX)];
-	q[X][Y] = t_q->data[FLDADR(tc_nSites,NQAB,index,XY)];
-	q[X][Z] = t_q->data[FLDADR(tc_nSites,NQAB,index,XZ)];
+	q[X][X] = qdata[FLDADR(tc_nSites,NQAB,index,XX)];
+	q[X][Y] = qdata[FLDADR(tc_nSites,NQAB,index,XY)];
+	q[X][Z] = qdata[FLDADR(tc_nSites,NQAB,index,XZ)];
 	q[Y][X] = q[X][Y];
-	q[Y][Y] = t_q->data[FLDADR(tc_nSites,NQAB,index,YY)];
-	q[Y][Z] = t_q->data[FLDADR(tc_nSites,NQAB,index,YZ)];
+	q[Y][Y] = qdata[FLDADR(tc_nSites,NQAB,index,YY)];
+	q[Y][Z] = qdata[FLDADR(tc_nSites,NQAB,index,YZ)];
 	q[Z][X] = q[X][Z];
 	q[Z][Y] = q[Y][Z];
 	q[Z][Z] = 0.0 - q[X][X] - q[Y][Y];
 	
 	for (ia = 0; ia < NVECTOR; ia++) {
-	  dq[ia][X][X] = t_q_grad->grad[FGRDADR(tc_nSites,NQAB,index,XX,ia)];
-	  dq[ia][X][Y] = t_q_grad->grad[FGRDADR(tc_nSites,NQAB,index,XY,ia)];
-	  dq[ia][X][Z] = t_q_grad->grad[FGRDADR(tc_nSites,NQAB,index,XZ,ia)];
+	  dq[ia][X][X] = graddata[FGRDADR(tc_nSites,NQAB,index,XX,ia)];
+	  dq[ia][X][Y] = graddata[FGRDADR(tc_nSites,NQAB,index,XY,ia)];
+	  dq[ia][X][Z] = graddata[FGRDADR(tc_nSites,NQAB,index,XZ,ia)];
 	  dq[ia][Y][X] = dq[ia][X][Y];
-	  dq[ia][Y][Y] = t_q_grad->grad[FGRDADR(tc_nSites,NQAB,index,YY,ia)];
-	  dq[ia][Y][Z] = t_q_grad->grad[FGRDADR(tc_nSites,NQAB,index,YZ,ia)];
+	  dq[ia][Y][Y] = graddata[FGRDADR(tc_nSites,NQAB,index,YY,ia)];
+	  dq[ia][Y][Z] = graddata[FGRDADR(tc_nSites,NQAB,index,YZ,ia)];
 	  dq[ia][Z][X] = dq[ia][X][Z];
 	  dq[ia][Z][Y] = dq[ia][Y][Z];
 	  dq[ia][Z][Z] = 0.0 - dq[ia][X][X] - dq[ia][Y][Y];
 	}
 	
 	
-	dsq[X][X] = t_q_grad->delsq[FLDADR(tc_nSites,NQAB,index,XX)];
-	dsq[X][Y] = t_q_grad->delsq[FLDADR(tc_nSites,NQAB,index,XY)];
-	dsq[X][Z] = t_q_grad->delsq[FLDADR(tc_nSites,NQAB,index,XZ)];
+	dsq[X][X] = graddelsq[FLDADR(tc_nSites,NQAB,index,XX)];
+	dsq[X][Y] = graddelsq[FLDADR(tc_nSites,NQAB,index,XY)];
+	dsq[X][Z] = graddelsq[FLDADR(tc_nSites,NQAB,index,XZ)];
 	dsq[Y][X] = dsq[X][Y];
-	dsq[Y][Y] = t_q_grad->delsq[FLDADR(tc_nSites,NQAB,index,YY)];
-	dsq[Y][Z] = t_q_grad->delsq[FLDADR(tc_nSites,NQAB,index,YZ)];
+	dsq[Y][Y] = graddelsq[FLDADR(tc_nSites,NQAB,index,YY)];
+	dsq[Y][Z] = graddelsq[FLDADR(tc_nSites,NQAB,index,YZ)];
 	dsq[Z][X] = dsq[X][Z];
 	dsq[Z][Y] = dsq[Y][Z];
 	dsq[Z][Z] = 0.0 - dsq[X][X] - dsq[Y][Y];
@@ -240,17 +332,17 @@ void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 	
 
 
-  if (isBPMF)
-	blue_phase_compute_h(q, dq, dsq, h, pbpc);
-  else
-    {
+	if (isBPMF)
+	  blue_phase_compute_h_inline(q, dq, dsq, h, pbpc);
+	else
+	{
 #ifndef __NVCC__
-      /*only BP supported for CUDA. This is caught earlier*/
-      molecular_field(index, h);
+	    /*only BP supported for CUDA. This is caught earlier*/
+	  molecular_field(index, h);
 #endif
-    }
+		  }
 	
-	if (hydro) {
+	if (hydroOn) {
 
 	  /* Velocity gradient tensor, symmetric and antisymmetric parts */
 
@@ -261,23 +353,23 @@ void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 	  int im1 = targetIndex3D(coords[X]-1,coords[Y],coords[Z],tc_Nall);
 	  int ip1 = targetIndex3D(coords[X]+1,coords[Y],coords[Z],tc_Nall);
 	  
-	  w[X][X] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,X)] - hydro->u[HYADR(tc_nSites,3,im1,X)]);
-	  w[Y][X] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,Y)] - hydro->u[HYADR(tc_nSites,3,im1,Y)]);
-	  w[Z][X] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,Z)] - hydro->u[HYADR(tc_nSites,3,im1,Z)]);
+	  w[X][X] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,X)] - hydrou[HYADR(tc_nSites,3,im1,X)]);
+	  w[Y][X] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,Y)] - hydrou[HYADR(tc_nSites,3,im1,Y)]);
+	  w[Z][X] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,Z)] - hydrou[HYADR(tc_nSites,3,im1,Z)]);
 	  
 	  im1 = targetIndex3D(coords[X],coords[Y]-1,coords[Z],tc_Nall);
 	  ip1 = targetIndex3D(coords[X],coords[Y]+1,coords[Z],tc_Nall);
 	  
-	  w[X][Y] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,X)] - hydro->u[HYADR(tc_nSites,3,im1,X)]);
-	  w[Y][Y] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,Y)] - hydro->u[HYADR(tc_nSites,3,im1,Y)]);
-	  w[Z][Y] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,Z)] - hydro->u[HYADR(tc_nSites,3,im1,Z)]);
+	  w[X][Y] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,X)] - hydrou[HYADR(tc_nSites,3,im1,X)]);
+	  w[Y][Y] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,Y)] - hydrou[HYADR(tc_nSites,3,im1,Y)]);
+	  w[Z][Y] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,Z)] - hydrou[HYADR(tc_nSites,3,im1,Z)]);
 	  
 	  im1 = targetIndex3D(coords[X],coords[Y],coords[Z]-1,tc_Nall);
 	  ip1 = targetIndex3D(coords[X],coords[Y],coords[Z]+1,tc_Nall);
 	  
-	  w[X][Z] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,X)] - hydro->u[HYADR(tc_nSites,3,im1,X)]);
-	  w[Y][Z] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,Y)] - hydro->u[HYADR(tc_nSites,3,im1,Y)]);
-	  w[Z][Z] = 0.5*(hydro->u[HYADR(tc_nSites,3,ip1,Z)] - hydro->u[HYADR(tc_nSites,3,im1,Z)]);
+	  w[X][Z] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,X)] - hydrou[HYADR(tc_nSites,3,im1,X)]);
+	  w[Y][Z] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,Y)] - hydrou[HYADR(tc_nSites,3,im1,Y)]);
+	  w[Z][Z] = 0.5*(hydrou[HYADR(tc_nSites,3,ip1,Z)] - hydrou[HYADR(tc_nSites,3,im1,Z)]);
 
 	  /* Enforce tracelessness */
 	  
@@ -313,9 +405,9 @@ void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 
     if (noise_on) {
 
-#ifdef __NVCC__ 
+#ifdef __NVCC__
       printf("Error: noise is not yet supported for CUDA\n");
-#else      
+#else
       noise_reap_n(noise, index, NQAB, chi);
       for (id = 0; id < NQAB; id++) {
 	chi[id] = tc_var*chi[id];
@@ -338,37 +430,37 @@ void   (*molecular_field)(const int, double h[3][3]), int isBPMF) {
 	indexk=targetIndex3D(coords[X],coords[Y],coords[Z]-1,tc_Nall);
 
 	q[X][X] += dt*(s[X][X] + tc_gamma*h[X][X] + chi_qab[X][X]
-		       - flux->fe[ADVADR(tc_nSites,nf,index,XX)] + flux->fw[ADVADR(tc_nSites,nf,index,XX)]
-		       - flux->fy[ADVADR(tc_nSites,nf,index,XX)] + flux->fy[ADVADR(tc_nSites,nf,indexj,XX)]
-		       - flux->fz[ADVADR(tc_nSites,nf,index,XX)] + flux->fz[ADVADR(tc_nSites,nf,indexk,XX)]);
+		       - fluxe[ADVADR(tc_nSites,nf,index,XX)] + fluxw[ADVADR(tc_nSites,nf,index,XX)]
+		       - fluxy[ADVADR(tc_nSites,nf,index,XX)] + fluxy[ADVADR(tc_nSites,nf,indexj,XX)]
+		       - fluxz[ADVADR(tc_nSites,nf,index,XX)] + fluxz[ADVADR(tc_nSites,nf,indexk,XX)]);
 
 
 	q[X][Y] += dt*(s[X][Y] + tc_gamma*h[X][Y] + chi_qab[X][Y]
-		       - flux->fe[ADVADR(tc_nSites,nf,index,XY)] + flux->fw[ADVADR(tc_nSites,nf,index,XY)]
-		       - flux->fy[ADVADR(tc_nSites,nf,index,XY)] + flux->fy[ADVADR(tc_nSites,nf,indexj,XY)]
-		       - flux->fz[ADVADR(tc_nSites,nf,index,XY)] + flux->fz[ADVADR(tc_nSites,nf,indexk,XY)]);
+		       - fluxe[ADVADR(tc_nSites,nf,index,XY)] + fluxw[ADVADR(tc_nSites,nf,index,XY)]
+		       - fluxy[ADVADR(tc_nSites,nf,index,XY)] + fluxy[ADVADR(tc_nSites,nf,indexj,XY)]
+		       - fluxz[ADVADR(tc_nSites,nf,index,XY)] + fluxz[ADVADR(tc_nSites,nf,indexk,XY)]);
 
 	q[X][Z] += dt*(s[X][Z] + tc_gamma*h[X][Z] + chi_qab[X][Z]
-		       - flux->fe[ADVADR(tc_nSites,nf,index,XZ)] + flux->fw[ADVADR(tc_nSites,nf,index,XZ)]
-		       - flux->fy[ADVADR(tc_nSites,nf,index,XZ)] + flux->fy[ADVADR(tc_nSites,nf,indexj,XZ)]
-		       - flux->fz[ADVADR(tc_nSites,nf,index,XZ)] + flux->fz[ADVADR(tc_nSites,nf,indexk,XZ)]);
+		       - fluxe[ADVADR(tc_nSites,nf,index,XZ)] + fluxw[ADVADR(tc_nSites,nf,index,XZ)]
+		       - fluxy[ADVADR(tc_nSites,nf,index,XZ)] + fluxy[ADVADR(tc_nSites,nf,indexj,XZ)]
+		       - fluxz[ADVADR(tc_nSites,nf,index,XZ)] + fluxz[ADVADR(tc_nSites,nf,indexk,XZ)]);
 
 	q[Y][Y] += dt*(s[Y][Y] + tc_gamma*h[Y][Y] + chi_qab[Y][Y]
-		       - flux->fe[ADVADR(tc_nSites,nf,index,YY)] + flux->fw[ADVADR(tc_nSites,nf,index,YY)]
-		       - flux->fy[ADVADR(tc_nSites,nf,index,YY)] + flux->fy[ADVADR(tc_nSites,nf,indexj,YY)]
-		       - flux->fz[ADVADR(tc_nSites,nf,index,YY)] + flux->fz[ADVADR(tc_nSites,nf,indexk,YY)]);
+		       - fluxe[ADVADR(tc_nSites,nf,index,YY)] + fluxw[ADVADR(tc_nSites,nf,index,YY)]
+		       - fluxy[ADVADR(tc_nSites,nf,index,YY)] + fluxy[ADVADR(tc_nSites,nf,indexj,YY)]
+		       - fluxz[ADVADR(tc_nSites,nf,index,YY)] + fluxz[ADVADR(tc_nSites,nf,indexk,YY)]);
 
 	q[Y][Z] += dt*(s[Y][Z] + tc_gamma*h[Y][Z] + chi_qab[Y][Z]
-		       - flux->fe[ADVADR(tc_nSites,nf,index,YZ)] + flux->fw[ADVADR(tc_nSites,nf,index,YZ)]
-		       - flux->fy[ADVADR(tc_nSites,nf,index,YZ)] + flux->fy[ADVADR(tc_nSites,nf,indexj,YZ)]
-		       - flux->fz[ADVADR(tc_nSites,nf,index,YZ)] + flux->fz[ADVADR(tc_nSites,nf,indexk,YZ)]);
+		       - fluxe[ADVADR(tc_nSites,nf,index,YZ)] + fluxw[ADVADR(tc_nSites,nf,index,YZ)]
+		       - fluxy[ADVADR(tc_nSites,nf,index,YZ)] + fluxy[ADVADR(tc_nSites,nf,indexj,YZ)]
+		       - fluxz[ADVADR(tc_nSites,nf,index,YZ)] + fluxz[ADVADR(tc_nSites,nf,indexk,YZ)]);
 
 
-	t_q->data[FLDADR(tc_nSites,NQAB,index,XX)] = q[X][X];
-	t_q->data[FLDADR(tc_nSites,NQAB,index,XY)] = q[X][Y];
-	t_q->data[FLDADR(tc_nSites,NQAB,index,XZ)] = q[X][Z];
-	t_q->data[FLDADR(tc_nSites,NQAB,index,YY)] = q[Y][Y];
-	t_q->data[FLDADR(tc_nSites,NQAB,index,YZ)] = q[Y][Z];
+	qdata[FLDADR(tc_nSites,NQAB,index,XX)] = q[X][X];
+	qdata[FLDADR(tc_nSites,NQAB,index,XY)] = q[X][Y];
+	qdata[FLDADR(tc_nSites,NQAB,index,XZ)] = q[X][Z];
+	qdata[FLDADR(tc_nSites,NQAB,index,YY)] = q[Y][Y];
+	qdata[FLDADR(tc_nSites,NQAB,index,YZ)] = q[Y][Z];
 
     }
   }
@@ -498,19 +590,62 @@ static int blue_phase_be_update(field_t * fq, field_grad_t * fq_grad, hydro_t * 
   copyToTarget(tmpptr,flux->fz,nf*nSites*sizeof(double));
 #endif
 
+  int hydroOn=0;
+  if (hydro)  hydroOn=1;
 
   /* launch update across lattice on target*/
 
+  double* hydrou;
+  double* qdata;
+  double* graddata;
+  double*  graddelsq;
+  double*  fluxe;
+  double*  fluxw;
+  double*  fluxy;
+  double*  fluxz;
+
+  copyFromTarget(&hydrou,&(hydro->tcopy->u),sizeof(double*)); 
+  copyFromTarget(&qdata,&(fq->tcopy->data),sizeof(double*)); 
+  copyFromTarget(&graddata,&(fq_grad->tcopy->grad),sizeof(double*)); 
+  copyFromTarget(&graddelsq,&(fq_grad->tcopy->delsq),sizeof(double*)); 
+  copyFromTarget(&fluxe,&(flux->tcopy->fe),sizeof(double*)); 
+  copyFromTarget(&fluxw,&(flux->tcopy->fw),sizeof(double*)); 
+  copyFromTarget(&fluxy,&(flux->tcopy->fy),sizeof(double*)); 
+  copyFromTarget(&fluxz,&(flux->tcopy->fz),sizeof(double*)); 
+
+
+#ifdef VERBOSE_PERF_REPORT
+  double t1, dataReadPerSite=0.,dataWrittenPerSite=0.;
+  t1=-MPI_Wtime();
+#endif
+
   TIMER_start(BP_BE_UPDATE_KERNEL);
 
-  blue_phase_be_update_lattice __targetLaunch__(nSites) (fq->tcopy, fq_grad->tcopy, 
-  							 t_hydro,
-  							 flux->tcopy, map,
+  blue_phase_be_update_lattice __targetLaunch__(nSites) (qdata, graddata, graddelsq,
+							 hydrou,
+  							 fluxe,fluxw,fluxy,fluxz, map,
   							 noise_on, noise, pcon,
-							 molecular_field, isBPMF);
+							  nf, hydroOn, molecular_field, isBPMF);
   
   targetSynchronize();
   TIMER_stop(BP_BE_UPDATE_KERNEL);
+
+#ifdef VERBOSE_PERF_REPORT
+  t1+=MPI_Wtime();
+
+  //dynamically allocated arrays
+  dataReadPerSite+=5*8;//fq
+  dataWrittenPerSite+=5*8;//fq
+  dataReadPerSite+=3*5*8;//grad 
+  dataReadPerSite+=5*8;//delsq 
+  dataReadPerSite+=4*5*8;//flux 
+  dataReadPerSite+=3*8;//u:
+
+  dataReadPerSite=dataReadPerSite/1024./1024./1024.; //convert to GB.
+  dataWrittenPerSite=dataWrittenPerSite/1024./1024./1024.; //convert to GB.
+
+  info("blue_phase_be_update: %1.3es, %3.3fGB/s read, %3.3fGB/s written, %3.3fGB/s total\n",t1,nSites*dataReadPerSite/t1,nSites*dataWrittenPerSite/t1,nSites*(dataReadPerSite+dataWrittenPerSite)/t1);
+#endif
 
 #ifndef KEEPFIELDONTARGET
   /* get result back from target*/
