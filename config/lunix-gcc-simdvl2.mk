750a5489caee9afa30732bb85a1b1642a948a946
Kevin Stratford <kevin@epcc.ed.ac.uk>
2016-01-29 12:39:18 +0000

    Add SIMD VL 2 config file
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@2838 40876441-3496-0410-a613-a2fca35b2ae2

---
+++ config/lunix-gcc-simdvl2.mk
@@ -0,0 +1,19 @@
+##############################################################################
+#
+#  lunix-gcc-simdvl2.mk
+#
+#  Sets SIMD vector length to 2. This may or may not be optimal
+#  for any given system. Intended for nightly tests.
+#
+##############################################################################
+
+CC=gcc
+MPICC=mpicc
+CFLAGS=-O3 -DNDEBUG -DVVL=2
+
+AR = ar
+ARFLAGS = -cru
+
+LAUNCH_SERIAL_CMD=
+LAUNCH_MPI_CMD=mpirun
+LAUNCH_MPI_NP_SWITCH=-np
