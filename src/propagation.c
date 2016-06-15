bc28d1841a943a5707676da03de299ac6fdd3ed2
Kevin Stratford <kevin@epcc.ed.ac.uk>
2016-06-15 17:18:19 +0000

    branch 1397 tested new vectorised propagation
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/branches/kevin-1397@2908 40876441-3496-0410-a613-a2fca35b2ae2

--- src/propagation.c
+++ src/propagation.c
@@ -2,12 +2,14 @@
  *
  *  propagation.c
  *
- *  Propagation schemes for the different models.
+ *  Propagation scheme for all models.
  *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
  *
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
+ *  Alan Gray (alang@epcc.ed.ac.uk)
+ *
  *  (c) 2010-2016 The University of Edinburgh
  *
  *****************************************************************************/
@@ -23,10 +25,6 @@
 #include "timer.h"
 
 __host__ int lb_propagation_driver(lb_t * lb);
-
-static int lb_propagate_d2q9(lb_t * lb);
-static int lb_propagate_d3q15(lb_t * lb);
-static int lb_propagate_d3q19(lb_t * lb);
 __host__ int lb_model_swapf(lb_t * lb);
 
 __global__ void lb_propagation_kernel(lb_t * lb);
@@ -44,336 +42,7 @@ __host__ int lb_propagation(lb_t * lb) {
 
   assert(lb);
 
-  if (NVEL ==  9) lb_propagate_d2q9(lb);
-  if (NVEL == 15) lb_propagate_d3q15(lb);
-  /* if (NVEL == 19) lb_propagate_d3q19(lb);*/
-
   lb_propagation_driver(lb);
-
-  return 0;
-}
-
-/*****************************************************************************
- *
- *  lb_propagate_d3q19
- *
- *  Follows the velocities defined in d3q19.c
- *
- *****************************************************************************/
-
-__target__  void lb_propagate_d3q19_site(const double* __restrict__ t_f, 
-					 double* __restrict__ t_fprime, 
-					 const int baseIndex){
-  
-  
-  
-
-  int coords[3];
-  
-  int i;
-  int n,p;
-  int iv=0;
-  
-  
-#if VVL == 1    
-  /*restrict operation to the interior lattice sites*/ 
-  targetCoords3D(coords,tc_Nall,baseIndex); 
-  if (coords[0] >= (tc_nhalo) && 
-      coords[1] >= (tc_nhalo) && 
-      coords[2] >= (tc_nhalo) &&
-      coords[0] < tc_Nall[X]-(tc_nhalo) &&  
-      coords[1] < tc_Nall[Y]-(tc_nhalo)  &&  
-      coords[2] < tc_Nall[Z]-(tc_nhalo) )
-#endif
-    
-    { 
-      
-      
-      
-      /* work out which sites in this chunk should be included */
-      int includeSite[VVL];
-      __targetILP__(iv) includeSite[iv]=0;
-      
-      int coordschunk[3][VVL];
-      
-      __targetILP__(iv){
-	for(i=0;i<3;i++){
-	  targetCoords3D(coords,tc_Nall,baseIndex+iv);
-	  coordschunk[i][iv]=coords[i];
-	}
-      }
-      
-      __targetILP__(iv){
-	
-	if ((coordschunk[0][iv] >= (tc_nhalo) &&
-	     coordschunk[1][iv] >= (tc_nhalo) &&
-	     coordschunk[2][iv] >= (tc_nhalo) &&
-	     coordschunk[0][iv] < tc_Nall[X]-(tc_nhalo) &&
-	     coordschunk[1][iv] < tc_Nall[Y]-(tc_nhalo)  &&
-	     coordschunk[2][iv] < tc_Nall[Z]-(tc_nhalo)))
-	  
-	  includeSite[iv]=1;
-      }
-      
-      
-      
-      int index1[VVL];
-      for (n = 0; n < tc_ndist; n++) {
-	
-	
-	for (p=0;p<NVEL;p++){
-	  
-	  
-	  /* get neighbour indices for this chunk */
-	  __targetILP__(iv) index1[iv] = 
-	    targetIndex3D(coordschunk[0][iv]-tc_cv[p][0],
-			  coordschunk[1][iv]-tc_cv[p][1],
-			  coordschunk[2][iv]-tc_cv[p][2],tc_Nall);
-	  
-	  
-	  /* perform propagation for to all non-halo sites */
-	  __targetILP__(iv){
-	    
-	    if(includeSite[iv])
-	      {
-		
-		t_fprime[LB_ADDR(tc_nSites, tc_ndist, NVEL,baseIndex+iv , n, p)] 
-		  = t_f[LB_ADDR(tc_nSites, tc_ndist, NVEL, index1[iv], n, p)];
-		
-	      }
-	    
-	  }
-	}
-	
-	
-	
-      }
-      
-      
-      
-    }
-  
-  
-  return;
-  
-  
-}
-
-__targetEntry__  void lb_propagate_d3q19_lattice(lb_t* t_lb) {
-
-  int baseIndex=0;
-
-  __targetTLP__(baseIndex,tc_nSites){
-    lb_propagate_d3q19_site (t_lb->f,t_lb->fprime,baseIndex);
-  }
-
-  return;
-}
-
-
-static __host__ int lb_propagate_d3q19(lb_t * lb) {
-
-  int nhalo;
-  int nlocal[3];
-
-  assert(lb);
-  assert(NVEL == 19);
-
-  nhalo = coords_nhalo();
-  coords_nlocal(nlocal);
-
-  int Nall[3];
-  Nall[X]=nlocal[X]+2*nhalo;  Nall[Y]=nlocal[Y]+2*nhalo;  Nall[Z]=nlocal[Z]+2*nhalo;
-
-  int nSites=Nall[X]*Nall[Y]*Nall[Z];
-
-  int nDist;
-  lb_ndist(lb, &nDist);
-
-  copyConstToTarget(&tc_nSites,&nSites, sizeof(int)); 
-  copyConstToTarget(&tc_ndist,&nDist, sizeof(int)); 
-  copyConstToTarget(&tc_nhalo,&nhalo, sizeof(int)); 
-  copyConstToTarget(tc_Nall,Nall, 3*sizeof(int)); 
-  copyConstToTarget(tc_cv,cv, NVEL*3*sizeof(int)); 
-
-  TIMER_start(TIMER_PROP_KERNEL);
-  lb_propagate_d3q19_lattice __targetLaunch__(nSites) (lb->target);
-  targetSynchronize();
-  TIMER_stop(TIMER_PROP_KERNEL);
-
-  lb_model_swapf(lb);
-
-  return 0;
-}
-
-/*****************************************************************************
- *
- *  lb_propagate_d2q9
- *
- *  General implementation.
- *
- *****************************************************************************/
-
-static int lb_propagate_d2q9(lb_t * lb) {
-
-  int ic, jc, kc, index, index1, n, p;
-  int xstr, ystr, zstr;
-  int nlocal[3];
-
-  assert(lb);
-  assert(NVEL == 9);
-
-  coords_nlocal(nlocal);
-
-  /* Stride in memory for velocities, and space */
-
-  p = lb->ndist*lb->nsite;
-  coords_strides(&xstr, &ystr, &zstr);
-  kc = 1;
-
-  for (n = 0; n < lb->ndist; n++) {
-
-    /* Distributions moving forward in memory. */
-  
-    for (ic = nlocal[X]; ic >= 1; ic--) {
-      for (jc = nlocal[Y]; jc >= 1; jc--) {
-
-	index = coords_index(ic, jc, kc);
-	index1 = coords_index(ic, jc - 1, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 4)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 4)];
-
-	index1 = coords_index(ic - 1, jc + 1, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 3)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 3)];
-
-	index1 = coords_index(ic - 1, jc, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 2)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 2)];
-
-	index1 = coords_index(ic - 1, jc - 1, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 1)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 1)];
-      }
-    }
-  
-    for (ic = 1; ic <= nlocal[X]; ic++) {
-      for (jc = 1; jc <= nlocal[Y]; jc++) {
-
-	index = coords_index(ic, jc, kc);
-	index1 = coords_index(ic, jc + 1, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 5)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 5)];
-
-	index1 = coords_index(ic + 1, jc - 1, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 6)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 6)];
-
-	index1 = coords_index(ic + 1, jc, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 7)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 7)];
-
-	index1 = coords_index(ic + 1, jc + 1, kc);
-	lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 8)] =
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 8)];
-      }
-    }
-  }
-
-  return 0;
-}
-
-/*****************************************************************************
- *
- *  lb_propagate_d3q15
- *
- *  General implementation
- *
- *****************************************************************************/
-
-static int lb_propagate_d3q15(lb_t * lb) {
-
-  int ic, jc, kc, index, index1, n, p;
-  int xstr, ystr, zstr;
-  int nlocal[3];
-
-  assert(lb);
-  assert(NVEL == 15);
-
-  coords_nlocal(nlocal);
-
-  /* Stride in memory for velocities, and space */
-
-  p = lb->ndist*lb->nsite;
-  coords_strides(&xstr, &ystr, &zstr);
-
-  for (n = 0; n < lb->ndist; n++) {
-
-    /* Distributions moving forward in memory. */
-  
-    for (ic = nlocal[X]; ic >= 1; ic--) {
-      for (jc = nlocal[Y]; jc >= 1; jc--) {
-	for (kc = nlocal[Z]; kc >= 1; kc--) {
-
-	  index = coords_index(ic, jc, kc);
-	  index1 = coords_index(ic, jc, kc - 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 7)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 7)];
-	  index1 = coords_index(ic, jc - 1, kc);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 6)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 6)];
-	  index1 = coords_index(ic - 1, jc + 1, kc + 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 5)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 5)];
-	  index1 = coords_index(ic - 1, jc + 1, kc - 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 4)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 4)];
-	  index1 = coords_index(ic - 1, jc, kc);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 3)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 3)];
-	  index1 = coords_index(ic - 1, jc - 1, kc + 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 2)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 2)];
-	  index1 = coords_index(ic - 1, jc - 1, kc - 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 1)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 1)];
-	}
-      }
-    }
-
-    /* Distributions mvoing backward in memory. */
-  
-    for (ic = 1; ic <= nlocal[X]; ic++) {
-      for (jc = 1; jc <= nlocal[Y]; jc++) {
-	for (kc = 1; kc <= nlocal[Z]; kc++) {
-
-	  index = coords_index(ic, jc, kc);
-
-	  index1 = coords_index(ic, jc, kc + 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 8)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 8)];
-	  index1 = coords_index(ic, jc + 1, kc);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 9)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 9)];
-	  index1 = coords_index(ic + 1, jc - 1, kc - 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 10)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 10)];
-	  index1 = coords_index(ic + 1, jc - 1, kc + 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 11)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 11)];
-	  index1 = coords_index(ic + 1, jc, kc);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 12)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 12)];
-	  index1 = coords_index(ic + 1, jc + 1, kc - 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 13)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 13)];
-	  index1 = coords_index(ic + 1, jc + 1, kc + 1);
-	  lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index, n, 14)] =
-	    lb->f[LB_ADDR(lb->nsite, lb->ndist, NVEL, index1, n, 14)];
-	}
-      }
-    }
-  }
 
   return 0;
 }
@@ -390,7 +59,6 @@ __host__ int lb_propagation_driver(lb_t * lb) {
   dim3 nblk, ntpb;
   kernel_info_t limits;
   kernel_ctxt_t * ctxt = NULL;
-  __host__ int lb_model_swapf(lb_t * lb);
 
   assert(lb);
 
@@ -402,14 +70,18 @@ __host__ int lb_propagation_driver(lb_t * lb) {
   limits.jmin = 1; limits.jmax = nlocal[Y];
   limits.kmin = 1; limits.kmax = nlocal[Z];
 
-  /* Encapsulate. lb_kernel_commit(lb); */
+  /* SHIT Encapsulate or remove requirement. */
   copyConstToTarget(tc_cv, cv, NVEL*3*sizeof(int)); 
 
   kernel_ctxt_create(NSIMDVL, limits, &ctxt);
   kernel_ctxt_launch_param(ctxt, &nblk, &ntpb);
 
+  TIMER_start(TIMER_PROP_KERNEL);
+
   __host_launch_kernel(lb_propagation_kernel, nblk, ntpb, lb->target);
   targetDeviceSynchronise();
+
+  TIMER_stop(TIMER_PROP_KERNEL);
 
   kernel_ctxt_free(ctxt);
 
@@ -422,7 +94,7 @@ __host__ int lb_propagation_driver(lb_t * lb) {
  *
  *  lb_propagation_kernel_novector
  *
- *  Non-vectorised version, just for testing.
+ *  Non-vectorised version, e.g., for testing.
  *
  *****************************************************************************/
 
@@ -469,7 +141,7 @@ __global__ void lb_propagation_kernel_novector(lb_t * lb) {
  *
  *  lb_propagation_kernel
  *
- *  Ultimately an optimised version.
+ *  A vectorised version.
  *
  *****************************************************************************/
 
