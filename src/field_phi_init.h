6723d91c5ef104f43cc85b2300f2239225c299dd
Oliver Henrich <oliver.henrich@strath.ac.uk>
2017-04-18 17:10:29 +0000

    Added various initialisations for binary OP
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/ludwig/trunk@3113 40876441-3496-0410-a613-a2fca35b2ae2

--- src/field_phi_init.h
+++ src/field_phi_init.h
@@ -25,12 +25,20 @@ struct field_phi_info_s {
   double phistar;         /* An amplitude (min/max order parameter) */
 };
 
+int field_phi_init_drop(double xi, double radius, double phistar,
+	field_t * phi);
+int field_phi_init_block(double xi, field_t * phi);
+int field_phi_init_block_X(double xi, field_t * phi, double block_dimension);
+int field_phi_init_block_Y(double xi, field_t * phi, double block_dimension);
+int field_phi_init_block_Z(double xi, field_t * phi, double block_dimension);
+int field_phi_init_layer_X(double xi, field_t * phi, double layer_size);
+int field_phi_init_layer_Y(double xi, field_t * phi, double layer_size);
+int field_phi_init_layer_Z(double xi, field_t * phi, double layer_size);
 int field_phi_init_bath(field_t * phi);
 int field_phi_init_spinodal(int seed, double phi0, double amp, field_t * phi);
 int field_phi_init_spinodal_patches(int seed, int patch, double volm1,
-				    field_t * phi);
-int field_phi_init_block(double xi, field_t * phi);
-int field_phi_init_drop(double xi, double radius, double phistar,
-			field_t * phi);
+	field_t * phi);
+int field_phi_init_emulsion(double xi, double radius, double phistar, 
+	int Ndrops, double d_centre, field_t * phi);
 
 #endif
