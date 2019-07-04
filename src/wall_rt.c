ff43a5617cb3ec51fbc6638277745819345245b0
Scolymus <lucasaction@gmail.com>
2019-07-04 16:05:56 +0200

    Minimum distance of particle to wall
    I added two parameters that have to be in the input:
    boundary_distance_floor
    boundary_distance_ceil
    These are the minimum distances particles will be to wall. It works with all the possible solid walls in Ludwig.

--- src/wall_rt.c
+++ src/wall_rt.c
@@ -27,6 +27,7 @@ int wall_rt_init(pe_t * pe, cs_t * cs, rt_t * rt, lb_t * lb, map_t * map,
 
   double ux_bot = 0.0;
   double ux_top = 0.0;
+  int vector[3]; 
   wall_param_t p = {0};
 
   assert(rt);
@@ -57,6 +58,16 @@ int wall_rt_init(pe_t * pe, cs_t * cs, rt_t * rt, lb_t * lb, map_t * map,
     rt_double_parameter(rt, "boundary_lubrication_rcnormal", &p.lubr_rc[X]);
     p.lubr_rc[Y] = p.lubr_rc[X];
     p.lubr_rc[Z] = p.lubr_rc[X];
+	//Distance to colloid -->
+    rt_double_parameter(rt, "boundary_distance_floor", &p.limit_colloid_floor[X]);
+    p.limit_colloid_floor[Y] = p.limit_colloid_floor[X];
+    p.limit_colloid_floor[Z] = p.limit_colloid_floor[X];
+    rt_double_parameter(rt, "boundary_distance_ceil", &p.limit_colloid_ceil[X]);
+    rt_int_parameter_vector(rt, "size", vector);
+    p.limit_colloid_ceil[Z] = vector[Z]-p.limit_colloid_ceil[X];
+    p.limit_colloid_ceil[Y] = vector[Y]-p.limit_colloid_ceil[X];
+    p.limit_colloid_ceil[X] = vector[X]-p.limit_colloid_ceil[X];
+    //<-- Distance to colloid
   }
 
   /* Allocate */
