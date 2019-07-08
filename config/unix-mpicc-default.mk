38f585ef9c7ee973efc8158eae33561b709590d2
Kevin Stratford <kevin@epcc.ed.ac.uk>
2019-07-08 15:30:25 +0100

    Add mpi default example

---
+++ config/unix-mpicc-default.mk
@@ -0,0 +1,24 @@
+##############################################################################
+#
+#  unix-mpicc-default.mk
+#
+#  Compile for parallel execution assuming an mpi compiler
+#  wrapper "mpicc" is available.
+#
+##############################################################################
+
+BUILD   = parallel
+MODEL   = -D_D3Q19_
+
+CC      = gcc
+MPICC   = mpicc
+CFLAGS  = -O -g
+
+
+MPI_INC_PATH      =
+MPI_LIB_PATH      =
+MPI_LIB           =
+
+LAUNCH_SERIAL_CMD =
+LAUNCH_MPIRUN_CMD = mpirun
+MPIRUN_NTASK_FLAG = -np
