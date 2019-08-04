9ac94bedd2e1a9ea264387bb17f8c9baa7d47366
Kevin Stratford <kevin@epcc.ed.ac.uk>
2019-08-04 19:25:31 +0100

    Add non LE versions 0-3rd order

--- src/advection.h
+++ src/advection.h
@@ -2,13 +2,12 @@
  *
  *  advection.h
  *
- *  $Id: advection.h,v 1.2 2010-10-15 12:40:02 kevin Exp $
- *
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
  *
+ *  (c) 2010-2019 The University of Edinburgh
+ *  Contributing authors:
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
- *  (c) 2010-2017 The University of Edinburgh
  *
  *****************************************************************************/
 
@@ -29,6 +28,10 @@ __host__ int advflux_le_create(pe_t * pe, cs_t * cs, lees_edw_t * le, int nf,
 			       advflux_t ** pobj);
 __host__ int advflux_free(advflux_t * obj);
 __host__ int advflux_memcpy(advflux_t * obj);
+
+
+__host__ int advflux_cs_compute(advflux_t * flux, hydro_t * h, field_t * f);
+__host__ int advflux_cs_zero(advflux_t * flux);
 
 __host__ int advection_x(advflux_t * obj, hydro_t * hydro, field_t * field);
 __host__ int advective_fluxes_d3qx(hydro_t * hydro, int nf, double * f, 
--- src/advection_s.h
+++ src/advection_s.h
@@ -5,7 +5,7 @@
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
  *
- *  (c) 2012-2017 The University of Edinburgh
+ *  (c) 2012-2019 The University of Edinburgh
  *
  *  Contributing authors:
  *  Kevin Stratford (kevin@epcc.ed.ac.uk)
@@ -23,10 +23,11 @@ struct advflux_s {
 
   int nf;        /* Number of fields (1 for scalar etc) */
   int nsite;     /* Number of sites allocated */
-  double * fe;   /* East face flxues */
-  double * fw;   /* West face flxues */
-  double * fy;   /* y-face fluxes */
-  double * fz;   /* z-face fluxes */
+  double * fe;   /* East face flxues (Lees-Edwards)   */
+  double * fw;   /* West face flxues (Less-Edwards)   */
+  double * fx;   /* x-face fluxes (between i and i+1) */
+  double * fy;   /* y-face fluxes (between j and j+1) */
+  double * fz;   /* z-face fluxes (between k and k+1) */
 
   pe_t * pe;           /* Parallel environment */
   cs_t * cs;           /* Coordinate system */
