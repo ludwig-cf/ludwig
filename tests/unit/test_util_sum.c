acedb2b35588a1bd946e174aac24675f50648afa
Kevin Stratford <kevin@epcc.ed.ac.uk>
2021-06-21 14:32:44 +0100

    Add kahan sum tests

--- tests/unit/test_util_sum.c
+++ tests/unit/test_util_sum.c
@@ -19,6 +19,12 @@
 #include "util_sum.h"
 #include "tests.h"
 
+int test_kahan_zero(pe_t * pe);
+int test_kahan_sum(pe_t * pe);
+int test_kahan_add(pe_t * pe);
+int test_kahan_mpi_datatype(pe_t * pe);
+int test_kahan_mpi_op_sum(pe_t * pe);
+
 int test_klein_zero(pe_t * pe);
 int test_klein_sum(pe_t * pe);
 int test_klein_add(pe_t * pe);
@@ -38,6 +44,12 @@ int test_util_sum_suite(void) {
 
   pe_create(MPI_COMM_WORLD, PE_QUIET, &pe);
 
+  test_kahan_zero(pe);
+  test_kahan_sum(pe);
+  test_kahan_add(pe);
+  test_kahan_mpi_datatype(pe);
+  test_kahan_mpi_op_sum(pe);
+
   test_klein_zero(pe);
   test_klein_sum(pe);
   test_klein_add(pe);
@@ -46,6 +58,25 @@ int test_util_sum_suite(void) {
 
   pe_info(pe, "PASS     ./unit/test_util_sum\n");
   pe_free(pe);
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  test_kahan_zero
+ *
+ *****************************************************************************/
+
+int test_kahan_zero(pe_t * pe) {
+
+  kahan_t k = kahan_zero();
+
+  assert(pe);
+
+  assert(k.lock == 0);
+  assert(k.sum  == 0.0);
+  assert(k.cs   == 0.0);
 
   return 0;
 }
@@ -68,6 +99,147 @@ int test_klein_zero(pe_t * pe) {
   assert(k.ccs == 0.0);
 
   return k.lock;
+}
+
+/*****************************************************************************
+ *
+ *  test_kahan_sum
+ *
+ *****************************************************************************/
+
+int test_kahan_sum(pe_t * pe) {
+
+  kahan_t k = kahan_zero();
+
+  assert(pe);
+
+  {
+    k.sum = 1.0;
+    k.cs  = 2.0;
+    assert(fabs(kahan_sum(&k) - 3.0) < DBL_EPSILON);
+  }
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  test_kahan_add
+ *
+ *****************************************************************************/
+
+int test_kahan_add(pe_t * pe) {
+
+  kahan_t k = kahan_zero();
+  double a = 1.0;
+  double b = 1.0e-17;
+
+  assert(pe);
+
+  /* Note: here must be add a then add b; not add b then add a */
+  kahan_add(&k, a);
+  kahan_add(&k, b);
+  assert(fabs(kahan_sum(&k) - a) < DBL_EPSILON);
+
+  kahan_add(&k, -b);
+  kahan_add(&k, -a);
+  assert(kahan_sum(&k) == 0.0);
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  test_kahan_mpi_datatype
+ *
+ *****************************************************************************/
+
+int test_kahan_mpi_datatype(pe_t * pe) {
+
+  MPI_Datatype dt = MPI_DATATYPE_NULL;
+
+  assert(pe);
+
+  kahan_mpi_datatype(&dt);
+
+  assert(dt != MPI_DATATYPE_NULL);
+
+  {
+    MPI_Aint lb = -1;
+    MPI_Aint extent = -1;
+
+    MPI_Type_get_extent(dt, &lb, &extent);
+    assert(lb == 0);
+    assert(extent == sizeof(kahan_t));
+  }
+
+  {
+    /* Bcast as example */
+    kahan_t k = kahan_zero();
+    MPI_Comm comm = MPI_COMM_NULL;
+    int rank = -1;
+
+    pe_mpi_comm(pe, &comm);
+    MPI_Comm_rank(comm, &rank);
+    if (rank == 0) kahan_add(&k, 1.0);
+    MPI_Bcast(&k, 1, dt, 0, comm);
+    assert(fabs(kahan_sum(&k) - 1.0) < DBL_EPSILON);
+  }
+
+  MPI_Type_free(&dt);
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  test_kahan_mpi_op_sum
+ *
+ *****************************************************************************/
+
+int test_kahan_mpi_op_sum(pe_t * pe) {
+
+  MPI_Datatype dt = MPI_DATATYPE_NULL;
+  MPI_Op op = MPI_OP_NULL;
+
+  assert(pe);
+
+  kahan_mpi_datatype(&dt);
+  kahan_mpi_op_sum(&op);
+  assert(op != MPI_OP_NULL);
+
+  {
+    /* internal smoke test */
+    void kahan_mpi_op_sum_function(kahan_t * invec, kahan_t * inoutvec,
+				   int * len, MPI_Datatype * dt);
+
+    kahan_t invec = kahan_zero();
+    kahan_t inoutvec = kahan_zero();
+    int count = 1;
+
+    kahan_mpi_op_sum_function(&invec, &inoutvec, &count, &dt);
+  }
+
+  {
+    /* Allreduce as example */
+    kahan_t send = kahan_zero();
+    kahan_t recv = kahan_zero();
+    MPI_Comm comm = MPI_COMM_NULL;
+    int rank = -1;
+
+    pe_mpi_comm(pe, &comm);
+    MPI_Comm_rank(comm, &rank);
+
+    if (rank == 0) kahan_add(&send, 1.0);
+
+    MPI_Allreduce(&send, &recv, 1, dt, op, comm);
+    assert(fabs(kahan_sum(&recv) - 1.0) < DBL_EPSILON);
+  }
+
+  MPI_Op_free(&op);
+  MPI_Type_free(&dt);
+
+  return 0;
 }
 
 /*****************************************************************************
