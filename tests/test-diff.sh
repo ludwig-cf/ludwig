e949574da625697bce93346900b75b2422b1a06d
Kevin Stratford <kevin@epcc.ed.ac.uk>
2021-08-19 15:17:36 +0100

    Allow GPU test to pass from CPU result

--- tests/test-diff.sh
+++ tests/test-diff.sh
@@ -85,6 +85,7 @@ sed -i~ '/Compiler:/d' test-diff-tmp.ref
 sed -i~ '/..name:/d' test-diff-tmp.ref
 sed -i~ '/..version-string:/d' test-diff-tmp.ref
 sed -i~ '/Target thread model:/d' test-diff-tmp.ref
+sed -i~ '/Default threads per block/d' test-diff-tmp.ref
 sed -i~ '/OpenMP/d' test-diff-tmp.ref
 sed -i~ '/^$/d' test-diff-tmp.ref
 sed -i~ '/Timer/d' test-diff-tmp.ref
@@ -104,6 +105,7 @@ sed -i~ '/Compiler:/d' test-diff-tmp.log
 sed -i~ '/..name:/d' test-diff-tmp.log
 sed -i~ '/..version-string:/d' test-diff-tmp.log
 sed -i~ '/Target thread model:/d' test-diff-tmp.log
+sed -i~ '/Default threads per block/d' test-diff-tmp.log
 sed -i~ '/OpenMP/d' test-diff-tmp.log
 sed -i~ '/SVN.revision/d' test-diff-tmp.log
 sed -i~ '/^$/d' test-diff-tmp.log
