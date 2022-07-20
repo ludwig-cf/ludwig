34fc35da58ee21f3ed5af38b41043d4f3bf71195
Kevin Stratford <kevin@epcc.ed.ac.uk>
2022-07-20 08:41:56 +0100

    MPI install

--- config/build-mpi.sh
+++ config/build-mpi.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -e
+
 printf "MPI installation script at %s\n" "$(date)"
+ls
 
 my_prefix=$(pwd)/mpi
 
@@ -8,10 +11,10 @@ if [ -d mpi ]; then
     export PATH=${my_prefix}/bin:${PATH}
     printf "Added existing local mpi to path:\n"
     printf "PATH is %s\n" "${PATH}"
+    which mpicc
 else
-    printf "%s\n" "Install MPI to prefix %s\n" "${my_prefix}"
-    wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.4.tar.\
-gz
+    printf "%s %s\n" "Install MPI to prefix" "${my_prefix}"
+    wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.4.tar.gz
     tar xf openmpi-4.1.4.tar.gz
     cd openmpi-4.1.4
     ./configure CC=gcc-11 CXX=g++-11    \
