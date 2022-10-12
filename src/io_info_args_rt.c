3b96c66fee407691e49342428a36ac4726df1822
Kevin Stratford <kevin@epcc.ed.ac.uk>
2022-10-12 15:57:14 +0100

    Bring into use

--- src/io_info_args_rt.c
+++ src/io_info_args_rt.c
@@ -6,7 +6,7 @@
  *  Edinburgh Soft Matter and Statistical Physics Group and
  *  Edinburgh Parallel Computing Centre
  *
- *  (c) 2020 The University of Edinburgh
+ *  (c) 2020-2022 The University of Edinburgh
  *
  *  Contribuiting authors:
  *  Kevin Stratford
@@ -16,7 +16,6 @@
 #include <assert.h>
 #include <string.h>
 
-#include "pe.h"
 #include "runtime.h"
 #include "io_info_args_rt.h"
 
@@ -29,43 +28,90 @@
  *
  *****************************************************************************/
 
-__host__ int io_info_args_rt(pe_t * pe, rt_t * rt, const char * stub,
+__host__ int io_info_args_rt(rt_t * rt, rt_enum_t lv, const char * stub,
 			     io_info_rw_enum_t rw, io_info_args_t * args) {
 
-  int iogrid[3] = {0};
-  int sz = 3*sizeof(int);
-  char stubi[BUFSIZ] = {0};
-  char stubo[BUFSIZ] = {0};
+  assert(rt);
+  assert(stub);
+  assert(args);
+  assert(strlen(stub) < BUFSIZ/2);
 
-  assert(pe);
+  /* User defaults */
+  /* Specific options for this info type */
+
+  if (rw == IO_INFO_READ_WRITE || rw == IO_INFO_READ_ONLY) {
+    io_info_args_rt_input(rt, lv, stub, args);
+  }
+
+  if (rw == IO_INFO_READ_WRITE || rw == IO_INFO_WRITE_ONLY) {
+    io_info_args_rt_output(rt, lv, stub, args);
+  }
+
+  /* Now the io grid (shared between input and output) */
+
+  {
+    char key[BUFSIZ] = {0};
+
+    sprintf(key, "%s_io_grid", stub);
+    io_info_args_rt_iogrid(rt, lv, "default_io_grid", args->grid);
+    io_info_args_rt_iogrid(rt, lv, key, args->grid);
+  }
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  io_info_args_rt_input
+ *
+ *****************************************************************************/
+
+__host__ int io_info_args_rt_input(rt_t * rt, rt_enum_t lv, const char * stub,
+				   io_info_args_t * args) {
+
+  char stub_input[BUFSIZ] = {0};
+
   assert(rt);
   assert(stub);
   assert(args);
 
   assert(strlen(stub) < BUFSIZ/2);
-  assert(0); /* io_options_rt() must not overwrite args */
 
-  sprintf(stubi, "%s_input", stub);
-  sprintf(stubo, "%s_output", stub);
+  sprintf(stub_input, "%s_input", stub);
 
-  /* User defaults */
-  /* Specific options for this info type */
+  /* This is in order of increasing precedence ... */
+  io_options_rt(rt, lv, "default",       &args->input);
+  io_options_rt(rt, lv, "default_input", &args->input);
+  io_options_rt(rt, lv, stub,            &args->input);
+  io_options_rt(rt, lv, stub_input,      &args->input);
 
-  io_options_rt(pe, rt, "default",        &args->input);
-  io_options_rt(pe, rt, "default",        &args->output);
-  io_options_rt(pe, rt, "default_input",  &args->input);
-  io_options_rt(pe, rt, "default_output", &args->output);
-  io_options_rt(pe, rt, stubi,            &args->input);
-  io_options_rt(pe, rt, stubo,            &args->output);
+  return 0;
+}
 
-  /* Now the io grid (shared between input and output) */
+/*****************************************************************************
+ *
+ *  io_info_args_rt_output
+ *
+ *****************************************************************************/
 
-  io_info_args_rt_iogrid(pe, rt, "default_io_grid", iogrid);
-  if (io_info_args_iogrid_valid(iogrid)) memcpy(args->grid, iogrid, sz);
+__host__ int io_info_args_rt_output(rt_t * rt, rt_enum_t lv, const char * stub,
+				    io_info_args_t * args) {
 
-  sprintf(stubi, "%s_io_grid", stub);
-  io_info_args_rt_iogrid(pe, rt, stubi, iogrid);
-  if (io_info_args_iogrid_valid(iogrid)) memcpy(args->grid, iogrid, sz);
+  char stub_output[BUFSIZ] = {0};
+
+  assert(rt);
+  assert(stub);
+  assert(args);
+
+  assert(strlen(stub) < BUFSIZ/2);
+
+  sprintf(stub_output, "%s_output", stub);
+
+  /* In order of increasing precedence .. */
+  io_options_rt(rt, lv, "default",        &args->output);
+  io_options_rt(rt, lv, "default_output", &args->output);
+  io_options_rt(rt, lv, stub,             &args->output);
+  io_options_rt(rt, lv, stub_output,      &args->output);
 
   return 0;
 }
@@ -74,33 +120,36 @@ __host__ int io_info_args_rt(pe_t * pe, rt_t * rt, const char * stub,
  *
  *  io_info_args_rt_iogrid
  *
- *  Could return an error flag here if an invalid grid.
- *
  *****************************************************************************/
 
-__host__ int io_info_args_rt_iogrid(pe_t * pe, rt_t * rt, const char * key,
-			            int iogrid[3]) {
+__host__ int io_info_args_rt_iogrid(rt_t * rt, rt_enum_t lv, const char * key,
+				    int grid[3]) {
 
   int key_present = 0;
-  int user_grid[3] = {0};
+  int iogrid[3] = {0};   /* invalid */
+  int ifail = -1;        /* Return -1 for no key; +1 for invalid key; 0 ok */
 
-  assert(pe);
   assert(rt);
   assert(key);
 
-  key_present = rt_int_parameter_vector(rt, key, user_grid);
+  key_present = rt_int_parameter_vector(rt, key, iogrid);
 
-  if (key_present && io_info_args_iogrid_valid(user_grid) == 0) {
-    pe_info(pe, "I/O grid key present but invalid\n");
-    pe_info(pe, "key: %s\n", key);
-    pe_info(pe, "value: %d %d %d\n", user_grid[0], user_grid[1], user_grid[2]);
-    pe_info(pe, "Must be greater than zero in each dimension\n");
-    pe_fatal(pe, "Please check the input file and try again!\n");
+  if (key_present) {
+    if (io_info_args_iogrid_valid(iogrid) == 1) {
+      grid[0] = iogrid[0];
+      grid[1] = iogrid[1];
+      grid[2] = iogrid[2];
+      ifail = 0;
+    }
+    else {
+      rt_vinfo(rt, lv, "I/O grid key present but invalid\n");
+      rt_vinfo(rt, lv, "key: %s\n", key);
+      rt_vinfo(rt, lv, "value: %d %d %d\n", iogrid[0], iogrid[1], iogrid[2]);
+      rt_vinfo(rt, lv, "Must be greater than zero in each dimension\n");
+      rt_fatal(rt, lv, "Please check the input file and try again!\n");
+      ifail = +1;
+    }
   }
 
-  iogrid[0] = user_grid[0];
-  iogrid[1] = user_grid[1];
-  iogrid[2] = user_grid[2];
-
-  return 0;
+  return ifail;
 }
