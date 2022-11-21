329dfa0e7f720b480870a1ca4fbfcfc4d0d0f3e8
Kevin Stratford <kevin@epcc.ed.ac.uk>
2022-11-21 17:46:36 +0000

    Add return code

--- src/io_impl.c
+++ src/io_impl.c
@@ -14,24 +14,37 @@
  *
  *****************************************************************************/
 
+#include <assert.h>
+
 #include "io_impl.h"
 #include "io_impl_mpio.h"
 
 int io_impl_create(const io_metadata_t * metadata, io_impl_t ** io) {
 
+  int ifail = 0;
+
+  assert(io);
+
   *io = NULL;
 
   switch (metadata->options.mode) {
+  case IO_MODE_SINGLE:
+    ifail = -1;
+    break;
+  case IO_MODE_MULTIPLE:
+    ifail = -1;
+    break;
   case IO_MODE_MPIIO:
     {
       io_impl_mpio_t * mpio = NULL;
-      io_impl_mpio_create(metadata, &mpio);
+      ifail = io_impl_mpio_create(metadata, &mpio);
       *io = (io_impl_t *) mpio;
     }
     break;
   default:
-    ;
+    /* Internal error */
+    assert(0);
   }
 
-  return 0;
+  return ifail;
 }
