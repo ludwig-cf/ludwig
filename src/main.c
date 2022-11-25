ea82eff6f30f3942b47194e0032f841a0c6f7bc5
Kevin Stratford <kevin@epcc.ed.ac.uk>
2022-11-25 16:04:25 +0000

    Pending one CodeQL alert

--- src/main.c
+++ src/main.c
@@ -13,17 +13,13 @@
  *
  *****************************************************************************/
 
-#include <ctype.h>
 #include <stdio.h>
-#include <string.h>
 
 #include "pe.h"
 #include "ludwig.h"
 #ifdef PETSC
   #include "petscksp.h"
 #endif
-
-int process_command_line(const char * arg);
 
 /*****************************************************************************
  *
@@ -33,19 +29,16 @@ int process_command_line(const char * arg);
 
 int main(int argc, char ** argv) {
 
+  char inputfile[FILENAME_MAX] = "input";
   int provided = MPI_THREAD_SINGLE;
 
   MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
 #ifdef PETSC
   PetscInitialize(&argc, &argv, (char*) 0, NULL); 
 #endif 
+  if (argc > 1) snprintf(inputfile, FILENAME_MAX, "%s", argv[1]);
 
-  if (argc == 1) {
-    ludwig_run("input");
-  }
-  else if (argc > 1 && process_command_line(argv[1]) == 0) {
-    ludwig_run(argv[1]);
-  }
+  ludwig_run(inputfile);
 
 #ifdef PETSC
   PetscFinalize();
@@ -53,38 +46,4 @@ int main(int argc, char ** argv) {
   MPI_Finalize();
 
   return 0;
-}
-
-/*****************************************************************************
- *
- *  process_command_line
- *
- *  Require a posix portable filename, with the additional condition
- *  that the first character is alphabetical.
- *
- *  A valid filename will return zero.
- *
- *****************************************************************************/
-
-int process_command_line(const char * arg) {
-
-  int ifail = -1;
-
-  /* The first character should be alphabetical */ 
-
-  if (isalpha(arg[0])) {
-    for (size_t i = 0; i < strnlen(arg, FILENAME_MAX); i++) {
-      const char c = arg[i];
-      ifail = -1;
-      if (isalnum(c) || c == '_' || c == '-' || c == '.') ifail = 0;
-    }
-  }
-
-  if (ifail != 0) {
-    printf("Input file name: %s\n"
-	   "Please use a posix file name with only alphanumeric\n"
-	   "characters or _ or - or .\n", arg);
-  }
-
-  return ifail;
 }
