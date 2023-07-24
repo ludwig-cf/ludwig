f129629278f55205f15f69c41b2a279eb08ccaf3
Kevin Stratford <kevin@cirrus.epcc.ed.ac.uk>
2023-07-24 20:42:38 +0100

    Add var epsilon stub test

--- tests/unit/test_psi_solver_petsc.c
+++ tests/unit/test_psi_solver_petsc.c
@@ -20,6 +20,7 @@
 #include "psi_petsc.h"
 
 int test_psi_solver_petsc_create(pe_t * pe);
+int test_psi_solver_petsc_var_epsilon_create(pe_t * pe);
 
 /*****************************************************************************
  *
@@ -34,6 +35,7 @@ int test_psi_solver_petsc_suite(void) {
   pe_create(MPI_COMM_WORLD, PE_QUIET, &pe);
 
   test_psi_solver_petsc_create(pe);
+  test_psi_solver_petsc_var_epsilon_create(pe);
 
   pe_info(pe, "%-9s %s\n", "PASS", __FILE__);
   pe_free(pe);
@@ -59,6 +61,32 @@ int test_psi_solver_petsc_create(pe_t * pe) {
     psi_solver_petsc_t * petsc = NULL;
 
     ifail = psi_solver_petsc_create(psi, &petsc);
+    assert(ifail != 0);
+    if (ifail != 0) ifail = 0;
+  }
+
+  return 0;
+}
+
+/*****************************************************************************
+ *
+ *  test_psi_solver_petsc_var_epsilon_create
+ *
+ *****************************************************************************/
+
+int test_psi_solver_petsc_var_epsilon_create(pe_t * pe) {
+
+  int ifail = 0;
+  int isInitialised = 0;
+
+  PetscInitialised(&isInitialised);
+
+  if (isInitialised == 0) {
+    psi_t * psi = NULL;
+    var_epsilon_t user = {0};
+    psi_solver_petsc_t * petsc = NULL;
+
+    ifail = psi_solver_petsc_var_epsilon_create(psi, user, &petsc);
     assert(ifail != 0);
     if (ifail != 0) ifail = 0;
   }
