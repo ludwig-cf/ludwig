58a76d65e0040adf806deb3e6cff33837d2c95bd
Sumesh <sumesh@iitm.ac.in>
2023-07-27 14:48:11 +0100

    modified utility to get ellipsoid description as a vtk file

--- util/extract_colloids.c
+++ util/extract_colloids.c
@@ -42,6 +42,9 @@
 
 #include "colloid.h"
 #include "util_fopen.h"
+#include "util.h"
+#include "util_ellipsoid.h"
+#include "util_vector.h"
 
 #define NX 32
 #define NY 32
@@ -55,11 +58,15 @@ static const int  cds_with_v  = 1;  /* Output coordinate, velocity vector and ma
 
 static const char * format3_    = "%10.5f, %10.5f, %10.5f, ";
 static const char * format3end_ = "%10.5f, %10.5f, %10.5f\n";
+static const char * format3wocomma_    = "%10.5f %10.5f %10.5f ";
+static const char * format3wocommaend_ = "%10.5f %10.5f %10.5f\n";
 static const char * formate4end_ = "%14.6e, %14.6e, %14.6e, %14.6e\n";
 
 void colloids_to_csv_header(FILE * fp);
 void colloids_to_csv_header_with_m(FILE * fp);
 void colloids_to_csv_header_with_v(FILE * fp);
+void colloids_to_vtk_header(FILE * fp);
+void colloids_to_vtk_inbetween(FILE * fp);
 int file_name_to_ntime(const char * filename);
 int file_name_to_nfile(const char * filename);
 
@@ -71,6 +78,10 @@ int main(int argc, char ** argv) {
   int nread;
   int ntime = 0;
   int ncount = 0;
+  double worldv1[3]={1.0,0.0,0.0};
+  double worldv2[3]={0.0,1.0,0.0};
+  double elev1[3], elev2[3], elev3[3];
+  double *quater;
   
   double normv;
 
@@ -80,6 +91,8 @@ int main(int argc, char ** argv) {
   FILE * fp_colloids = NULL;
   FILE * fp_csv = NULL;
   char csv_filename[BUFSIZ] = {0};
+  FILE * fp_vtk = NULL;
+  char vtk_filename[BUFSIZ] = {0};
 
   if (argc < 2) {
     printf("Usage: %s <colloid_datafile>\n", argv[0]);
@@ -103,8 +116,21 @@ int main(int argc, char ** argv) {
     exit(0);
   }
 
+  /* Open vtk file (output) */
+
+  sprintf(vtk_filename, "colloids-%8.8d.vtk", ntime);
+  fp_vtk = util_fopen(vtk_filename, "w");
+
+
+  if (fp_csv == NULL) {
+    printf("fopen(%s) failed\n", argv[3]);
+    exit(0);
+  }
+
   if (cds_with_m) colloids_to_csv_header_with_m(fp_csv);
   if (cds_with_v) colloids_to_csv_header_with_v(fp_csv);
+  
+  colloids_to_vtk_header(fp_vtk);
 
   for (nf = 1; nf <= nfile; nf++) {
 
@@ -159,13 +185,28 @@ int main(int argc, char ** argv) {
 	normv = sqrt(s1.v[0]*s1.v[0] + s1.v[1]*s1.v[1] + s1.v[2]*s1.v[2]);
 	fprintf(fp_csv, formate4end_, s1.v[0], s1.v[1], s1.v[2], normv);
       }
+      /* Write coordinates to vtk file*/
+      fprintf(fp_vtk, format3wocommaend_, s1.r[0]-1.0, s1.r[1]-1.0, s1.r[2]-1.0);
+      /* Write Orientations to vtk file*/
+      colloids_to_vtk_inbetween(fp_vtk);
+      printf("reached here\n");
+      quater = s1.quater;
+      rotate_tobodyframe_quaternion(quater, worldv1, elev1);
+      rotate_tobodyframe_quaternion(quater, worldv2, elev2);
+      cross_product(elev1,elev2,elev3);
+      util_vector_normalise(3, elev3);
+      fprintf(fp_vtk, format3wocomma_, 2.0*s1.elabc[0]*elev1[0], 2.0*s1.elabc[0]*elev1[1],2.0*s1.elabc[0]*elev1[2]);
+      fprintf(fp_vtk, format3wocomma_, 2.0*s1.elabc[1]*elev2[0], 2.0*s1.elabc[1]*elev2[1], 2.0*s1.elabc[1]*elev2[2]);
+      fprintf(fp_vtk, format3wocommaend_, 2.0*s1.elabc[2]*elev3[0], 2.0*s1.elabc[2]*elev3[1], 2.0*s1.elabc[2]*elev3[2]);
       ncount += 1;
 
     }
+
   }
 
   /* Finish colloid coordinate output */
   fclose(fp_csv);
+  fclose(fp_vtk);
   if (include_ref) {
     printf("Wrote %d actual colloids + 3 reference colloids in header to %s\n",
 	   ncount, csv_filename);
@@ -220,6 +261,43 @@ void colloids_to_csv_header(FILE * fp) {
 
   return;
 }
+
+/*****************************************************************************
+ *
+ *  colloids_to_vtk_header
+ *
+ *****************************************************************************/
+
+void colloids_to_vtk_header(FILE * fp) {
+
+  assert(fp);
+
+  fprintf(fp, "# vtk DataFile Version 5.1\n");
+  fprintf(fp, "vtk output\n");
+  fprintf(fp, "ASCII\n");
+  fprintf(fp, "DATASET POLYDATA\n");
+  fprintf(fp, "POINTS 2 float\n");
+  fprintf(fp, "%d %d %d\n", 0, 0, 0);
+
+ }
+
+/*****************************************************************************
+ *
+ *  colloids_to_vtk_inbetween
+ *
+ *****************************************************************************/
+
+void colloids_to_vtk_inbetween(FILE * fp) {
+
+  assert(fp);
+
+  fprintf(fp, "POINT_DATA %d\n", 2);
+  fprintf(fp, "TENSORS tensors double\n");
+  fprintf(fp, "%d %d %d ", 0, 0, 0);
+  fprintf(fp, "%d %d %d ", 0, 0, 0);
+  fprintf(fp, "%d %d %d \n", 0, 0, 0);
+
+ }
 
 /*****************************************************************************
  *
