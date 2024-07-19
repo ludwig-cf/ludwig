318a3ad804b49bf067c23dd6abdcd6f9f2709cdb
Kevin <kevin@epcc.ed.ac.uk>
2024-07-19 10:33:41 +0100

    Specifically serial build

--- config/epcc-cirrus-nvcc.mk
+++ config/epcc-cirrus-nvcc.mk
@@ -2,18 +2,15 @@
 #
 #  nvcc build
 #
-#  There are a number of ways forward here... see further comments below
+#  "Serial" build.
 #
-#  E.g., serial with stub MPI
-#  module load nvidia/nvhpc-nompi/22.11
-#
-#  E.g., parallel using NVHPC with MPI ...
-#  module load nvidia/nvhpc/22.11
+#  module load nvidia/nvhpc-nompi
 #
 ###############################################################################
 
-BUILD  = parallel
+BUILD  = serial
 MODEL  = -D_D3Q19_
+TARGET = nvcc
 
 CC     = nvcc
 CFLAGS = -g -DADDR_SOA -O2 -arch=sm_70 -x cu -dc
@@ -24,13 +21,3 @@ CFLAGS = -g -DADDR_SOA -O2 -arch=sm_70 -x cu -dc
 AR = ar
 ARFLAGS = -cr
 LDFLAGS = -arch=sm_70
-
-# nvhpc (mpicc is present but drives nvc not nvcc) so use nvcc still ... but
-MPI_HOME     = ${NVHPC_ROOT}/comm_libs/mpi
-MPI_INC_PATH = -I$(MPI_HOME)/include
-MPI_LIB_PATH = -L$(MPI_HOME)/lib -lmpi
-
-# NVHPC bundled MPI must use mpirun supplied ...
-LAUNCH_SERIAL_CMD =
-LAUNCH_MPIRUN_CMD = mpirun
-MPIRUN_NTASK_FLAG = -np
