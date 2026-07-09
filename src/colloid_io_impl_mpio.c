cc85655a3f47cb6a8da821746487bd63d15c0511
alexei <a.borissov@epcc.ed.ac.uk>
2026-07-09 16:25:43 +0100

    add calls to colloid_info_add_with_state (retain old colloids_info_add calls, just commented out)

--- src/colloid_io_impl_mpio.c
+++ src/colloid_io_impl_mpio.c
@@ -401,7 +401,8 @@ int colloid_io_mpio_read_ascii(colloid_io_mpio_t * io, FILE * fp) {
     /* Add if local and assign the state */
     {
       colloid_t * pc = NULL;
-      colloids_info_add_local(io->info, s.index, s.r, s.a0, &pc);
+      //colloids_info_add_local(io->info, s.index, s.r, s.a0, &pc);
+      colloids_info_add_local_with_state(io->info, &s, &pc);
       if (pc) pc->s = s;
     }
   }
@@ -453,7 +454,8 @@ int colloid_io_mpio_read_binary(colloid_io_mpio_t * io, FILE * fp) {
     /* Add if local and assign the state */
     {
       colloid_t * pc = NULL;
-      colloids_info_add_local(io->info, s.index, s.r, s.a0, &pc);
+      //colloids_info_add_local(io->info, s.index, s.r, s.a0, &pc);
+      colloids_info_add_local_with_state(io->info, &s, &pc);
       if (pc) pc->s = s;
     }
   }
